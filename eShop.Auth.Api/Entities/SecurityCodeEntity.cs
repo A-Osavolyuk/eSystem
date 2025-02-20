888b5e30e2f11165679bf36bb21ff453e2430359
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-02-20 15:49:09 +0200

    Added entity for SSO (Single Sign-On)

---
+++ eShop.Auth.Api/Entities/SecurityCodeEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace eShop.Auth.Api.Entities;
+
+public class SecurityCodeEntity : IIdentifiable<Guid>, IAuditable
+{
+    public Guid Id { get; init; }
+    public string Destination { get; init; } = string.Empty;
+    public string Code { get; init; } = string.Empty;
+    public DateTime ExpireDate { get; init; } = DateTime.UtcNow.AddMinutes(10);
+    public DateTime CreateDate { get; set; }
+    public DateTime UpdateDate { get; set; }
+}
