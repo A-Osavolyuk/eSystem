a65578fcd89a76dbc5dd21edbfc4a7a953eb4b77
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-04-04 13:37:57 +0300

    Implement error handling and response modification

---
+++ eShop.Application/Utilities/ErrorHandler.cs
@@ -0,0 +1,19 @@
+﻿using eShop.Domain.Common.Api;
+
+namespace eShop.Application.Utilities;
+
+public static class ErrorHandler
+{
+    public static ActionResult<Response> Handle(Error error)
+    {
+        return new ObjectResult(
+            new ResponseBuilder()
+                .Failed()
+                .WithResult(error)
+                .WithMessage(error.Message)
+                .Build())
+        {
+            StatusCode = Convert.ToInt32(error.Code),
+        };
+    }
+}
--- eShop.Domain/Common/Api/Response.cs
+++ eShop.Domain/Common/Api/Response.cs
@@ -2,26 +2,24 @@
 
 public class Response
 {
-    private Response() {}
-    public string Message { get; private init; } = string.Empty;
-    public object? Result { get; private init; } = null!;
-    public bool Success { get; private init; } = false;
+    public string Message { get; set; } = string.Empty;
+    public object? Result { get; set; }
+    public bool Success { get; set; }
 
     public static Response Create(string message, object? result = null, bool isSucceeded = false)
     {
-        return new Response()
+        return new Response
         {
-             Message = message,
-             Result = result,
-             Success = isSucceeded
+            Message = message,
+            Result = result,
+            Success = isSucceeded
         };
     }
-
-    public TValue DeserializeResult<TValue>()
+    
+    public TValue? Get<TValue>()
     {
-        var json = JsonConvert.SerializeObject(Result!);
-        var result = JsonConvert.DeserializeObject<TValue>(json);
-        
-        return result!;
+        var json = JsonConvert.SerializeObject(Result);
+        var value = JsonConvert.DeserializeObject<TValue>(json);
+        return value;
     }
 }
