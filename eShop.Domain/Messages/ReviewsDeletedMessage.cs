79811c121200431b07a8003929b3ae6c7532081c
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2025-04-04 18:21:50 +0300

    Remove redundant message

--- eShop.Domain/Messages/ReviewsDeletedMessage.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace eShop.Domain.Messages;
-
-public class ReviewsDeletedMessage
-{
-    public bool IsSucceeded { get; set; } = false;
-    public string Status { get; set; } = string.Empty;
-}
