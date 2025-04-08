93b6001d3788413e4f85e667d39195231a4336c4
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2025-04-08 15:53:00 +0300

    Implement IStorage interface

--- eShop.Domain/Interfaces/Client/IStorage.cs
+++ eShop.Domain/Interfaces/Client/IStorage.cs
@@ -2,8 +2,8 @@
 
 public interface IStorage
 {
-    public ValueTask<T> GetAsync<T>(string key);
-    public ValueTask<T> UpdateAsync<T>(string key, T value);
+    public ValueTask<T?> GetAsync<T>(string key);
+    public ValueTask UpdateAsync<T>(string key, T value);
     public ValueTask CreateAsync<T>(string key, T value);
     public ValueTask DeleteAsync<T>(string key);
     public ValueTask ClearAsync();
