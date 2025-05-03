666eb5e08f5f406a99e495681c7b828c1feb987c
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2025-05-03 15:04:01 +0300

    Refactor Seed class to support generic key types.

--- eShop.Domain/Abstraction/Data/Seed.cs
+++ eShop.Domain/Abstraction/Data/Seed.cs
@@ -1,6 +1,13 @@
 ﻿namespace eShop.Domain.Abstraction.Data;
 
-public abstract class Seed<TEntity>
+public abstract class Seed<TEntity, TKey> 
+    where TEntity : IEntity<TKey>
+{
+    protected abstract List<TEntity> Get();
+}
+
+public abstract class Seed<TEntity> 
+    where TEntity : IEntity
 {
     protected abstract List<TEntity> Get();
 }
