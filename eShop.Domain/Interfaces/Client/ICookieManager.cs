5495d4c3402a18bd5834de8f5db4d4f6bd4881e8
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-05-05 10:58:59 +0300

    Implement cookie management

---
+++ eShop.Domain/Interfaces/Client/ICookieManager.cs
@@ -0,0 +1,8 @@
+﻿namespace eShop.Domain.Interfaces.Client;
+
+public interface ICookieManager
+{
+    public Task<string> GetAsync(string key);
+    public Task SetAsync(string key, string value, int days);
+    public Task RemoveAsync(string key);
+}
