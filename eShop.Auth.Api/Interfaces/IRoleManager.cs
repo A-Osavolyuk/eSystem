9714ff6b01a22d606f34e6338a7142db8d02fb33
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-05-06 11:29:32 +0300

    Implement custom role manager

---
+++ eShop.Auth.Api/Interfaces/IRoleManager.cs
@@ -0,0 +1,7 @@
+﻿namespace eShop.Auth.Api.Interfaces;
+
+public interface IRoleManager
+{
+    public ValueTask<List<RoleEntity>> GetAllAsync(CancellationToken cancellationToken = default);
+    public ValueTask<List<RoleEntity>> GetAllAsync(UserEntity user, CancellationToken cancellationToken = default);
+}
---
+++ eShop.Auth.Api/Services/RoleManager.cs
@@ -0,0 +1,23 @@
+﻿namespace eShop.Auth.Api.Services;
+
+public class RoleManager(AuthDbContext context) : IRoleManager
+{
+    private readonly AuthDbContext context = context;
+
+    public async ValueTask<List<RoleEntity>> GetAllAsync(CancellationToken cancellationToken = default)
+    {
+        var roles = await context.Roles.ToListAsync(cancellationToken);
+        return roles;
+    }
+
+    public async ValueTask<List<RoleEntity>> GetAllAsync(UserEntity user, CancellationToken cancellationToken = default)
+    {
+        var roles = await context.UserRoles
+            .Where(x => x.UserId == user.Id)
+            .Include(x => x.Role)
+            .Select(x => x.Role)
+            .ToListAsync(cancellationToken);
+        
+        return roles;
+    }
+}
