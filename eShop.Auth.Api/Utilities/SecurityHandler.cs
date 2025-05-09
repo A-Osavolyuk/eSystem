973b6c9b40adec77208565da8c9fff2bbf607685
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-05-09 15:44:46 +0300

    Add SecretManager service for managing user secrets

--- eShop.Auth.Api/Utilities/SecurityHandler.cs
+++ eShop.Auth.Api/Utilities/SecurityHandler.cs
@@ -4,14 +4,6 @@ namespace eShop.Auth.Api.Utilities;
 
 public static class SecurityHandler
 {
-    public static string GenerateSecret()
-    {
-        var secretKey = KeyGeneration.GenerateRandomKey(20);
-        var base32Secret = Base32Encoding.ToString(secretKey);
-
-        return base32Secret;
-    }
-    
     public static string GenerateToken()
     {
         var randomCode = new Random().Next(0, 999999).ToString();
