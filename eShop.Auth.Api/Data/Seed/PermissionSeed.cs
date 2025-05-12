4aba07b60e5b4421285e91d37a2ab951d87781b1
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-05-12 16:01:24 +0300

    Add new admin permissions to PermissionSeed

--- eShop.Auth.Api/Data/Seed/PermissionSeed.cs
+++ eShop.Auth.Api/Data/Seed/PermissionSeed.cs
@@ -73,6 +73,48 @@ public class PermissionSeed : Seed<PermissionEntity, Guid>
             },
             new()
             {
+                Id = Guid.Parse("0e572d3d-b4a1-4dc8-8c3b-c52c86a61a45"),
+                ResourceId = Guid.Parse("70969390-c949-4426-b3c1-7fd86580b62e"), 
+                Name = "Admin:Assign",
+                Action = ActionType.Assign
+            },
+            new()
+            {
+                Id = Guid.Parse("80f1dc43-fd49-4c57-8d30-635f5f5a9f37"),
+                ResourceId = Guid.Parse("70969390-c949-4426-b3c1-7fd86580b62e"), 
+                Name = "Admin:Unassign",
+                Action = ActionType.Unassign
+            },
+            new()
+            {
+                Id = Guid.Parse("3be35bbf-bc09-4ccf-a4d5-6211f9b09a90"),
+                ResourceId = Guid.Parse("70969390-c949-4426-b3c1-7fd86580b62e"), 
+                Name = "Admin:Grant",
+                Action = ActionType.Grant
+            },
+            new()
+            {
+                Id = Guid.Parse("65b6506b-52bc-4f9f-9741-37b1059e1d7c"),
+                ResourceId = Guid.Parse("70969390-c949-4426-b3c1-7fd86580b62e"), 
+                Name = "Admin:Revoke",
+                Action = ActionType.Revoke
+            },
+            new()
+            {
+                Id = Guid.Parse("a2b77f86-80e9-468f-b875-d63087f9a68f"),
+                ResourceId = Guid.Parse("70969390-c949-4426-b3c1-7fd86580b62e"), 
+                Name = "Admin:Invite",
+                Action = ActionType.Invite
+            },
+            new()
+            {
+                Id = Guid.Parse("d4aeea79-58d1-4a66-8f02-9d3c9c40e2f7"),
+                ResourceId = Guid.Parse("70969390-c949-4426-b3c1-7fd86580b62e"), 
+                Name = "Admin:Remove",
+                Action = ActionType.Remove
+            },
+            new()
+            {
                 Id = Guid.Parse("a4e12251-2d6d-4b0d-9053-2d56fcd8ac9b"),
                 ResourceId = Guid.Parse("70969390-c949-4426-b3c1-7fd86580b62e"), 
                 Name = "Admin:All",
