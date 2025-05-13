3c3ec22e26ab05933b361ed3bebc9747b1b3615a
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-05-13 11:23:24 +0300

    Enhance permission check with "All" access support

--- eShop.Auth.Api/Security/Authorization/PermissionRequirement.cs
+++ eShop.Auth.Api/Security/Authorization/PermissionRequirement.cs
@@ -11,11 +11,19 @@ public class PermissionHandler : AuthorizationHandler<PermissionRequirement>
         PermissionRequirement requirement)
     {
         if (context.User.HasClaim(c => c.Type == ClaimTypes.Permission && c.Value == requirement.PermissionName) 
-            || context.User.IsInRole("Admin"))
+            || context.User.IsInRole("Admin") || HasAll(requirement.PermissionName))
         {
             context.Succeed(requirement);
         }
 
         return Task.CompletedTask;
+    }
+
+    private bool HasAll(string permissionName)
+    {
+        var permissionNameParts = permissionName.Split(':');
+        var resource = permissionNameParts[0];
+        var allAccessPermission = $"{resource}:All";
+        return permissionName == allAccessPermission;
     }
 }
