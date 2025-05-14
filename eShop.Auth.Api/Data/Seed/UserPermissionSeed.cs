68ea9a44d9afd87f329dabbc10a81d012770775d
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-05-14 12:02:01 +0300

    Clean up user permission seed

--- eShop.Auth.Api/Data/Seed/UserPermissionSeed.cs
+++ eShop.Auth.Api/Data/Seed/UserPermissionSeed.cs
@@ -4,18 +4,6 @@ public class UserPermissionSeed : Seed<UserPermissionsEntity>
 {
     public override List<UserPermissionsEntity> Get()
     {
-        return
-        [
-            new ()
-            {
-                UserId = Guid.Parse("abb9d2ed-c3d2-4df9-ba88-eab018b95bc3"),
-                PermissionId = Guid.Parse("e61e3480-4e63-4f42-bb3f-9744415036cb")
-            },
-            new ()
-            {
-                UserId = Guid.Parse("abb9d2ed-c3d2-4df9-ba88-eab018b95bc3"),
-                PermissionId = Guid.Parse("a4e12251-2d6d-4b0d-9053-2d56fcd8ac9b")
-            }
-        ];
+        return [];
     }
 }
