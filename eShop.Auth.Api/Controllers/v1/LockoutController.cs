126d698de55cc7a470dd63a93a60e2ba01eddf1b
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-05-27 13:22:45 +0300

    Add lockout controller

---
+++ eShop.Auth.Api/Controllers/v1/LockoutController.cs
@@ -0,0 +1,51 @@
+﻿using eShop.Auth.Api.Features.Lockout.Commands;
+using eShop.Auth.Api.Features.Lockout.Queries;
+using eShop.Domain.Requests.API.Admin;
+
+namespace eShop.Auth.Api.Controllers.v1;
+
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiController]
+[ApiVersion("1.0")]
+[Authorize]
+public class LockoutController(ISender sender) : ControllerBase
+{
+    private readonly ISender sender = sender;
+    
+    [EndpointSummary("Get lockout state")]
+    [EndpointDescription("Get lockout state")]
+    [ProducesResponseType(200)]
+    [HttpGet("get-state/{email}")]
+    public async ValueTask<ActionResult<Response>> GetStateAsync(string email)
+    {
+        var result = await sender.Send(new GetLockoutStateQuery(email));
+
+        return result.Match(
+            s => Ok(new ResponseBuilder().Succeeded().WithResult(s.Value!).Build()),
+            ErrorHandler.Handle);
+    }
+    
+    [EndpointSummary("Lockout")]
+    [EndpointDescription("Lockout")]
+    [ProducesResponseType(200)]
+    [HttpPost("lockout")]
+    public async ValueTask<ActionResult<Response>> LockoutAsync([FromBody] LockoutRequest request)
+    {
+        var result = await sender.Send(new LockoutCommand(request));
+        return result.Match(
+            s => Ok(new ResponseBuilder().Succeeded().WithMessage(s.Message).Build()),
+            ErrorHandler.Handle);
+    }
+    
+    [EndpointSummary("Unlock")]
+    [EndpointDescription("Unlock")]
+    [ProducesResponseType(200)]
+    [HttpPost("unlock")]
+    public async ValueTask<ActionResult<Response>> UnlockAsync([FromBody] UnlockRequest request)
+    {
+        var result = await sender.Send(new UnlockCommand(request));
+        return result.Match(
+            s => Ok(new ResponseBuilder().Succeeded().WithMessage(s.Message).Build()),
+            ErrorHandler.Handle);
+    }
+}
--- eShop.Auth.Api/Features/Lockout/Queries/GetLockoutStateQuery.cs
+++ eShop.Auth.Api/Features/Lockout/Queries/GetLockoutStateQuery.cs
@@ -1,6 +1,6 @@
 ﻿namespace eShop.Auth.Api.Features.Lockout.Queries;
 
-public record GetLockoutStateQuery(Guid UserId) : IRequest<Result>;
+public record GetLockoutStateQuery(string Email) : IRequest<Result>;
 
 public class GetLockoutStateQueryHandler(
     ILockoutManager lockoutManager,
@@ -11,11 +11,11 @@ public class GetLockoutStateQueryHandler(
 
     public async Task<Result> Handle(GetLockoutStateQuery request, CancellationToken cancellationToken)
     {
-        var user = await userManager.FindByIdAsync(request.UserId, cancellationToken);
+        var user = await userManager.FindByEmailAsync(request.Email, cancellationToken);
 
         if (user is null)
         {
-            return Results.NotFound($"Cannot find user with ID {request.UserId}.");
+            return Results.NotFound($"Cannot find user with email {request.Email}.");
         }
 
         var lockoutState = await lockoutManager.FindAsync(user, cancellationToken);
