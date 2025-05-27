dbb6e19ceaec244aed1f0df244afd077abd56336
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-05-27 15:04:24 +0300

    Migrate DB

---
+++ eShop.Auth.Api/Migrations/20250527115621_AddPermanentColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eShop.Auth.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPermanentColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Permanent",
+                table: "LockoutState",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Permanent",
+                table: "LockoutState");
+        }
+    }
+}
