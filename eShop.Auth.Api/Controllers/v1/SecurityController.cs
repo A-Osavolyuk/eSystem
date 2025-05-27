4ec51b8b6670255587e1995ee24c03f21a8aaaa6
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-05-27 16:28:32 +0300

    Refactor SecurityController for clarity and endpoint reordering

--- eShop.Auth.Api/Controllers/v1/SecurityController.cs
+++ eShop.Auth.Api/Controllers/v1/SecurityController.cs
@@ -9,33 +9,19 @@ namespace eShop.Auth.Api.Controllers.v1;
 public class SecurityController(ISender sender) : ControllerBase
 {
     private readonly ISender sender = sender;
-
-    [EndpointSummary("Verify code")]
-    [EndpointDescription("Verifies code")]
+    
+    [EndpointSummary("Login")]
+    [EndpointDescription("Login")]
     [ProducesResponseType(200)]
     [AllowAnonymous]
-    [HttpPost("verify-code")]
-    public async ValueTask<ActionResult<Response>> VerifyCodeAsync([FromBody] VerifyCodeRequest request)
+    [HttpPost("login")]
+    [ValidationFilter]
+    public async ValueTask<ActionResult<Response>> Login([FromBody] LoginRequest request)
     {
-        var result = await sender.Send(new VerifyCodeCommand(request));
+        var result = await sender.Send(new LoginCommand(request));
 
         return result.Match(
-            s => Ok(new ResponseBuilder().Succeeded().WithMessage(s.Message).Build()),
-            ErrorHandler.Handle);
-    }
-
-    [EndpointSummary("Resend verification code")]
-    [EndpointDescription("Resends verification code")]
-    [ProducesResponseType(200)]
-    [AllowAnonymous]
-    [HttpPost("resend-verification-code")]
-    public async ValueTask<ActionResult<Response>> ResendVerificationCode(
-        [FromBody] ResendEmailVerificationCodeRequest request)
-    {
-        var result = await sender.Send(new ResendEmailVerificationCodeCommand(request));
-
-        return result.Match(
-            s => Ok(new ResponseBuilder().Succeeded().WithMessage(s.Message).Build()),
+            s => Ok(new ResponseBuilder().Succeeded().WithResult(s.Value!).WithMessage(s.Message).Build()),
             ErrorHandler.Handle);
     }
 
@@ -53,19 +39,34 @@ public class SecurityController(ISender sender) : ControllerBase
             s => Ok(new ResponseBuilder().Succeeded().WithMessage(s.Message).Build()),
             ErrorHandler.Handle);
     }
-
-    [EndpointSummary("Login")]
-    [EndpointDescription("Login")]
+    
+    [EndpointSummary("Refresh token")]
+    [EndpointDescription("Refresh token")]
     [ProducesResponseType(200)]
-    [AllowAnonymous]
-    [HttpPost("login")]
-    [ValidationFilter]
-    public async ValueTask<ActionResult<Response>> Login([FromBody] LoginRequest request)
+    [Authorize]
+    [HttpPost("refresh-token")]
+    public async ValueTask<ActionResult<Response>> RefreshTokenAsync([FromBody] RefreshTokenRequest request)
     {
-        var result = await sender.Send(new LoginCommand(request));
+        var result = await sender.Send(new RefreshTokenCommand(request));
 
         return result.Match(
-            s => Ok(new ResponseBuilder().Succeeded().WithResult(s.Value!).WithMessage(s.Message).Build()),
+            s => Ok(new ResponseBuilder().Succeeded().WithMessage(s.Message).WithResult(s.Value!).Build()),
+            ErrorHandler.Handle);
+    }
+    
+    [EndpointSummary("Change password")]
+    [EndpointDescription("Change password")]
+    [ProducesResponseType(200)]
+    [Authorize(Policy = "UpdateAccountPolicy")]
+    [HttpPost("change-password")]
+    [ValidationFilter]
+    public async ValueTask<ActionResult<Response>> ChangePassword(
+        [FromBody] ChangePasswordRequest changePasswordRequest)
+    {
+        var result = await sender.Send(new ChangePasswordCommand(changePasswordRequest));
+
+        return result.Match(
+            s => Ok(new ResponseBuilder().Succeeded().WithMessage(s.Message).WithResult(s.Value!).Build()),
             ErrorHandler.Handle);
     }
 
@@ -82,71 +83,12 @@ public class SecurityController(ISender sender) : ControllerBase
             s => Ok(new ResponseBuilder().Succeeded().WithMessage(s.Message).Build()),
             ErrorHandler.Handle);
     }
-
-    [EndpointSummary("Verify email")]
-    [EndpointDescription("Verifies email")]
-    [ProducesResponseType(200)]
-    [AllowAnonymous]
-    [HttpPost("verify-email")]
-    public async ValueTask<ActionResult<Response>> ConfirmEmail(
-        [FromBody] VerifyEmailRequest request)
-    {
-        var result = await sender.Send(new VerifyEmailCommand(request));
-
-        return result.Match(
-            s => Ok(new ResponseBuilder().Succeeded().WithMessage(s.Message).Build()),
-            ErrorHandler.Handle);
-    }
-
-    [EndpointSummary("Confirm change email")]
-    [EndpointDescription("Confirms an email change")]
-    [ProducesResponseType(200)]
-    [Authorize(Policy = "UpdateAccountPolicy")]
-    [HttpPost("confirm-email")]
-    public async ValueTask<ActionResult<Response>> ConfirmChangeEmail(
-        [FromBody] ConfirmChangeEmailRequest request)
-    {
-        var result = await sender.Send(new ConfirmChangeEmailCommand(request));
-
-        return result.Match(
-            s => Ok(new ResponseBuilder().Succeeded().WithMessage(s.Message).Build()),
-            ErrorHandler.Handle);
-    }
-
-    [EndpointSummary("Confirm change phone number")]
-    [EndpointDescription("Confirm a phone number change")]
-    [ProducesResponseType(200)]
-    [Authorize(Policy = "UpdateAccountPolicy")]
-    [HttpPost("confirm-phone-number")]
-    public async ValueTask<ActionResult<Response>> ConfirmChangePhoneNumber(
-        [FromBody] ConfirmChangePhoneNumberRequest request)
-    {
-        var result = await sender.Send(new ConfirmChangePhoneNumberCommand(request));
-
-        return result.Match(
-            s => Ok(new ResponseBuilder().Succeeded().WithMessage(s.Message).WithResult(s.Value!).Build()),
-            ErrorHandler.Handle);
-    }
     
-    [EndpointSummary("Refresh token")]
-    [EndpointDescription("Refresh token")]
-    [ProducesResponseType(200)]
-    [Authorize]
-    [HttpPost("refresh-token")]
-    public async ValueTask<ActionResult<Response>> RefreshTokenAsync([FromBody] RefreshTokenRequest request)
-    {
-        var result = await sender.Send(new RefreshTokenCommand(request));
-
-        return result.Match(
-            s => Ok(new ResponseBuilder().Succeeded().WithMessage(s.Message).WithResult(s.Value!).Build()),
-            ErrorHandler.Handle);
-    }
-
     [EndpointSummary("Confirm password")]
     [EndpointDescription("Confirm password reset")]
     [ProducesResponseType(200)]
     [AllowAnonymous]
-    [HttpPost("confirm-password")]
+    [HttpPost("confirm-reset")]
     [ValidationFilter]
     public async ValueTask<ActionResult<Response>> ConfirmResetPassword(
         [FromBody] ConfirmResetPasswordRequest confirmPasswordResetRequest)
@@ -173,6 +115,36 @@ public class SecurityController(ISender sender) : ControllerBase
             s => Ok(new ResponseBuilder().Succeeded().WithMessage(s.Message).WithResult(s.Value!).Build()),
             ErrorHandler.Handle);
     }
+    
+    [EndpointSummary("Confirm change email")]
+    [EndpointDescription("Confirms an email change")]
+    [ProducesResponseType(200)]
+    [Authorize(Policy = "UpdateAccountPolicy")]
+    [HttpPost("confirm-email")]
+    public async ValueTask<ActionResult<Response>> ConfirmChangeEmail(
+        [FromBody] ConfirmChangeEmailRequest request)
+    {
+        var result = await sender.Send(new ConfirmChangeEmailCommand(request));
+
+        return result.Match(
+            s => Ok(new ResponseBuilder().Succeeded().WithMessage(s.Message).Build()),
+            ErrorHandler.Handle);
+    }
+    
+    [EndpointSummary("Verify email")]
+    [EndpointDescription("Verifies email")]
+    [ProducesResponseType(200)]
+    [AllowAnonymous]
+    [HttpPost("verify-email")]
+    public async ValueTask<ActionResult<Response>> ConfirmEmail(
+        [FromBody] VerifyEmailRequest request)
+    {
+        var result = await sender.Send(new VerifyEmailCommand(request));
+
+        return result.Match(
+            s => Ok(new ResponseBuilder().Succeeded().WithMessage(s.Message).Build()),
+            ErrorHandler.Handle);
+    }
 
     [EndpointSummary("Request change phone number")]
     [EndpointDescription("Request a phone number change")]
@@ -189,20 +161,48 @@ public class SecurityController(ISender sender) : ControllerBase
             s => Ok(new ResponseBuilder().Succeeded().WithMessage(s.Message).WithResult(s.Value!).Build()),
             ErrorHandler.Handle);
     }
-
-    [EndpointSummary("Change password")]
-    [EndpointDescription("Change password")]
+    
+    [EndpointSummary("Confirm change phone number")]
+    [EndpointDescription("Confirm a phone number change")]
     [ProducesResponseType(200)]
     [Authorize(Policy = "UpdateAccountPolicy")]
-    [HttpPost("change-password")]
-    [ValidationFilter]
-    public async ValueTask<ActionResult<Response>> ChangePassword(
-        [FromBody] ChangePasswordRequest changePasswordRequest)
+    [HttpPost("confirm-phone-number")]
+    public async ValueTask<ActionResult<Response>> ConfirmChangePhoneNumber(
+        [FromBody] ConfirmChangePhoneNumberRequest request)
     {
-        var result = await sender.Send(new ChangePasswordCommand(changePasswordRequest));
+        var result = await sender.Send(new ConfirmChangePhoneNumberCommand(request));
 
         return result.Match(
             s => Ok(new ResponseBuilder().Succeeded().WithMessage(s.Message).WithResult(s.Value!).Build()),
+            ErrorHandler.Handle);
+    }
+    
+    [EndpointSummary("Verify code")]
+    [EndpointDescription("Verifies code")]
+    [ProducesResponseType(200)]
+    [AllowAnonymous]
+    [HttpPost("verify-code")]
+    public async ValueTask<ActionResult<Response>> VerifyCodeAsync([FromBody] VerifyCodeRequest request)
+    {
+        var result = await sender.Send(new VerifyCodeCommand(request));
+
+        return result.Match(
+            s => Ok(new ResponseBuilder().Succeeded().WithMessage(s.Message).Build()),
+            ErrorHandler.Handle);
+    }
+
+    [EndpointSummary("Resend verification code")]
+    [EndpointDescription("Resends verification code")]
+    [ProducesResponseType(200)]
+    [AllowAnonymous]
+    [HttpPost("resend-verification-code")]
+    public async ValueTask<ActionResult<Response>> ResendVerificationCode(
+        [FromBody] ResendEmailVerificationCodeRequest request)
+    {
+        var result = await sender.Send(new ResendEmailVerificationCodeCommand(request));
+
+        return result.Match(
+            s => Ok(new ResponseBuilder().Succeeded().WithMessage(s.Message).Build()),
             ErrorHandler.Handle);
     }
 }
