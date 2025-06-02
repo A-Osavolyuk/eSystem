cd9417fc54d7ac6d33df9d588ca641d11fab6083
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-06-02 13:35:21 +0300

    Add QueueConfiguration class for storing queue configurations

---
+++ eShop.MessageBus/Configurations/QueueConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using eShop.Domain.Enums;
+
+namespace eShop.MessageBus.Configurations;
+
+public class QueueConfiguration(Type type, string queueName, SenderType sender)
+{
+    public Type MessageType { get; } = type;
+    public string QueueName { get; } = queueName;
+    public SenderType Sender { get; } = sender;
+}
