acbf9cbcf5c342372c6017f45bfc95a2053c251c
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-06-03 13:42:08 +0300

    Remove profile service methods as redundant

--- eShop.Domain/Interfaces/Client/IProfileService.cs
+++ eShop.Domain/Interfaces/Client/IProfileService.cs
@@ -5,8 +5,5 @@ namespace eShop.Domain.Interfaces.Client;
 
 public interface IProfileService
 {
-    public ValueTask<Response> GetPhoneNumberAsync(string email);
-    public ValueTask<Response> GetPersonalDataAsync(string email);
-    public ValueTask<Response> ChangeUserNameAsync(ChangeUserNameRequest request);
-    public ValueTask<Response> ChangePersonalDataAsync(ChangePersonalDataRequest request);
+
 }
--- eShop.Infrastucture/Services/ProfileService.cs
+++ eShop.Infrastucture/Services/ProfileService.cs
@@ -9,20 +9,6 @@ public class ProfileService(
     IApiClient pipe,
     IConfiguration configuration) : ApiService(configuration, pipe), IProfileService
 {
-    public async ValueTask<Response> GetPhoneNumberAsync(string email) => await ApiClient.SendAsync(
-        new HttpRequest { Url = $"{Gateway}/api/v1/Profile/get-phone-number/{email}", Method = HttpMethod.Get },
-        new HttpOptions { ValidateToken = true, WithBearer = true });
 
-    public async ValueTask<Response> GetPersonalDataAsync(string email) => await ApiClient.SendAsync(
-        new HttpRequest { Url = $"{Gateway}/api/v1/Profile/get-personal-data/{email}", Method = HttpMethod.Get },
-        new HttpOptions { ValidateToken = true, WithBearer = true });
-
-    public async ValueTask<Response> ChangeUserNameAsync(ChangeUserNameRequest request) => await ApiClient.SendAsync(
-        new HttpRequest { Url = $"{Gateway}/api/v1/Profile/change-user-name", Method = HttpMethod.Patch, Data = request },
-        new HttpOptions { ValidateToken = true, WithBearer = true });
-
-    public async ValueTask<Response> ChangePersonalDataAsync(ChangePersonalDataRequest request) => await ApiClient.SendAsync(
-        new HttpRequest { Url = $"{Gateway}/api/v1/Profile/change-personal-data", Method = HttpMethod.Put, Data = request },
-        new HttpOptions { ValidateToken = true, WithBearer = true });
 
 }
