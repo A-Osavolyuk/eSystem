29f7b83fc1c307f293e3ed103ad3aa7715c206f8
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-06-04 11:25:43 +0300

    Remove unused CodeSet and SecurityToken types

--- eShop.Domain/Types/SecurityToken.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace eShop.Domain.Types;
-
-public class SecurityToken
-{
-    public string AccessToken { get; set; } = string.Empty;
-    public string RefreshToken { get; set; } = string.Empty;
-}
