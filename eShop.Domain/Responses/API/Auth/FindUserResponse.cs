a8f4a096703e5682d144200abddd962701aaac5e
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-06-18 15:46:26 +0300

    Remove unused DTOs and queries

--- eShop.Domain/Responses/API/Auth/FindUserResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace eShop.Domain.Responses.API.Auth;
-
-public record FindUserResponse
-{
-    public AccountDataDto AccountDataDto { get; set; } = null!;
-    public PersonalDataDto PersonalDataDtoEntity { get; set; } = null!;
-    public PermissionsDataDto PermissionsDataDto {  get; set; } = null!; 
-}
