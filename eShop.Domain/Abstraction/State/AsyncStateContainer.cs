9e9a3fb3cae20facf3e1dd319bdcb817e1d18f3d
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-06-20 13:23:59 +0300

    Add abstraction for state containers

---
+++ eShop.Domain/Abstraction/State/AsyncStateContainer.cs
@@ -0,0 +1,10 @@
+﻿namespace eShop.Domain.Abstraction.State;
+
+public abstract class AsyncStateContainer
+{
+    public event Func<Task>? OnChange;
+
+    public abstract Task Change();
+
+    protected async Task StateChanged() => await OnChange!.Invoke();
+}
---
+++ eShop.Domain/Abstraction/State/StateContainer.cs
@@ -0,0 +1,13 @@
+﻿namespace eShop.Domain.Abstraction.State;
+
+public abstract class StateContainer
+{
+    public event Action? OnChange;
+
+    public void Change()
+    {
+        StateChanged();
+    }
+
+    private void StateChanged() => OnChange!.Invoke();
+}
