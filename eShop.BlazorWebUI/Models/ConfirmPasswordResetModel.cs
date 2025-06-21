ce872cb8233fb68d0d1aad38be2ab99df7f5ee98
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2025-06-21 23:05:18 +0300

    Refactor reset password page

---
+++ eShop.BlazorWebUI/Models/ConfirmPasswordResetModel.cs
@@ -0,0 +1,7 @@
+﻿namespace eShop.BlazorWebUI.Models;
+
+public class ConfirmPasswordResetModel
+{
+    public string NewPassword { get; set; } = string.Empty;
+    public string ConfirmNewPassword { get; set; } = string.Empty;
+}
---
+++ eShop.BlazorWebUI/Models/VerifyCodeModel.cs
@@ -0,0 +1,9 @@
+﻿using eShop.Domain.Enums;
+
+namespace eShop.BlazorWebUI.Models;
+
+public class VerifyCodeModel
+{
+    public string Code { get; set; } = string.Empty;
+    public CodeType Type { get; set; }
+}
