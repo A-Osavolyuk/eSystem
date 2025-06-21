0439af1f310de117f9c67bdb5d43a16ab5198df8
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2025-06-21 23:05:46 +0300

    Remove destination property

--- eShop.Domain/Requests/API/Auth/VerifyCodeRequest.cs
+++ eShop.Domain/Requests/API/Auth/VerifyCodeRequest.cs
@@ -4,6 +4,5 @@ public class VerifyCodeRequest
 {
     public Guid UserId { get; set; }
     public string Code { get; set; } = string.Empty;
-    public string Destination { get; set; } = string.Empty;
     public CodeType Type { get; set; }
 }
