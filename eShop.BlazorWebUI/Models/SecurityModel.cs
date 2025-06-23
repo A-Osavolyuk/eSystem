9f5f00ae98dabdb629afd0f515872ecb643a4ccf
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-06-23 16:19:42 +0300

    Add dialog to display authenticator QR code

--- eShop.BlazorWebUI/Models/SecurityModel.cs
+++ eShop.BlazorWebUI/Models/SecurityModel.cs
@@ -5,4 +5,5 @@ namespace eShop.BlazorWebUI.Models;
 public class SecurityModel
 {
     public List<UserProviderDto> Providers { get; set; } = [];
+    public string QrCode { get; set; } = string.Empty;
 }
