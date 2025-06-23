872196b287361702b547a136b17a22072875a66f
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-06-23 14:21:18 +0300

    Add all providers to default seed

--- eShop.Auth.Api/Data/Seed/UserProviderSeed.cs
+++ eShop.Auth.Api/Data/Seed/UserProviderSeed.cs
@@ -11,7 +11,24 @@ public class UserProviderSeed : Seed<UserProviderEntity>
                 UserId = Guid.Parse("abb9d2ed-c3d2-4df9-ba88-eab018b95bc3"),
                 ProviderId = Guid.Parse("a4d155e3-1746-4f4d-af0e-e5ebca3ffcac"),
                 CreateDate = DateTimeOffset.UtcNow,
-                UpdateDate = null
+                UpdateDate = null,
+                Subscribed = true
+            },
+            new UserProviderEntity()
+            {
+                UserId = Guid.Parse("abb9d2ed-c3d2-4df9-ba88-eab018b95bc3"),
+                ProviderId = Guid.Parse("60c95fd9-c0e1-4a08-8d21-50c66f3a91d4"),
+                CreateDate = DateTimeOffset.UtcNow,
+                UpdateDate = null,
+                Subscribed = false
+            },
+            new UserProviderEntity()
+            {
+                UserId = Guid.Parse("abb9d2ed-c3d2-4df9-ba88-eab018b95bc3"),
+                ProviderId = Guid.Parse("f01b5a5a-8f4b-4814-8ade-2935e02af0a5"),
+                CreateDate = DateTimeOffset.UtcNow,
+                UpdateDate = null,
+                Subscribed = false
             },
         ];
     }
