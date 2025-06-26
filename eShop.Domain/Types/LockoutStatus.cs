3eba3680afd9af9640b21b0d9127711af8d7973c
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-06-26 15:11:19 +0300

    Remove unused types

--- eShop.Domain/Types/LockoutStatus.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace eShop.Domain.Types;
-
-public class LockoutStatus
-{
-    public bool LockoutEnabled { get; set; }
-    public DateTimeOffset? LockoutEnd { get; set; }
-}
--- eShop.Domain/Types/QrCode.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace eShop.Domain.Types;
-
-public class QrCode
-{
-    public string Url { get; set; } = string.Empty;
-}
