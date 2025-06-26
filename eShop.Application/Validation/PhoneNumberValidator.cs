4ebb37d8a3f7cbd18420f3d17229dc10054e8624
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-06-26 10:58:02 +0300

    Add standard validators

---
+++ eShop.Application/Validation/PhoneNumberValidator.cs
@@ -0,0 +1,11 @@
+﻿namespace eShop.Application.Validation;
+
+public class PhoneNumberValidator : AbstractValidator<string>
+{
+    public PhoneNumberValidator()
+    {
+        RuleFor(x => x)
+            .Matches(@"^\+\(\d{2}\)-\d{3}-\d{3}-\d{4}$|^\d{12}$").WithMessage("Wrong phone number format. Must be: +(xx)-xxx-xxx-xxxx")
+            .NotEmpty().WithMessage("Field is must!");
+    }
+}
