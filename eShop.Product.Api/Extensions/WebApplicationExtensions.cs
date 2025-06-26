c19e26a253e1ad6e9a86333b721135dfb3e6225e
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-06-26 15:20:12 +0300

    Remove redundant mongo configuration

--- eShop.Product.Api/Extensions/WebApplicationExtensions.cs
+++ eShop.Product.Api/Extensions/WebApplicationExtensions.cs
@@ -16,36 +16,6 @@ public static class WebApplicationExtensions
         app.UseAuthentication();
         app.UseAuthorization();
         app.MapControllers();
-        app.ConfigureMongoDb();
         app.UseExceptionHandler();
-    }
-
-    private static void ConfigureMongoDb(this WebApplication app)
-    {
-        RegisterClassMaps();
-    }
-
-    private static void RegisterClassMaps()
-    {
-        if (!BsonClassMap.IsClassMapRegistered(typeof(ProductEntity)))
-        {
-            BsonClassMap.RegisterClassMap<ProductEntity>(cm =>
-            {
-                cm.AutoMap();
-                cm.SetIsRootClass(true);
-                cm.SetDiscriminator("Product");
-            });
-
-            BsonClassMap.RegisterClassMap<ShoesEntity>(cm =>
-            {
-                cm.AutoMap();
-                cm.SetDiscriminator("Shoes");
-            });
-            BsonClassMap.RegisterClassMap<ClothingEntity>(cm =>
-            {
-                cm.AutoMap();
-                cm.SetDiscriminator("Clothing");
-            });
-        }
     }
 }
