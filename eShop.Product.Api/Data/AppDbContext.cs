97a63dc710a720d8793405793e0e89c4d831a13c
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-06-27 11:10:56 +0300

    Add DB configuration for code flow

--- eShop.Product.Api/Data/AppDbContext.cs
+++ eShop.Product.Api/Data/AppDbContext.cs
@@ -1,6 +1,25 @@
-﻿namespace eShop.Product.Api.Data;
+﻿using eShop.Domain.Enums;
+using eShop.Product.Api.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace eShop.Product.Api.Data;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
+    public DbSet<ProductEntity> Products { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<ProductEntity>(e =>
+        {
+            e.UseTptMappingStrategy();
+
+            e.HasKey(x => x.Id);
+            
+            e.Property(x => x.Name).HasMaxLength(128);
+            e.Property(x => x.ProductType).HasEnumConversion();
+            e.Property(x => x.PricePerUnitType).HasEnumConversion();
+            e.Property(x => x.UnitOfMeasure).HasEnumConversion();
+        });
+    }
 }
---
+++ eShop.Product.Api/Entities/ProductEntity.cs
@@ -0,0 +1,22 @@
+﻿using eShop.Domain.Abstraction.Data;
+using eShop.Domain.Enums;
+
+namespace eShop.Product.Api.Entities;
+
+public class ProductEntity : IEntity
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+    public long Article { get; set; }
+    
+    public ProductType ProductType { get; set; }
+    public UnitOfMeasure UnitOfMeasure { get; set; }
+    public PricePerUnitType PricePerUnitType { get; set; }
+    
+    public decimal Price { get; set; }
+    public int QuantityInStock { get; set; }
+
+    public DateTimeOffset? CreateDate { get; set; }
+    public DateTimeOffset? UpdateDate { get; set; }
+}
