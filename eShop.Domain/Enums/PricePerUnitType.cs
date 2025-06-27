42f1a56dccca405bf1b4319e7317243b64690b9d
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-06-27 11:10:36 +0300

    Add default enums for product flow

---
+++ eShop.Domain/Enums/PricePerUnitType.cs
@@ -0,0 +1,18 @@
+﻿namespace eShop.Domain.Enums;
+
+public enum PricePerUnitType
+{
+    PerUnit,        
+    PerKilogram,    
+    PerGram,        
+    PerLiter,       
+    PerMilliliter,  
+    PerMeter,       
+    PerSquareMeter, 
+    PerCubicMeter,  
+    PerHour,        
+    PerDay,         
+    PerItem,        
+    PerDozen,       
+    PerPackage      
+}
---
+++ eShop.Domain/Enums/ProductType.cs
@@ -0,0 +1,20 @@
+﻿namespace eShop.Domain.Enums;
+
+public enum ProductType
+{
+    Fruit,
+    Vegetable,
+    Berries,
+    Dairy,
+    Meat,
+    Seafood,
+    Grain,
+    Beverage,
+    Bakery,
+    Snack,
+    Spice,
+    Herb,
+    Condiment,
+    Frozen,
+    PreparedFood,
+}
