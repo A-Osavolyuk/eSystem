da52ad1b1980efba699161d289de119073fab859
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-06-30 13:14:25 +0300

    Add product create page

---
+++ eShop.BlazorWebUI/Models/ProductModel.cs
@@ -0,0 +1,16 @@
+﻿using eShop.Domain.DTOs;
+using eShop.Domain.Enums;
+
+namespace eShop.BlazorWebUI.Models;
+
+public class ProductModel
+{
+    public string Name { get; set; } = string.Empty;
+    public TypeModel Type { get; set; } = new();
+    public decimal Price { get; set; }
+    public int QuantityInStock { get; set; }
+    public UnitOfMeasure UnitOfMeasure { get; set; }
+    public PricePerUnitType PricePerUnitType { get; set; }
+    public Dictionary<string, object> Properties { get; set; } = [];
+    public List<ProductTypeDto> Types { get; set; } = [];
+}
---
+++ eShop.BlazorWebUI/Models/TypeModel.cs
@@ -0,0 +1,7 @@
+﻿namespace eShop.BlazorWebUI.Models;
+
+public class TypeModel
+{
+    public Guid? Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+}
