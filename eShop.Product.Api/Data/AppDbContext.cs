880669728c43a94bee245bfd207b80a315bdcba4
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-06-30 10:54:05 +0300

    Fix issue with missed product type entity registration

--- eShop.Product.Api/Data/AppDbContext.cs
+++ eShop.Product.Api/Data/AppDbContext.cs
@@ -4,11 +4,17 @@ namespace eShop.Product.Api.Data;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
+    public DbSet<ProductTypeEntity> Types { get; set; }
     public DbSet<ProductEntity> Products { get; set; }
     public DbSet<FruitProductEntity> Fruits { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
+        builder.Entity<ProductTypeEntity>(e =>
+        {
+            e.HasKey(x => x.Id);
+        });
+        
         builder.Entity<ProductEntity>(e =>
         {
             e.UseTptMappingStrategy();
@@ -18,7 +24,6 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
             
             e.Property(x => x.Name).HasMaxLength(128);
             e.Property(p => p.Price).HasPrecision(18, 4);
-            e.Property(x => x.ProductType).HasEnumConversion();
             e.Property(x => x.PricePerUnitType).HasEnumConversion();
             e.Property(x => x.UnitOfMeasure).HasEnumConversion();
         });
