42a677f7b0ca1dbc8874190a13aec97423b0d91b
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-06-30 10:54:42 +0300

    Replace product type enum with type id

--- eShop.Domain/Requests/API/Product/CreateProductRequest.cs
+++ eShop.Domain/Requests/API/Product/CreateProductRequest.cs
@@ -5,7 +5,7 @@ public class CreateProductRequest
     public string Name { get; set; } = string.Empty;
     public int QuantityInStock { get; set; }
     public decimal Price { get; set; }
-    public ProductType ProductType { get; set; }
+    public Guid TypeId { get; set; }
     public UnitOfMeasure UnitOfMeasure { get; set; }
     public PricePerUnitType PricePerUnitType { get; set; }
     public Dictionary<string, object> Properties { get; set; } = [];
--- eShop.Product.Api/Entities/ProductEntity.cs
+++ eShop.Product.Api/Entities/ProductEntity.cs
@@ -5,17 +5,16 @@ namespace eShop.Product.Api.Entities;
 public class ProductEntity : IEntity
 {
     public Guid Id { get; set; }
-
     public string Name { get; set; } = string.Empty;
     public long Article { get; set; }
-    
-    public ProductType ProductType { get; set; }
-    public UnitOfMeasure UnitOfMeasure { get; set; }
-    public PricePerUnitType PricePerUnitType { get; set; }
-    
     public decimal Price { get; set; }
     public int QuantityInStock { get; set; }
+    public Guid TypeId { get; set; }
+    public UnitOfMeasure UnitOfMeasure { get; set; }
+    public PricePerUnitType PricePerUnitType { get; set; }
 
     public DateTimeOffset? CreateDate { get; set; }
     public DateTimeOffset? UpdateDate { get; set; }
+
+    public ProductTypeEntity? Type { get; set; } 
 }
