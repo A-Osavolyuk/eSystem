32211b46bbd8243cd87f5bc1909b0fe408581e4f
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-06-30 14:46:21 +0300

    Fix issue with order of seed

--- eShop.Product.Api/Extensions/DbContextExtensions.cs
+++ eShop.Product.Api/Extensions/DbContextExtensions.cs
@@ -6,18 +6,18 @@ public static class DbContextExtensions
 {
     public static async Task SeedAsync(this AppDbContext context, CancellationToken cancellationToken = default)
     {
-        if (!await context.Types.AnyAsync(cancellationToken))
-        {
-            var seed = new TypeSeed();
-
-            await context.Types.AddRangeAsync(seed.Get(), cancellationToken);
-            await context.SaveChangesAsync(cancellationToken);
-        }
         if (!await context.Categories.AnyAsync(cancellationToken))
         {
             var seed = new CategorySeed();
 
             await context.Categories.AddRangeAsync(seed.Get(), cancellationToken);
+            await context.SaveChangesAsync(cancellationToken);
+        }
+        if (!await context.Types.AnyAsync(cancellationToken))
+        {
+            var seed = new TypeSeed();
+
+            await context.Types.AddRangeAsync(seed.Get(), cancellationToken);
             await context.SaveChangesAsync(cancellationToken);
         }
         if (!await context.Units.AnyAsync(cancellationToken))
