ca5d0e93adcc7ef5800fffc7b9b33baa9be91f98
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-06-30 10:47:22 +0300

    Add product type entity

---
+++ eShop.Product.Api/Entities/ProductTypeEntity.cs
@@ -0,0 +1,12 @@
+﻿using eShop.Domain.Abstraction.Data;
+
+namespace eShop.Product.Api.Entities;
+
+public class ProductTypeEntity : IEntity
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    
+    public DateTimeOffset? CreateDate { get; set; }
+    public DateTimeOffset? UpdateDate { get; set; }
+}
