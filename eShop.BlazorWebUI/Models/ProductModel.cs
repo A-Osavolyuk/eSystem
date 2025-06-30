8535f744f4d754b309fc756b2aca1c87e0137df6
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-06-30 16:35:02 +0300

    Implement basic functionality for create product page

--- eShop.BlazorWebUI/Models/ProductModel.cs
+++ eShop.BlazorWebUI/Models/ProductModel.cs
@@ -6,11 +6,13 @@ namespace eShop.BlazorWebUI.Models;
 public class ProductModel
 {
     public string Name { get; set; } = string.Empty;
-    public TypeModel Type { get; set; } = new();
     public decimal Price { get; set; }
     public int QuantityInStock { get; set; }
-    public UnitDto Unit { get; set; } = new();
-    public PriceTypeDto PriceType { get; set; } = new();
+    public TypeModel Type { get; set; } = new();
+    public UnitModel Unit { get; set; } = new();
+    public PriceTypeModel PriceType { get; set; } = new();
     public Dictionary<string, object> Properties { get; set; } = [];
     public List<TypeDto> Types { get; set; } = [];
+    public List<UnitDto> Units { get; set; } = [];
+    public List<PriceTypeDto> PriceTypes { get; set; } = [];
 }
