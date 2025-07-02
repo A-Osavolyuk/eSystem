5e263713f5a59a88febc91e820bd9c3efbe89b88
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-07-02 14:05:55 +0300

    Replace images/files handling

--- eShop.BlazorWebUI/Models/ProductModel.cs
+++ eShop.BlazorWebUI/Models/ProductModel.cs
@@ -1,8 +1,4 @@
-﻿using eShop.Domain.DTOs;
-using eShop.Domain.Enums;
-using Microsoft.AspNetCore.Components.Forms;
-
-namespace eShop.BlazorWebUI.Models;
+﻿namespace eShop.BlazorWebUI.Models;
 
 public class ProductModel
 {
@@ -14,6 +10,5 @@ public class ProductModel
     public PriceTypeModel PriceType { get; set; } = new();
     public CategoryModel Category { get; set; } = new();
     public Dictionary<string, object> Properties { get; set; } = [];
-    public List<IBrowserFile> Files { get; set; } = [];
-    public List<string> Images { get; set; } = [];
+    public List<ImageModel> Images { get; set; } = [];
 }
