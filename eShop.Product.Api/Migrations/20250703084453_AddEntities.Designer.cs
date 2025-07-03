606501bf055d808002539750db3b6b6aa921aa46
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-07-03 11:45:09 +0300

    Re-migrate DB

--- eShop.Product.Api/Migrations/20250703084453_AddEntities.Designer.cs
+++ eShop.Product.Api/Migrations/20250703084453_AddEntities.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eShop.Product.Api.Data;
 
@@ -11,9 +12,11 @@ using eShop.Product.Api.Data;
 namespace eShop.Product.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250703084453_AddEntities")]
+    partial class AddEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
