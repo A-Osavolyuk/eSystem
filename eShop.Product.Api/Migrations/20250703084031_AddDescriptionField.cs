c716f46ca5baf0704eac47711c260a00fa970047
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-07-03 11:40:49 +0300

    Re-migrate DB

---
+++ eShop.Product.Api/Migrations/20250703084031_AddDescriptionField.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eShop.Product.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDescriptionField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "nvarchar(3000)",
+                maxLength: 3000,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Products");
+        }
+    }
+}
