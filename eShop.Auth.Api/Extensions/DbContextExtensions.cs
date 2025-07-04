8727d593c7833cd136945b66c3bb5fd821b76e69
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-07-04 16:27:45 +0300

    Refactor seed structure

--- eShop.Auth.Api/Extensions/DbContextExtensions.cs
+++ eShop.Auth.Api/Extensions/DbContextExtensions.cs
@@ -9,88 +9,68 @@ public static class DbContextExtensions
         if (!await context.Providers.AnyAsync(cancellationToken))
         {
             var seed = new ProviderSeed();
-
             await context.Providers.AddRangeAsync(seed.Get(), cancellationToken);
-            await context.SaveChangesAsync(cancellationToken);
         }
         
         if (!await context.Users.AnyAsync(cancellationToken))
         {
             var seed = new UserSeed();
-
             await context.Users.AddRangeAsync(seed.Get(), cancellationToken);
-            await context.SaveChangesAsync(cancellationToken);
         }
         
         if (!await context.PersonalData.AnyAsync(cancellationToken))
         {
             var seed = new PersonalDataSeed();
-
             await context.PersonalData.AddRangeAsync(seed.Get(), cancellationToken);
-            await context.SaveChangesAsync(cancellationToken);
         }
 
         if (!await context.UserProvider.AnyAsync(cancellationToken))
         {
             var seed = new UserProviderSeed();
-            
             await context.UserProvider.AddRangeAsync(seed.Get(), cancellationToken);
-            await context.SaveChangesAsync(cancellationToken);
         }
         
         if (!await context.Resources.AnyAsync(cancellationToken))
         {
             var seed = new ResourceSeed();
-
             await context.Resources.AddRangeAsync(seed.Get(), cancellationToken);
-            await context.SaveChangesAsync(cancellationToken);
         }
 
         if (!await context.Permissions.AnyAsync(cancellationToken))
         {
             var seed = new PermissionSeed();
-
             await context.Permissions.AddRangeAsync(seed.Get(), cancellationToken);
-            await context.SaveChangesAsync(cancellationToken);
         }
                 
         if (!await context.Roles.AnyAsync(cancellationToken))
         {
             var seed = new RoleSeed();
-
             await context.Roles.AddRangeAsync(seed.Get(), cancellationToken);
-            await context.SaveChangesAsync(cancellationToken);
         }
                 
         if (!await context.UserRoles.AnyAsync(cancellationToken))
         {
             var seed = new UserRoleSeed();
-
             await context.UserRoles.AddRangeAsync(seed.Get(), cancellationToken);
-            await context.SaveChangesAsync(cancellationToken);
         }
                 
         if (!await context.UserPermissions.AnyAsync(cancellationToken))
         {
             var seed = new UserPermissionSeed();
-
             await context.UserPermissions.AddRangeAsync(seed.Get(), cancellationToken);
-            await context.SaveChangesAsync(cancellationToken);
         }
         
         if (!await context.RolePermissions.AnyAsync(cancellationToken))
         {
             var seed = new RolePermissionSeed();
-
             await context.RolePermissions.AddRangeAsync(seed.Get(), cancellationToken);
-            await context.SaveChangesAsync(cancellationToken);
         }
         
         if (!await context.LockoutState.AnyAsync(cancellationToken))
         {
             var seed = new LockoutSeed();
-
             await context.LockoutState.AddRangeAsync(seed.Get(), cancellationToken);
-            await context.SaveChangesAsync(cancellationToken);
         }
+        
+        await context.SaveChangesAsync(cancellationToken);
     }}
