7ea53c3ae75e1eec0749cccceb5787a128b86ab2
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-07-04 15:14:43 +0300

    Fix issue with suppliers website urls

--- eShop.Product.Api/Data/Seed/SupplierSeed.cs
+++ eShop.Product.Api/Data/Seed/SupplierSeed.cs
@@ -12,7 +12,7 @@ public class SupplierSeed : Seed<SupplierEntity>
                 Name = "Global Fresh Farms",
                 Email = "contact@globalfreshfarms.com",
                 PhoneNumber = "+1-555-123-4567",
-                WebsiteUrl = "https://www.globalfreshfarms.com",
+                WebsiteUrl = "https://globalfarms.com/",
                 Address = "123 Farm Lane, California, USA",
                 Description = "Supplier of fresh organic fruits and vegetables."
             },
@@ -22,7 +22,7 @@ public class SupplierSeed : Seed<SupplierEntity>
                 Name = "Green Valley Distributors",
                 Email = "sales@greenvalleydist.com",
                 PhoneNumber = "+44-20-7946-0123",
-                WebsiteUrl = "https://www.greenvalleydist.com",
+                WebsiteUrl = "https://www.greenvalleyintl.com/",
                 Address = "45 Distribution St, London, UK",
                 Description = "Distributor of imported fruits and vegetables in Europe."
             },
@@ -32,7 +32,7 @@ public class SupplierSeed : Seed<SupplierEntity>
                 Name = "Sunrise Agro Supply",
                 Email = "info@sunriseagro.com",
                 PhoneNumber = "+91-80-1234-5678",
-                WebsiteUrl = "https://www.sunriseagro.com",
+                WebsiteUrl = "https://sunrise-agro.com/",
                 Address = "78 Agro Park, Bangalore, India",
                 Description = "Supplier of fresh berries and organic vegetables."
             },
@@ -42,7 +42,7 @@ public class SupplierSeed : Seed<SupplierEntity>
                 Name = "Tropical Fruit Imports Ltd.",
                 Email = "contact@tropicalfruitimports.com",
                 PhoneNumber = "+61-2-9876-5432",
-                WebsiteUrl = "https://www.tropicalfruitimports.com",
+                WebsiteUrl = "https://tropifruit.co.uk/",
                 Address = "9 Harbor Road, Sydney, Australia",
                 Description = "Importer and supplier of tropical fruits."
             },
@@ -52,7 +52,7 @@ public class SupplierSeed : Seed<SupplierEntity>
                 Name = "Fresh Roots Cooperative",
                 Email = "support@freshrootscoop.org",
                 PhoneNumber = "+1-604-555-7890",
-                WebsiteUrl = "https://www.freshrootscoop.org",
+                WebsiteUrl = "https://freshroots.ca/",
                 Address = "456 Farm Road, Vancouver, Canada",
                 Description = "Cooperative of local farms supplying seasonal produce."
             }
