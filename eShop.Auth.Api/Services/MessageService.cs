5b5d0bd731d40e5e151198bdbd38aa3d3ff84830
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2025-07-13 11:04:13 +0300

    Simplify send of message

--- eShop.Auth.Api/Services/MessageService.cs
+++ eShop.Auth.Api/Services/MessageService.cs
@@ -1,6 +1,5 @@
 ﻿using eShop.Domain.Common.Messaging;
 using MassTransit;
-using Newtonsoft.Json;
 
 namespace eShop.Auth.Api.Services;
 
@@ -13,40 +12,16 @@ public sealed class MessageService(IBus bus) : IMessageService
     {
         var body = message.Build();
         var credentials = message.Credentials;
-
-        var queue = type switch
-        {
-            SenderType.Email => "email-message",
-            SenderType.Sms => "sms-message",
-            SenderType.Telegram => "telegram-message",
-            _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
-        };
         
         var request = new MessageRequest()
         {
             Type = type,
-            Queue = queue,
             Body = body,
             Credentials = credentials
         };
 
-        var address = ToQueueUri("unified-message");
+        var address = new Uri($"rabbitmq://localhost/unified-message");
         var endpoint = await bus.GetSendEndpoint(address);
         await endpoint.Send(request as object, cancellationToken);
-    }
-
-    private Uri ToQueueUri(string queueName) => new($"rabbitmq://localhost/{queueName}");
-
-    private Dictionary<string, string> ToDictionary(object obj)
-    {
-        if (obj is null)
-        {
-            throw new NullReferenceException("Cannot serialize null to dictionary");
-        }
-
-        var json = JsonConvert.SerializeObject(obj);
-        var dictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(json)!;
-
-        return dictionary;
     }
 }
