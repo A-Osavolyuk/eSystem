12bb556560840c00a6c2ef2f2beb1efff2701d2d
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2025-07-13 11:03:54 +0300

    Refactor message request

--- eShop.Domain/Common/Messaging/MessageRequest.cs
+++ eShop.Domain/Common/Messaging/MessageRequest.cs
@@ -2,8 +2,8 @@
 
 public class MessageRequest
 {
+    public Guid Id { get; set; }
     public SenderType Type { get; set; }
-    public required string Queue { get; set; }
     public required string Body { get; set; }
     public required Dictionary<string, string> Credentials { get; set; }
 }
