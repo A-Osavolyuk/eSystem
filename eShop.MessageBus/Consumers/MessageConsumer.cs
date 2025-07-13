d6eef9ee9bffd7fe2e49955f55b8aa2d1a9912f3
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2025-07-13 11:05:01 +0300

    Refactor message redirection via message bus

--- eShop.MessageBus/Consumers/MessageConsumer.cs
+++ eShop.MessageBus/Consumers/MessageConsumer.cs
@@ -1,45 +1,27 @@
 ﻿using eShop.Domain.Common.Messaging;
-using eShop.MessageBus.Bus;
+using eShop.Domain.Enums;
 using MassTransit;
-using Newtonsoft.Json;
 
 namespace eShop.MessageBus.Consumers;
 
-public class MessageConsumer(
-    MessageBusRegistry registry,
-    IBus bus) : IConsumer<MessageRequest>
+public class MessageConsumer(IBus bus) : IConsumer<MessageRequest>
 {
-    private readonly MessageBusRegistry registry = registry;
     private readonly IBus bus = bus;
 
     public async Task Consume(ConsumeContext<MessageRequest> context)
     {
-        // var request = context.Message;
-        // var queueName = $"{request.Type.ToString().ToLower()}:{request.Queue}";
-        // var configuration = registry.GetByQueueName(queueName);
-        //
-        // if (configuration is null)
-        // {
-        //     throw new NotSupportedException($"Queue {request.Queue} not supported");
-        // }
-        //
-        // var message = MapTo(configuration.MessageType, request.Payload, request.Credentials);
-        // await SendAsync(configuration.QueueName, message);
-    }
+        var request = context.Message;
 
-    private async Task SendAsync(string queue, object message, CancellationToken cancellationToken = default)
-    {
+        var queue = request.Type switch
+        {
+            SenderType.Email => "email-message",
+            SenderType.Sms => "sms-message",
+            SenderType.Telegram => "telegram-message",
+            _ => throw new ArgumentOutOfRangeException()
+        };
+
         var address = new Uri($"rabbitmq://localhost/{queue}");
         var endpoint = await bus.GetSendEndpoint(address);
-        await endpoint.Send(message, cancellationToken);
-    }
-
-    private static object MapTo(Type type, Dictionary<string, string> payload, Dictionary<string, string> credentials)
-    {
-        var combined = payload.ToDictionary(kvp => kvp.Key, object (kvp) => kvp.Value);
-        combined.Add("Credentials", credentials.ToDictionary(kvp => kvp.Key, object (kvp) => kvp.Value));
-        
-        var json = JsonConvert.SerializeObject(combined);
-        return JsonConvert.DeserializeObject(json, type)!;
+        await endpoint.Send(request);
     }
 }
