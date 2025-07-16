cd82bc1bf9c5937b2718097b532cf4fbeffc6f50
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-07-16 15:36:56 +0300

    Add base entity type

---
+++ eShop.Domain/Abstraction/Data/Entities/Entity.cs
@@ -0,0 +1,7 @@
+﻿namespace eShop.Domain.Abstraction.Data.Entities;
+
+public abstract class Entity
+{
+    public DateTimeOffset? CreateDate { get; set; }
+    public DateTimeOffset? UpdateDate { get; set; }
+}
