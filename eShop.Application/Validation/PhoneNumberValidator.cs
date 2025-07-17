bdea9a248090128b6b0987f00d52436a59faac58
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-07-17 14:59:04 +0300

    Refactor phone number pattern

--- eShop.Application/Validation/PhoneNumberValidator.cs
+++ eShop.Application/Validation/PhoneNumberValidator.cs
@@ -5,7 +5,7 @@ public class PhoneNumberValidator : AbstractValidator<string>
     public PhoneNumberValidator()
     {
         RuleFor(x => x)
-            .Matches(@"^\+\(\d{2}\)-\d{3}-\d{3}-\d{4}$|^\d{12}$").WithMessage("Wrong phone number format. Must be: +(xx)-xxx-xxx-xxxx")
+            .Matches(@"^\+\d{1,3}\s\(\d{3}\)-\d{3}-\d{4}$").WithMessage("Wrong phone number format. Must be: +xx (xxx)-xxx-xxxx")
             .NotEmpty().WithMessage("Field is must!");
     }
 }
