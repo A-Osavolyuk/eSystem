f23e77599b366af99483b00f3fbf7ecb59867408
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-07-18 15:47:48 +0300

    Add messages for sending rollback code on field changed/reset

---
+++ eShop.Auth.Api/Messages/Sms/PhoneNumberChangedMessage.cs
@@ -0,0 +1,13 @@
+﻿namespace eShop.Auth.Api.Messages.Sms;
+
+public class PhoneNumberChangedMessage : Message
+{
+    public override string Build()
+    {
+        return $"""
+                Your phone number has beed changed. 
+                If it was not you, please, enter code from below.
+                Rollback code: {Payload["Code"]}
+                """;
+    }
+}
