08287fe9b05c7698a9a3e307db68fa32a2cd99e0
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-07-18 16:17:11 +0300

    Set max length for rollback value property to 1000

--- eShop.Auth.Api/Data/AuthDbContext.cs
+++ eShop.Auth.Api/Data/AuthDbContext.cs
@@ -204,7 +204,7 @@ public sealed class AuthDbContext(DbContextOptions<AuthDbContext> options) : DbC
         {
             entity.HasKey(x => x.Id);
             
-            entity.Property(x => x.Value).HasMaxLength(64);
+            entity.Property(x => x.Value).HasMaxLength(1000);
             entity.Property(x => x.Code).HasMaxLength(64);
             entity.Property(x => x.Field).HasEnumConversion();
             
---
+++ eShop.Auth.Api/Migrations/20250718131551_ChangeValueMaxLength.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eShop.Auth.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeValueMaxLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                table: "Rollback",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(64)",
+                oldMaxLength: 64);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                table: "Rollback",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldMaxLength: 1000);
+        }
+    }
+}
