52ca8140f7df5400f815e408e4328f18e1a1c3f9
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-07-21 15:14:58 +0300

    Re-migrate DB

---
+++ eShop.Auth.Api/Migrations/20250721114546_RefactorEntities.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eShop.Auth.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefactorEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Code",
+                table: "RecoveryCodes");
+
+            migrationBuilder.DropColumn(
+                name: "Code",
+                table: "LoginCodes");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Hash",
+                table: "RecoveryCodes",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Hash",
+                table: "LoginCodes",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Hash",
+                table: "RecoveryCodes");
+
+            migrationBuilder.DropColumn(
+                name: "Hash",
+                table: "LoginCodes");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Code",
+                table: "RecoveryCodes",
+                type: "nvarchar(6)",
+                maxLength: 6,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Code",
+                table: "LoginCodes",
+                type: "nvarchar(6)",
+                maxLength: 6,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
