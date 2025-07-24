a963f5d13da17884e3e59511d54112ca6b2142c6
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-07-24 14:29:34 +0300

    Implement code generation functionality

---
+++ eShop.Auth.Api/Utilities/CodeGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography;
+
+namespace eShop.Auth.Api.Utilities;
+
+public static class CodeGenerator
+{
+    public static string Generate(int length)
+    {
+        var random = new Random();
+        var max = (int)Math.Pow(10, length);
+        var code = random.Next(1, max).ToString("D6");
+        
+        return code;
+    }
+}
