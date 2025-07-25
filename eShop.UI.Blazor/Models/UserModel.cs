17d2f53b53ca966faff2aa78235ef11e310e9d6d
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-07-25 15:51:30 +0300

    Refactor user model

--- eShop.UI.Blazor/Models/UserModel.cs
+++ eShop.UI.Blazor/Models/UserModel.cs
@@ -4,13 +4,20 @@ public class UserModel
 {
     public Guid Id { get; set; }
     public string Email { get; set; } = string.Empty;
-    public string UserName { get; set; } = string.Empty;
-    public string? PhoneNumber { get; set; } = string.Empty;
-    public bool? TwoFactorEnabled  { get; set; }
-    public string? AvatarUri { get; set; } = string.Empty;
-    public DateTimeOffset? PasswordChangeDate { get; set; }
     public DateTimeOffset? EmailChangeDate { get; set; }
-    public DateTimeOffset? PhoneNumberChangeDate { get; set; }
+    
+    public string? RecoveryEmail { get; set; } = string.Empty;
+    public DateTimeOffset? RecoveryEmailChangeDate { get; set; }
+    
+    public string UserName { get; set; } = string.Empty;
     public DateTimeOffset? UserNameChangeDate { get; set; }
+    
+    public string? PhoneNumber { get; set; } = string.Empty;
+    public DateTimeOffset? PhoneNumberChangeDate { get; set; }
+    
+    public bool? TwoFactorEnabled  { get; set; }
+    public DateTimeOffset? PasswordChangeDate { get; set; }
+    public string? AvatarUri { get; set; } = string.Empty;
+    
     public PersonalDataModel? PersonalData { get; set; }
 }
