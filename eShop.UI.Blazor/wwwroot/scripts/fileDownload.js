4840ee7b781e5c9a53dce56a71fbd4cd96842d0b
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-07-25 15:25:08 +0300

    Add file download script

---
+++ eShop.UI.Blazor/wwwroot/scripts/fileDownload.js
@@ -0,0 +1,8 @@
+﻿function downloadFile(fileName, base64Data, mimeType = "application/octet-stream") {
+    const link = document.createElement("a");
+    link.href = `data:${mimeType};base64,${base64Data}`;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
