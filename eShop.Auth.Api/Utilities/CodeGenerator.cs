7f176cc56b6c4f5f964a81d8e41783892b567158
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-07-28 11:12:37 +0300

    Implement multiple code generation

--- eShop.Auth.Api/Utilities/CodeGenerator.cs
+++ eShop.Auth.Api/Utilities/CodeGenerator.cs
@@ -4,7 +4,25 @@ namespace eShop.Auth.Api.Utilities;
 
 public static class CodeGenerator
 {
-    public static string Generate(int length)
+    public static string Generate(byte length)
+    {
+        var code = GenerateCode(length);
+        return code;
+    }
+
+    public static List<string> GenerateMany(byte length, byte amount = 10)
+    {
+        var codes = new List<string>();
+        for (var i = 0; i < amount; i++)
+        {
+            var code = GenerateCode(length);
+            codes.Add(code);
+        }
+        
+        return codes;
+    }
+
+    private static string GenerateCode(byte length)
     {
         var random = new Random();
         var max = (int)Math.Pow(10, length);
