5385cc12ecf52f0a04f5a5079334efb554307d2f
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-07-28 13:05:23 +0300

    Implement send 2FA code message on subscribe on provider

--- eShop.Auth.Api/Features/Providers/Commands/SubscribeProviderCommand.cs
+++ eShop.Auth.Api/Features/Providers/Commands/SubscribeProviderCommand.cs
@@ -1,4 +1,7 @@
-﻿using eShop.Domain.Requests.API.Auth;
+﻿using eShop.Auth.Api.Messages.Email;
+using eShop.Auth.Api.Messages.Sms;
+using eShop.Domain.Common.Security;
+using eShop.Domain.Requests.API.Auth;
 
 namespace eShop.Auth.Api.Features.Providers.Commands;
 
@@ -6,10 +9,14 @@ public record SubscribeProviderCommand(SubscribeProviderRequest Request) : IRequ
 
 public class SubscribeProviderCommandHandler(
     IUserManager userManager,
-    IProviderManager providerManager) : IRequestHandler<SubscribeProviderCommand, Result>
+    IProviderManager providerManager,
+    ILoginTokenManager loginTokenManager,
+    IMessageService messageService) : IRequestHandler<SubscribeProviderCommand, Result>
 {
     private readonly IUserManager userManager = userManager;
     private readonly IProviderManager providerManager = providerManager;
+    private readonly ILoginTokenManager loginTokenManager = loginTokenManager;
+    private readonly IMessageService messageService = messageService;
 
     public async Task<Result> Handle(SubscribeProviderCommand request, CancellationToken cancellationToken)
     {
@@ -29,6 +36,51 @@ public class SubscribeProviderCommandHandler(
         
         var result = await providerManager.SubscribeAsync(user, provider, cancellationToken);
 
-        return result;
+        if (!result.Succeeded)
+        {
+            return result;
+        }
+        
+        var code = await loginTokenManager.GenerateAsync(user, provider, cancellationToken);
+
+        var sender = provider.Name switch
+        {
+            ProviderTypes.Email => SenderType.Email,
+            ProviderTypes.Sms => SenderType.Sms,
+            _ => throw new NotSupportedException($"Provider type {provider.Name} is not supported.")
+        };
+
+        Message message = provider.Name switch
+        {
+            ProviderTypes.Email => new TwoFactorTokenEmailMessage()
+            {
+                Credentials = new ()
+                {
+                    { "To", user!.Email },
+                    { "Subject", "Two-factor authentication" }
+                }, 
+                Payload = new()
+                {
+                    { "UserName", user.UserName },
+                    { "Code", code },
+                },
+            },
+            ProviderTypes.Sms => new TwoFactorTokenSmsMessage()
+            {
+                Credentials = new ()
+                {
+                    { "PhoneNumber", user.PhoneNumber! },
+                }, 
+                Payload = new()
+                {
+                    { "Code", code },
+                },
+            },
+            _ => throw new NotSupportedException($"Provider type {provider.Name} is not supported.")
+        };
+        
+        await messageService.SendMessageAsync(sender, message, cancellationToken);
+
+        return Result.Success();
     }
 }
