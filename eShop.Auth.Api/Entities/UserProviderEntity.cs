5fae00386a7e8f317836728ca37cef574b159f35
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-07-29 10:42:47 +0300

    Re-migrate DB

--- eShop.Auth.Api/Entities/UserProviderEntity.cs
+++ eShop.Auth.Api/Entities/UserProviderEntity.cs
@@ -5,6 +5,10 @@ public class UserProviderEntity : Entity
     public Guid UserId { get; set; }
     public Guid ProviderId { get; set; }
     public bool Subscribed { get; set; }
+    
+    public DateTimeOffset? SubscribedDate { get; set; }
+    public DateTimeOffset? UnsubscribedDate { get; set; }
+    
     public UserEntity User { get; set; } = null!;
     public ProviderEntity Provider { get; set; } = null!;
 }
