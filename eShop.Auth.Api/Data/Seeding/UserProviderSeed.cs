07f088221c5c0f8057c9050dab366a9538cce5c7
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-07-29 10:46:47 +0300

    Refactor default user seeding

--- eShop.Auth.Api/Data/Seeding/UserProviderSeed.cs
+++ eShop.Auth.Api/Data/Seeding/UserProviderSeed.cs
@@ -13,7 +13,7 @@ public class UserProviderSeed : Seed<UserProviderEntity>
                 UserId = Guid.Parse("abb9d2ed-c3d2-4df9-ba88-eab018b95bc3"),
                 ProviderId = Guid.Parse("a4d155e3-1746-4f4d-af0e-e5ebca3ffcac"),
                 CreateDate = DateTimeOffset.UtcNow,
-                UpdateDate = null,
+                SubscribedDate = DateTimeOffset.UtcNow,
                 Subscribed = true
             },
             new UserProviderEntity()
@@ -21,7 +21,6 @@ public class UserProviderSeed : Seed<UserProviderEntity>
                 UserId = Guid.Parse("abb9d2ed-c3d2-4df9-ba88-eab018b95bc3"),
                 ProviderId = Guid.Parse("60c95fd9-c0e1-4a08-8d21-50c66f3a91d4"),
                 CreateDate = DateTimeOffset.UtcNow,
-                UpdateDate = null,
                 Subscribed = false
             },
             new UserProviderEntity()
@@ -29,7 +28,6 @@ public class UserProviderSeed : Seed<UserProviderEntity>
                 UserId = Guid.Parse("abb9d2ed-c3d2-4df9-ba88-eab018b95bc3"),
                 ProviderId = Guid.Parse("f01b5a5a-8f4b-4814-8ade-2935e02af0a5"),
                 CreateDate = DateTimeOffset.UtcNow,
-                UpdateDate = null,
                 Subscribed = false
             },
         ];
