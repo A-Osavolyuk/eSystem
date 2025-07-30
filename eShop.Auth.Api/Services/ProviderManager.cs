eec04e1d9cc4b4ce7aa353f59d99c2111f648a98
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-07-30 15:52:53 +0300

    Fix issue with logging 2FA providers unsubscribe

--- eShop.Auth.Api/Services/ProviderManager.cs
+++ eShop.Auth.Api/Services/ProviderManager.cs
@@ -64,7 +64,12 @@ public sealed class ProviderManager(AuthDbContext context) : IProviderManager
 
     public async ValueTask<Result> UnsubscribeAsync(UserEntity user, CancellationToken cancellationToken = default)
     {
-        user.Providers.ToList().ForEach(x => x.Subscribed = false);
+        user.Providers.ToList().ForEach(x =>
+        {
+            x.Subscribed = false;
+            x.UnsubscribedDate = DateTimeOffset.UtcNow;
+            x.UpdateDate = DateTimeOffset.UtcNow;
+        });
         
         context.UserProvider.UpdateRange(user.Providers);
         await context.SaveChangesAsync(cancellationToken);
