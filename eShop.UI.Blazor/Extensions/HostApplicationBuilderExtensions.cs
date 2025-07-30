c26890eb563a83c0975c8b7e848ab7bee8560d01
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-07-30 14:47:45 +0300

    Refactor routing, provide provider type as path param

--- eShop.UI.Blazor/Extensions/HostApplicationBuilderExtensions.cs
+++ eShop.UI.Blazor/Extensions/HostApplicationBuilderExtensions.cs
@@ -44,7 +44,7 @@ public static class HostApplicationBuilderExtensions
                 new() { Routes = ["/account/oauth/login"] },
                 new() { Routes = ["/account/2fa/login"] },
                 new() { Routes = ["/account/2fa/authenticator"], RequireAuthorization = true },
-                new() { Routes = ["/account/2fa/provider"], RequireAuthorization = true },
+                new() { Routes = ["/account/2fa/provider/{type}"], RequireAuthorization = true },
                 new() { Routes = ["/account/locked-out"], RequireAuthorization = true },
                 new() { Routes = ["/account/unlock"], RequireAuthorization = true },
                 new() { Routes = ["/account/register"] },
