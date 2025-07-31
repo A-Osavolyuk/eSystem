5bee01e1f801a42fb8b470b69ba7c68ed022306b
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-07-31 10:13:16 +0300

    Re-migrate DB

---
+++ eShop.Auth.Api/Migrations/20250731071213_RemoveRedundantFields.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eShop.Auth.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveRedundantFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SubscribedDate",
+                table: "UserProvider");
+
+            migrationBuilder.DropColumn(
+                name: "UnsubscribedDate",
+                table: "UserProvider");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "SubscribedDate",
+                table: "UserProvider",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "UnsubscribedDate",
+                table: "UserProvider",
+                type: "datetimeoffset",
+                nullable: true);
+        }
+    }
+}
