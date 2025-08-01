ee5be3271ba303a2e2a2831b008eac48587cb8c0
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-08-01 12:40:54 +0300

    Remove redundant option from sign in options

--- eShop.Auth.Api/Options/SignInOptions.cs
+++ eShop.Auth.Api/Options/SignInOptions.cs
@@ -10,5 +10,4 @@ public class SignInOptions
     public bool AllowUserNameLogin { get; set; } = true;
     public bool AllowEmailLogin { get; set; } = true;
     public int MaxFailedLoginAttempts { get; set; } = 5;
-    public TimeSpan LockoutDuration { get; set; } = TimeSpan.FromMinutes(15);
 }
