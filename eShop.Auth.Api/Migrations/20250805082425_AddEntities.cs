57cad2cfa7b6c50a677a91a2d155688fb2e3b0e5
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-08-05 11:24:47 +0300

    Re-migrate DB

---
+++ eShop.Auth.Api/Migrations/20250805082425_AddEntities.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eShop.Auth.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OAuthSessions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    ProviderId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IsSucceeded = table.Column<bool>(type: "bit", nullable: false),
+                    ErrorMessage = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: true),
+                    ErrorDescription = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: true),
+                    ErrorCode = table.Column<int>(type: "int", nullable: true),
+                    SignType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ExpiredDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    CreateDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    UpdateDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OAuthSessions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OAuthSessions_OAuthProviders_ProviderId",
+                        column: x => x.ProviderId,
+                        principalTable: "OAuthProviders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OAuthSessions_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OAuthSessions_ProviderId",
+                table: "OAuthSessions",
+                column: "ProviderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OAuthSessions_UserId",
+                table: "OAuthSessions",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OAuthSessions");
+        }
+    }
+}
