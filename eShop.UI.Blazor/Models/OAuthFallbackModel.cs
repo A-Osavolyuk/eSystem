14ffd1322c145751cceee2e76a20b08f609424be
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-08-06 16:21:43 +0300

    Implement UI for OAuth fallback page

---
+++ eShop.UI.Blazor/Models/OAuthFallbackModel.cs
@@ -0,0 +1,9 @@
+﻿using eShop.Domain.Enums;
+
+namespace eShop.BlazorWebUI.Models;
+
+public class OAuthFallbackModel
+{
+    public string Title { get; set; } = string.Empty;
+    public string? Message { get; set; }
+}
