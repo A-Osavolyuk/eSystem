ac9e335957f52ed405178d8841c1f448ae9ba863
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-08-06 12:25:44 +0300

    Refactor OAuth login command

--- eShop.Auth.Api/Features/Security/Commands/OAuthLoginCommand.cs
+++ eShop.Auth.Api/Features/Security/Commands/OAuthLoginCommand.cs
@@ -5,17 +5,19 @@ namespace eShop.Auth.Api.Features.Security.Commands;
 public sealed record OAuthLoginCommand(string Provider, string? ReturnUri) : IRequest<Result>;
 
 public sealed class OAuthLoginCommandHandler(
-    ISignInManager signInManager) : IRequestHandler<OAuthLoginCommand, Result>
+    ISignInManager signInManager,
+    IOAuthProviderManager providerManager) : IRequestHandler<OAuthLoginCommand, Result>
 {
     private readonly ISignInManager signInManager = signInManager;
+    private readonly IOAuthProviderManager providerManager = providerManager;
 
     public async Task<Result> Handle(OAuthLoginCommand request,
         CancellationToken cancellationToken)
     {
-        var providers = await signInManager.GetOAuthSchemasAsync(cancellationToken);
-        var validProvider = providers.Any(x => x == request.Provider);
+        var providers = await providerManager.GetAllAsync(cancellationToken);
+        var isValidProvider = providers.Any(x => x.Name == request.Provider);
 
-        if (!validProvider)
+        if (!isValidProvider)
         {
             return Results.BadRequest($"Invalid oauth provider {request.Provider}.");
         }
@@ -23,7 +25,7 @@ public sealed class OAuthLoginCommandHandler(
         var redirectUri = UrlGenerator.Action("handle", "OAuth",
             new { ReturnUri = request.ReturnUri ?? "/" });
         
-        var properties = signInManager.ConfigureOAuthProperties(request.Provider, redirectUri );;
+        var properties = signInManager.ConfigureOAuthProperties(request.Provider, redirectUri);
         
         var result = Result.Success(new OAuthLoginResponse()
         {
