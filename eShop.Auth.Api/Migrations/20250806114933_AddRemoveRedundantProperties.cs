dfde46f8bcee15dc34cad327706fd11ae722f5d5
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-08-06 15:13:41 +0300

    Re-migrate DB

---
+++ eShop.Auth.Api/Migrations/20250806114933_AddRemoveRedundantProperties.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eShop.Auth.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRemoveRedundantProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ErrorMessage",
+                table: "OAuthSessions");
+
+            migrationBuilder.DropColumn(
+                name: "ErrorType",
+                table: "OAuthSessions");
+
+            migrationBuilder.DropColumn(
+                name: "IsSucceeded",
+                table: "OAuthSessions");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ErrorMessage",
+                table: "OAuthSessions",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ErrorType",
+                table: "OAuthSessions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSucceeded",
+                table: "OAuthSessions",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
