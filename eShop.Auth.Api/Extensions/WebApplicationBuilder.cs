60a718abc01c43006645f9229efc9a5ac3b38c96
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-08-07 10:42:04 +0300

    Enable forwarded headers use

--- eShop.Auth.Api/Extensions/WebApplicationBuilder.cs
+++ eShop.Auth.Api/Extensions/WebApplicationBuilder.cs
@@ -1,10 +1,17 @@
-﻿namespace eShop.Auth.Api.Extensions;
+﻿using Microsoft.AspNetCore.HttpOverrides;
+
+namespace eShop.Auth.Api.Extensions;
 
 public static class WebApplicationBuilder
 {
     public static async Task MapApiServices(this WebApplication app)
     {
         app.MapDefaultEndpoints();
+        
+        app.UseForwardedHeaders(new ForwardedHeadersOptions
+        {
+            ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+        });
 
         app.MapOpenApi();
         app.MapScalarApiReference();
