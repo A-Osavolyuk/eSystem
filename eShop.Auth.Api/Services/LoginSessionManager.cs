057c7ff099a1252d911bc979e827c75a77824628
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-08-07 14:16:05 +0300

    Refactor login session creation

--- eShop.Auth.Api/Services/LoginSessionManager.cs
+++ eShop.Auth.Api/Services/LoginSessionManager.cs
@@ -15,23 +15,112 @@ public class LoginSessionManager(AuthDbContext context) : ILoginSessionManager
         var ipAddress = httpContext.Connection.RemoteIpAddress?.MapToIPv4().ToString()!;
         var clientInfo = parser.Parse(userAgent);
 
-        var entity = new LoginSessionEntity()
+        if (!await context.UserDevices.AnyAsync(x => x.UserId == user.Id, cancellationToken))
         {
-            Id = Guid.CreateVersion7(),
-            UserId = user.Id,
-            UserAgent = userAgent,
-            IpAddress = ipAddress,
-            Provider = provider,
-            Device = clientInfo.Device.Family,
-            Status = status,
-            Type = type,
-            Timestamp = DateTimeOffset.UtcNow,
-            CreateDate = DateTimeOffset.UtcNow
-        };
+            var device = new UserDeviceEntity()
+            {
+                Id = Guid.CreateVersion7(),
+                UserId = user.Id,
+                IsTrusted = true,
+                IpAddress = ipAddress,
+                UserAgent = userAgent,
+                Device = clientInfo.Device.ToString(),
+                OS = clientInfo.OS.ToString(),
+                Browser = clientInfo.UA.ToString(),
+                FirstSeen = DateTimeOffset.UtcNow,
+                LastSeen = DateTimeOffset.UtcNow,
+                CreateDate = DateTimeOffset.UtcNow
+            };
+
+            var session = new LoginSessionEntity()
+            {
+                Id = Guid.CreateVersion7(),
+                UserId = user.Id,
+                DeviceId = device.Id,
+                IpAddress = ipAddress,
+                UserAgent = userAgent,
+                Type = type,
+                Status = status,
+                Provider = provider,
+                Timestamp = DateTimeOffset.UtcNow,
+                CreateDate = DateTimeOffset.UtcNow
+            };
+            
+            await context.UserDevices.AddAsync(device, cancellationToken);
+            await context.LoginSessions.AddAsync(session, cancellationToken);
+            await context.SaveChangesAsync(cancellationToken);
         
-        await context.LoginSessions.AddAsync(entity, cancellationToken);
+            return Result.Success();
+        }
+        
+        var existedDevice = await context.UserDevices.FirstOrDefaultAsync(
+            x => x.UserId == user.Id 
+                 && x.IpAddress == ipAddress 
+                 && x.UserAgent == userAgent, cancellationToken);
+
+        if (existedDevice is null)
+        {
+            var device = new UserDeviceEntity()
+            {
+                Id = Guid.CreateVersion7(),
+                UserId = user.Id,
+                IsTrusted = false,
+                IpAddress = ipAddress,
+                UserAgent = userAgent,
+                Device = clientInfo.Device.ToString(),
+                OS = clientInfo.OS.ToString(),
+                Browser = clientInfo.UA.ToString(),
+                FirstSeen = DateTimeOffset.UtcNow,
+                LastSeen = DateTimeOffset.UtcNow,
+                CreateDate = DateTimeOffset.UtcNow
+            };
+            
+            var session = new LoginSessionEntity()
+            {
+                Id = Guid.CreateVersion7(),
+                UserId = user.Id,
+                DeviceId = device.Id,
+                IpAddress = ipAddress,
+                UserAgent = userAgent,
+                Type = type,
+                Status = status,
+                Provider = provider,
+                Timestamp = DateTimeOffset.UtcNow,
+                CreateDate = DateTimeOffset.UtcNow
+            };
+            
+            await context.UserDevices.AddAsync(device, cancellationToken);
+            await context.LoginSessions.AddAsync(session, cancellationToken);
+        }
+        else
+        {
+            if (!existedDevice.IsTrusted)
+            {
+                return Results.BadRequest("Device is not trusted");
+            }
+            
+            existedDevice.UpdateDate = DateTimeOffset.UtcNow;
+            existedDevice.LastSeen = DateTimeOffset.UtcNow;
+            
+            var session = new LoginSessionEntity()
+            {
+                Id = Guid.CreateVersion7(),
+                UserId = user.Id,
+                DeviceId = existedDevice.Id,
+                IpAddress = ipAddress,
+                UserAgent = userAgent,
+                Type = type,
+                Status = status,
+                Provider = provider,
+                Timestamp = DateTimeOffset.UtcNow,
+                CreateDate = DateTimeOffset.UtcNow
+            };
+            
+            context.UserDevices.Update(existedDevice);
+            await context.LoginSessions.AddAsync(session, cancellationToken);
+        }
+
         await context.SaveChangesAsync(cancellationToken);
-        
         return Result.Success();
     }
 }
