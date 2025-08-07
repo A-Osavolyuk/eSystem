bd9e368e794e6dd765190ace7b07bb9deec6333a
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-08-07 10:33:45 +0300

    Re-migrate DB

---
+++ eShop.Auth.Api/Migrations/20250807073331_RefactorSessionEntity.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eShop.Auth.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefactorSessionEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Provider",
+                table: "OAuthSessions");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ProviderId",
+                table: "OAuthSessions",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OAuthSessions_ProviderId",
+                table: "OAuthSessions",
+                column: "ProviderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OAuthSessions_UserId",
+                table: "OAuthSessions",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OAuthSessions_OAuthProviders_ProviderId",
+                table: "OAuthSessions",
+                column: "ProviderId",
+                principalTable: "OAuthProviders",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OAuthSessions_Users_UserId",
+                table: "OAuthSessions",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OAuthSessions_OAuthProviders_ProviderId",
+                table: "OAuthSessions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OAuthSessions_Users_UserId",
+                table: "OAuthSessions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OAuthSessions_ProviderId",
+                table: "OAuthSessions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OAuthSessions_UserId",
+                table: "OAuthSessions");
+
+            migrationBuilder.DropColumn(
+                name: "ProviderId",
+                table: "OAuthSessions");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Provider",
+                table: "OAuthSessions",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
