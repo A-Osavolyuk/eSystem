f6ce4ac3f3842915f38f40570dd57efc48a8c715
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-08-08 11:14:49 +0300

    Add password add dialog

---
+++ eShop.UI.Blazor/Validation/AddPasswordValidator.cs
@@ -0,0 +1,18 @@
+﻿using eShop.Application.Validation;
+using eShop.BlazorWebUI.Models;
+using eShop.Domain.Requests.API.Auth;
+
+namespace eShop.BlazorWebUI.Validation;
+
+public class AddPasswordValidator : Validator<AddPasswordModel>
+{
+    public AddPasswordValidator()
+    {
+        RuleFor(p => p.Password)
+            .SetValidator(new PasswordValidator());
+
+        RuleFor(p => p.ConfirmPassword)
+            .NotEmpty().WithMessage("You must confirm your password.")
+            .Equal(x => x.Password).WithMessage("Must be the same with new password.");
+    }
+}
