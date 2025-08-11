f1999c7b160db0255550a006c34de9816e5da3df
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-08-11 16:25:27 +0300

    Refactor login session flow

--- eShop.Auth.Api/Services/LoginSessionManager.cs
+++ eShop.Auth.Api/Services/LoginSessionManager.cs
@@ -10,109 +10,40 @@ public class LoginSessionManager(AuthDbContext context) : ILoginSessionManager
     {
         var userAgent = RequestUtils.GetUserAgent(httpContext);
         var ipAddress = RequestUtils.GetIpV4(httpContext);
-        var clientInfo = RequestUtils.GetClientInfo(httpContext);
-
-        if (!await context.UserDevices.AnyAsync(x => x.UserId == user.Id, cancellationToken))
-        {
-            var device = new UserDeviceEntity()
-            {
-                Id = Guid.CreateVersion7(),
-                UserId = user.Id,
-                IsTrusted = true,
-                IpAddress = ipAddress,
-                UserAgent = userAgent,
-                Device = clientInfo.Device.ToString(),
-                OS = clientInfo.OS.ToString(),
-                Browser = clientInfo.UA.ToString(),
-                FirstSeen = DateTimeOffset.UtcNow,
-                LastSeen = DateTimeOffset.UtcNow,
-                CreateDate = DateTimeOffset.UtcNow
-            };
-
-            var session = new LoginSessionEntity()
-            {
-                Id = Guid.CreateVersion7(),
-                DeviceId = device.Id,
-                IpAddress = ipAddress,
-                UserAgent = userAgent,
-                Type = type,
-                Status = status,
-                Provider = provider,
-                Timestamp = DateTimeOffset.UtcNow,
-                CreateDate = DateTimeOffset.UtcNow
-            };
-            
-            await context.UserDevices.AddAsync(device, cancellationToken);
-            await context.LoginSessions.AddAsync(session, cancellationToken);
-            await context.SaveChangesAsync(cancellationToken);
         
-            return Result.Success();
-        }
-        
-        var existedDevice = await context.UserDevices.FirstOrDefaultAsync(
+        var device = await context.UserDevices.FirstOrDefaultAsync(
             x => x.UserId == user.Id 
                  && x.IpAddress == ipAddress 
                  && x.UserAgent == userAgent, cancellationToken);
 
-        if (existedDevice is null)
+        if (device is null)
         {
-            var device = new UserDeviceEntity()
-            {
-                Id = Guid.CreateVersion7(),
-                UserId = user.Id,
-                IsTrusted = false,
-                IpAddress = ipAddress,
-                UserAgent = userAgent,
-                Device = clientInfo.Device.ToString(),
-                OS = clientInfo.OS.ToString(),
-                Browser = clientInfo.UA.ToString(),
-                FirstSeen = DateTimeOffset.UtcNow,
-                LastSeen = DateTimeOffset.UtcNow,
-                CreateDate = DateTimeOffset.UtcNow
-            };
-            
-            var session = new LoginSessionEntity()
-            {
-                Id = Guid.CreateVersion7(),
-                DeviceId = device.Id,
-                IpAddress = ipAddress,
-                UserAgent = userAgent,
-                Type = type,
-                Status = status,
-                Provider = provider,
-                Timestamp = DateTimeOffset.UtcNow,
-                CreateDate = DateTimeOffset.UtcNow
-            };
-            
-            await context.UserDevices.AddAsync(device, cancellationToken);
-            await context.LoginSessions.AddAsync(session, cancellationToken);
+            return Results.InternalServerError("Failed to create login session. Device not found.");
         }
-        else
+
+        if (!device.IsTrusted)
         {
-            if (!existedDevice.IsTrusted)
-            {
-                return Results.BadRequest("Device is not trusted");
-            }
-            
-            existedDevice.UpdateDate = DateTimeOffset.UtcNow;
-            existedDevice.LastSeen = DateTimeOffset.UtcNow;
-            
-            var session = new LoginSessionEntity()
-            {
-                Id = Guid.CreateVersion7(),
-                DeviceId = existedDevice.Id,
-                IpAddress = ipAddress,
-                UserAgent = userAgent,
-                Type = type,
-                Status = status,
-                Provider = provider,
-                Timestamp = DateTimeOffset.UtcNow,
-                CreateDate = DateTimeOffset.UtcNow
-            };
-            
-            context.UserDevices.Update(existedDevice);
-            await context.LoginSessions.AddAsync(session, cancellationToken);
+            return Results.BadRequest("Device is not trusted");
         }
+            
+        device.UpdateDate = DateTimeOffset.UtcNow;
+        device.LastSeen = DateTimeOffset.UtcNow;
+            
+        var session = new LoginSessionEntity()
+        {
+            Id = Guid.CreateVersion7(),
+            DeviceId = device.Id,
+            IpAddress = ipAddress,
+            UserAgent = userAgent,
+            Type = type,
+            Status = status,
+            Provider = provider,
+            Timestamp = DateTimeOffset.UtcNow,
+            CreateDate = DateTimeOffset.UtcNow
+        };
+            
+        context.UserDevices.Update(device);
+        await context.LoginSessions.AddAsync(session, cancellationToken);
 
         await context.SaveChangesAsync(cancellationToken);
         return Result.Success();
