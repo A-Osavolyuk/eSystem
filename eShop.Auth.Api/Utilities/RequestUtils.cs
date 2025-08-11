2800be07dfcce8134cac2533c36d40cbddbe46ca
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-08-11 15:47:47 +0300

    Add RequestUtils utility class

---
+++ eShop.Auth.Api/Utilities/RequestUtils.cs
@@ -0,0 +1,26 @@
+﻿using UAParser;
+
+namespace eShop.Auth.Api.Utilities;
+
+public static class RequestUtils
+{
+    public static string GetIpV4(HttpContext context)
+    {
+        var ipV4 = context.Connection.RemoteIpAddress?.MapToIPv4().ToString()!;
+        return ipV4;
+    }
+
+    public static string GetUserAgent(HttpContext context)
+    {
+        var userAgent = context.Request.Headers.UserAgent.ToString();
+        return userAgent;
+    }
+
+    public static ClientInfo GetClientInfo(HttpContext context)
+    {
+        var parser = Parser.GetDefault();
+        var userAgent = context.Request.Headers.UserAgent.ToString();
+        var clientInfo = parser.Parse(userAgent);
+        return clientInfo;
+    }
+}
