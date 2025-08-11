aa331a3dd3990ee6a09880060e626db15d463715
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-08-11 15:00:02 +0300

    Implement user device management

---
+++ eShop.Auth.Api/Interfaces/IDeviceManager.cs
@@ -0,0 +1,13 @@
+﻿using UAParser;
+
+namespace eShop.Auth.Api.Interfaces;
+
+public interface IDeviceManager
+{
+    public ValueTask<UserDeviceEntity?> FindAsync(UserEntity user, string userAgent, 
+        string ipAddress, CancellationToken cancellationToken = default);
+    public ValueTask<Result> TrustAsync(UserDeviceEntity device,  CancellationToken cancellationToken = default);
+    public ValueTask<Result> DistrustAsync(UserDeviceEntity device,  CancellationToken cancellationToken = default);
+    public ValueTask<Result> BlockAsync(UserDeviceEntity device,  CancellationToken cancellationToken = default);
+    public ValueTask<Result> UnblockAsync(UserDeviceEntity device,  CancellationToken cancellationToken = default);
+}
---
+++ eShop.Auth.Api/Services/DeviceManager.cs
@@ -0,0 +1,64 @@
+﻿namespace eShop.Auth.Api.Services;
+
+[Injectable(typeof(IDeviceManager), ServiceLifetime.Singleton)]
+public class DeviceManager(AuthDbContext context) : IDeviceManager
+{
+    private readonly AuthDbContext context = context;
+
+    public async ValueTask<UserDeviceEntity?> FindAsync(UserEntity user, string userAgent, 
+        string ipAddress, CancellationToken cancellationToken = default)
+    {
+        var device = await context.UserDevices.FirstOrDefaultAsync(
+            x => x.UserId == user.Id 
+                 && x.UserAgent == userAgent 
+                 && x.IpAddress == ipAddress, cancellationToken);
+
+        return device;
+    }
+
+    public async ValueTask<Result> TrustAsync(UserDeviceEntity device, CancellationToken cancellationToken = default)
+    {
+        device.IsTrusted = true;
+        device.UpdateDate = DateTimeOffset.UtcNow;
+        
+        context.UserDevices.Update(device);
+        await context.SaveChangesAsync(cancellationToken);
+
+        return Result.Success();
+    }
+
+    public async ValueTask<Result> DistrustAsync(UserDeviceEntity device, CancellationToken cancellationToken = default)
+    {
+        device.IsTrusted = false;
+        device.UpdateDate = DateTimeOffset.UtcNow;
+        
+        context.UserDevices.Update(device);
+        await context.SaveChangesAsync(cancellationToken);
+
+        return Result.Success();
+    }
+
+    public async ValueTask<Result> BlockAsync(UserDeviceEntity device, CancellationToken cancellationToken = default)
+    {
+        device.IsBlocked = true;
+        device.BlockedDate = DateTimeOffset.UtcNow;
+        device.UpdateDate = DateTimeOffset.UtcNow;
+        
+        context.UserDevices.Update(device);
+        await context.SaveChangesAsync(cancellationToken);
+
+        return Result.Success();
+    }
+
+    public async ValueTask<Result> UnblockAsync(UserDeviceEntity device, CancellationToken cancellationToken = default)
+    {
+        device.IsBlocked = false;
+        device.BlockedDate = null;
+        device.UpdateDate = DateTimeOffset.UtcNow;
+        
+        context.UserDevices.Update(device);
+        await context.SaveChangesAsync(cancellationToken);
+
+        return Result.Success();
+    }
+}
