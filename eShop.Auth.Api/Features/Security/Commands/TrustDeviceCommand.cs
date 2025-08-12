ac19dc476e1415ae75d797c625c2e724121535cf
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-08-12 11:07:08 +0300

    Implement device trust functionality

---
+++ eShop.Auth.Api/Features/Security/Commands/TrustDeviceCommand.cs
@@ -0,0 +1,58 @@
+﻿using eShop.Domain.Requests.API.Auth;
+using eShop.Domain.Responses.API.Auth;
+
+namespace eShop.Auth.Api.Features.Security.Commands;
+
+public record TrustDeviceCommand(TrustDeviceRequest Request) : IRequest<Result>;
+
+public class TrustDeviceCommandHandler(
+    IUserManager userManager,
+    ICodeManager codeManager,
+    ITokenManager tokenManager,
+    IDeviceManager deviceManager) : IRequestHandler<TrustDeviceCommand, Result>
+{
+    private readonly IUserManager userManager = userManager;
+    private readonly ICodeManager codeManager = codeManager;
+    private readonly ITokenManager tokenManager = tokenManager;
+    private readonly IDeviceManager deviceManager = deviceManager;
+
+    public async Task<Result> Handle(TrustDeviceCommand request, CancellationToken cancellationToken)
+    {
+        var user = await userManager.FindByIdAsync(request.Request.UserId, cancellationToken);
+        if (user is null) return Results.NotFound($"Cannot find user with ID {request.Request.UserId}.");
+
+        var code = request.Request.Code;
+        
+        var codeResult = await codeManager.VerifyAsync(user, code, SenderType.Email, 
+            CodeType.Trust, CodeResource.Device, cancellationToken);
+        
+        if (!codeResult.Succeeded) return codeResult;
+        
+        var device = await deviceManager.FindByIdAsync(request.Request.DeviceId, cancellationToken);
+        if (device is null) return Results.NotFound($"Cannot find user with ID {request.Request.DeviceId}.");
+        
+        var deviceResult = await deviceManager.TrustAsync(device, cancellationToken);
+        if (!deviceResult.Succeeded) return deviceResult;
+
+        if (user.Providers.Any(x => x.Subscribed))
+        {
+            return Result.Success(new TrustDeviceResponse()
+            {
+                IsTwoFactorEnabled = true,
+                UserId = user.Id
+            });
+        }
+        
+        var accessToken = await tokenManager.GenerateAsync(user, TokenType.Access,  cancellationToken);
+        var refreshToken = await tokenManager.GenerateAsync(user, TokenType.Refresh,  cancellationToken);
+
+        var response = new TrustDeviceResponse()
+        {
+            UserId = user.Id,
+            AccessToken = accessToken,
+            RefreshToken = refreshToken,
+        };
+        
+        return Result.Success(response);
+    }
+}
---
+++ eShop.Domain/Responses/API/Auth/TrustDeviceResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace eShop.Domain.Responses.API.Auth;
+
+public class TrustDeviceResponse
+{
+    public Guid UserId { get; set; }
+    public bool IsTwoFactorEnabled { get; set; }
+    
+    public string? AccessToken { get; set; }
+    public string? RefreshToken { get; set; }
+}
