1dc072929d0eaa41d0c9905412aff8d1754cf63c
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-08-14 15:11:20 +0300

    Refactor device verification flow

--- eShop.Auth.Api/Features/Devices/Commands/VerifyDeviceCommand.cs
+++ eShop.Auth.Api/Features/Devices/Commands/VerifyDeviceCommand.cs
@@ -1,4 +1,5 @@
-﻿using eShop.Domain.Requests.API.Auth;
+﻿using eShop.Auth.Api.Messages.Email;
+using eShop.Domain.Requests.API.Auth;
 
 namespace eShop.Auth.Api.Features.Devices.Commands;
 
@@ -6,10 +7,14 @@ public record VerifyDeviceCommand(VerifyDeviceRequest Request) : IRequest<Result
 
 public class VerifyDeviceCommandHandler(
     IUserManager userManager,
-    IDeviceManager deviceManager) : IRequestHandler<VerifyDeviceCommand, Result>
+    IDeviceManager deviceManager,
+    ICodeManager codeManager,
+    IMessageService messageService) : IRequestHandler<VerifyDeviceCommand, Result>
 {
     private readonly IUserManager userManager = userManager;
     private readonly IDeviceManager deviceManager = deviceManager;
+    private readonly ICodeManager codeManager = codeManager;
+    private readonly IMessageService messageService = messageService;
 
     public async Task<Result> Handle(VerifyDeviceCommand request, CancellationToken cancellationToken)
     {
@@ -19,7 +24,29 @@ public class VerifyDeviceCommandHandler(
         var device = await deviceManager.FindByIdAsync(request.Request.DeviceId, cancellationToken);
         if (device is null) return Results.NotFound($"Cannot find user with ID {request.Request.DeviceId}.");
         
-        var deviceResult = await deviceManager.TrustAsync(device, cancellationToken);
-        return deviceResult;
+        var code = await codeManager.GenerateAsync(user!, SenderType.Email, CodeType.Verify, 
+            CodeResource.Device, cancellationToken);
+        
+        var message = new VerifyDeviceMessage()
+        {
+            Credentials = new ()
+            {
+                { "To", user!.Email },
+                { "Subject", "Device verification" }
+            },
+            Payload = new()
+            {
+                { "Code", code },
+                { "UserName", user.UserName },
+                { "Ip", device.IpAddress! },
+                { "OS", device.OS! },
+                { "Device", device.Device! },
+                { "Browser", device.Browser! }
+            }
+        };
+
+        await messageService.SendMessageAsync(SenderType.Email, message, cancellationToken);
+
+        return Result.Success();
     }
 }
