89e8ec334025b32dd38c393a5d0f1b57eca43e40
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-08-14 15:00:00 +0300

    Refactor namespace issue with data seeds

--- eShop.Product.Api/Data/Seeding/CategorySeed.cs
+++ eShop.Product.Api/Data/Seeding/CategorySeed.cs
@@ -1,6 +1,6 @@
 ﻿using eShop.Domain.Abstraction.Data.Seeding;
 
-namespace eShop.Product.Api.Data.Seed;
+namespace eShop.Product.Api.Data.Seeding;
 
 public class CategorySeed : Seed<CategoryEntity>
 {
