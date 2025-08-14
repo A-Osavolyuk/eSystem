f30d1ffe7d21947d0b8a205992794aa8c0b0e9ce
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-08-14 15:10:57 +0300

    Refactor device block flow

--- eShop.Auth.Api/Features/Devices/Commands/BlockDeviceCommand.cs
+++ eShop.Auth.Api/Features/Devices/Commands/BlockDeviceCommand.cs
@@ -1,4 +1,5 @@
-﻿using eShop.Domain.Requests.API.Auth;
+﻿using eShop.Auth.Api.Messages.Email;
+using eShop.Domain.Requests.API.Auth;
 
 namespace eShop.Auth.Api.Features.Devices.Commands;
 
@@ -6,28 +7,46 @@ public record BlockDeviceCommand(BlockDeviceRequest Request) : IRequest<Result>;
 
 public class BlockDeviceCommandHandler(
     IUserManager userManager,
-    IDeviceManager deviceManager) : IRequestHandler<BlockDeviceCommand, Result>
+    IDeviceManager deviceManager,
+    ICodeManager codeManager,
+    IMessageService messageService) : IRequestHandler<BlockDeviceCommand, Result>
 {
     private readonly IUserManager userManager = userManager;
     private readonly IDeviceManager deviceManager = deviceManager;
+    private readonly ICodeManager codeManager = codeManager;
+    private readonly IMessageService messageService = messageService;
 
     public async Task<Result> Handle(BlockDeviceCommand request, CancellationToken cancellationToken)
     {
         var user = await userManager.FindByIdAsync(request.Request.UserId, cancellationToken);
-
-        if (user is null)
-        {
-            return Results.NotFound($"Cannot find user with ID {request.Request.UserId}.");
-        }
+        if (user is null) return Results.NotFound($"Cannot find user with ID {request.Request.UserId}.");
         
         var device = await deviceManager.FindByIdAsync(request.Request.DeviceId, cancellationToken);
+        if (device is null) return Results.NotFound($"Cannot find device with ID {request.Request.DeviceId}.");
         
-        if (device is null)
+        var code = await codeManager.GenerateAsync(user!, SenderType.Email, CodeType.Block, 
+            CodeResource.Device, cancellationToken);
+        
+        var message = new BlockDeviceMessage()
         {
-            return Results.NotFound($"Cannot find device with ID {request.Request.DeviceId}.");
-        }
-        
-        var result = await deviceManager.BlockAsync(device, cancellationToken);
-        return result;
+            Credentials = new ()
+            {
+                { "To", user!.Email },
+                { "Subject", "Device block" }
+            },
+            Payload = new()
+            {
+                { "Code", code },
+                { "UserName", user.UserName },
+                { "Ip", device.IpAddress! },
+                { "OS", device.OS! },
+                { "Device", device.Device! },
+                { "Browser", device.Browser! }
+            }
+        };
+
+        await messageService.SendMessageAsync(SenderType.Email, message, cancellationToken);
+
+        return Result.Success();
     }
 }
