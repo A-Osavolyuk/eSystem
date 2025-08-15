22eff47448a96084f793db1df223e429a2e9e511
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-08-15 11:05:53 +0300

    Fix issue with proxy route

--- eShop.Proxy/Extensions/HostApplicationBuilderExtensions.cs
+++ eShop.Proxy/Extensions/HostApplicationBuilderExtensions.cs
@@ -74,7 +74,7 @@ public static class HostApplicationBuilderExtensions
             },
             new RouteConfig
             {
-                RouteId = "providers-route", ClusterId = "security-cluster",
+                RouteId = "device-route", ClusterId = "security-cluster",
                 Match = new RouteMatch { Path = "/api/v1/Device/{**catch-all}" }
             },
             new RouteConfig
