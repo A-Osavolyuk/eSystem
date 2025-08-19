574bae15d4cdd4e95acef3572f36e477648dad1a
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-08-19 12:07:20 +0300

    Implement linked accounts management on client-side

--- eShop.Domain/Interfaces/Client/IOAuthService.cs
+++ eShop.Domain/Interfaces/Client/IOAuthService.cs
@@ -6,4 +6,7 @@ namespace eShop.Domain.Interfaces.Client;
 public interface IOAuthService
 {
     public ValueTask<Response> LoadSessionAsync(LoadOAuthSessionRequest request);
+    public ValueTask<Response> DisconnectAsync(DisconnectLinkedAccountRequest request);
+    public ValueTask<Response> AllowAsync(AllowLinkedAccountRequest request);
+    public ValueTask<Response> DisallowAsync(DisallowLinkedAccountRequest request);
 }
--- eShop.Infrastucture/Services/OAuthService.cs
+++ eShop.Infrastucture/Services/OAuthService.cs
@@ -6,9 +6,23 @@ using eShop.Domain.Requests.API.Auth;
 
 namespace eShop.Infrastructure.Services;
 
-public class OAuthService(IConfiguration configuration, IApiClient apiClient) : ApiService(configuration, apiClient), IOAuthService
+public class OAuthService(
+    IConfiguration configuration, 
+    IApiClient apiClient) : ApiService(configuration, apiClient), IOAuthService
 {
     public async ValueTask<Response> LoadSessionAsync(LoadOAuthSessionRequest request) => await ApiClient.SendAsync(
         new HttpRequest { Url = $"{Gateway}/api/v1/OAuth/load", Method = HttpMethod.Post, Data = request },
         new HttpOptions { WithBearer = false, Type = DataType.Text });
+
+    public async ValueTask<Response> DisconnectAsync(DisconnectLinkedAccountRequest request) => await ApiClient.SendAsync(
+        new HttpRequest { Url = $"{Gateway}/api/v1/OAuth/disconnect", Method = HttpMethod.Post, Data = request },
+        new HttpOptions { WithBearer = true, Type = DataType.Text });
+
+    public async ValueTask<Response> AllowAsync(AllowLinkedAccountRequest request) => await ApiClient.SendAsync(
+        new HttpRequest { Url = $"{Gateway}/api/v1/OAuth/allow", Method = HttpMethod.Post, Data = request },
+        new HttpOptions { WithBearer = true, Type = DataType.Text });
+
+    public async ValueTask<Response> DisallowAsync(DisallowLinkedAccountRequest request) => await ApiClient.SendAsync(
+        new HttpRequest { Url = $"{Gateway}/api/v1/OAuth/disallow", Method = HttpMethod.Post, Data = request },
+        new HttpOptions { WithBearer = true, Type = DataType.Text });
 }
