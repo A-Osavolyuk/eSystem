80ab833c3879f74f496288a885e41b5eff0c49c9
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-08-20 10:52:55 +0300

    Register script for WebAuthN

---
+++ eShop.UI.Blazor/wwwroot/scripts/webAuthN.js
@@ -0,0 +1,20 @@
+﻿function base64ToUint8Array(base64) {
+    const binary = atob(base64.replace(/-/g, '+').replace(/_/g, '/'));
+    const len = binary.length;
+    const bytes = new Uint8Array(len);
+    for (let i = 0; i < len; i++) {
+        bytes[i] = binary.charCodeAt(i);
+    }
+    return bytes;
+}
+
+async function attestate(optionsFromServer) {
+    optionsFromServer.challenge = base64ToUint8Array(optionsFromServer.challenge);
+    optionsFromServer.user.id = base64ToUint8Array(optionsFromServer.user.id);
+    
+    const credential = await navigator.credentials.create({
+        publicKey: optionsFromServer
+    });
+
+    console.log("Credential:", credential);
+}
