1612c9a36feb304418f2c9545651f2ac19952642
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-08-20 14:32:28 +0300

    Implement user credential managament

---
+++ eShop.Auth.Api/Interfaces/ICredentialManager.cs
@@ -0,0 +1,6 @@
+﻿namespace eShop.Auth.Api.Interfaces;
+
+public interface ICredentialManager
+{
+    public ValueTask<Result> CreateAsync(UserCredentialEntity entity, CancellationToken cancellationToken = default);
+}
---
+++ eShop.Auth.Api/Services/CredentialManager.cs
@@ -0,0 +1,15 @@
+﻿namespace eShop.Auth.Api.Services;
+
+[Injectable(typeof(ICredentialManager), ServiceLifetime.Scoped)]
+public class CredentialManager(AuthDbContext context) : ICredentialManager
+{
+    private readonly AuthDbContext context = context;
+
+    public async ValueTask<Result> CreateAsync(UserCredentialEntity entity, CancellationToken cancellationToken = default)
+    {
+        await context.UserCredentials.AddAsync(entity, cancellationToken);
+        await context.SaveChangesAsync(cancellationToken);
+        
+        return Result.Success();
+    }
+}
