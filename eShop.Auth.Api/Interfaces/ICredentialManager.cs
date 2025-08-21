8a8646996700603aa30ce0062492fc71dd70de3c
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-08-21 16:14:35 +0300

    Refactor credential manager

--- eShop.Auth.Api/Interfaces/ICredentialManager.cs
+++ eShop.Auth.Api/Interfaces/ICredentialManager.cs
@@ -2,6 +2,9 @@
 
 public interface ICredentialManager
 {
-    public ValueTask<UserCredentialEntity?> FindAsync(string credentialId, CancellationToken cancellationToken);
+    public ValueTask<UserCredentialEntity?> FindByCredentialIdAsync(string credentialId, CancellationToken cancellationToken);
+    public ValueTask<UserCredentialEntity?> FindByIdAsync(Guid id, CancellationToken cancellationToken);
     public ValueTask<Result> CreateAsync(UserCredentialEntity entity, CancellationToken cancellationToken = default);
+    public ValueTask<Result> UpdateAsync(UserCredentialEntity entity, CancellationToken cancellationToken = default);
+    public ValueTask<Result> DeleteAsync(UserCredentialEntity entity, CancellationToken cancellationToken = default);
 }
--- eShop.Auth.Api/Services/CredentialManager.cs
+++ eShop.Auth.Api/Services/CredentialManager.cs
@@ -5,10 +5,18 @@ public class CredentialManager(AuthDbContext context) : ICredentialManager
 {
     private readonly AuthDbContext context = context;
 
-    public async ValueTask<UserCredentialEntity?> FindAsync(string credentialId, CancellationToken cancellationToken)
+    public async ValueTask<UserCredentialEntity?> FindByCredentialIdAsync(string credentialId, CancellationToken cancellationToken)
     {
         var entity = await context.UserCredentials
             .FirstOrDefaultAsync(x => x.CredentialId == credentialId, cancellationToken);
+        
+        return entity;
+    }
+    
+    public async ValueTask<UserCredentialEntity?> FindByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var entity = await context.UserCredentials
+            .FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
         
         return entity;
     }
@@ -16,6 +24,22 @@ public class CredentialManager(AuthDbContext context) : ICredentialManager
     public async ValueTask<Result> CreateAsync(UserCredentialEntity entity, CancellationToken cancellationToken = default)
     {
         await context.UserCredentials.AddAsync(entity, cancellationToken);
+        await context.SaveChangesAsync(cancellationToken);
+        
+        return Result.Success();
+    }
+
+    public async ValueTask<Result> UpdateAsync(UserCredentialEntity entity, CancellationToken cancellationToken = default)
+    {
+        context.UserCredentials.Update(entity);
+        await context.SaveChangesAsync(cancellationToken);
+        
+        return Result.Success();
+    }
+
+    public async ValueTask<Result> DeleteAsync(UserCredentialEntity entity, CancellationToken cancellationToken = default)
+    {
+        context.UserCredentials.Remove(entity);
         await context.SaveChangesAsync(cancellationToken);
         
         return Result.Success();
