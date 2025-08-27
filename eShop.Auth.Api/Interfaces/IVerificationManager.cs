0dd6a76337e487f24c4a1420c9d1dd0200514e22
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-08-27 10:58:22 +0300

    Implement action verification management

---
+++ eShop.Auth.Api/Interfaces/IVerificationManager.cs
@@ -0,0 +1,10 @@
+﻿namespace eShop.Auth.Api.Interfaces;
+
+public interface IVerificationManager
+{
+    public ValueTask<Result> CreateAsync(UserEntity user, CodeResource resource, 
+        CodeType type, CancellationToken cancellationToken);
+    
+    public ValueTask<Result> VerifyAsync(UserEntity user, CodeResource resource, 
+        CodeType type, CancellationToken cancellationToken);
+}
---
+++ eShop.Auth.Api/Services/VerificationManager.cs
@@ -0,0 +1,38 @@
+﻿namespace eShop.Auth.Api.Services;
+
+[Injectable(typeof(IVerificationManager), ServiceLifetime.Scoped)]
+public class VerificationManager(AuthDbContext context) : IVerificationManager
+{
+    public async ValueTask<Result> CreateAsync(UserEntity user, 
+        CodeResource resource, CodeType type, CancellationToken cancellationToken)
+    {
+        var entity = new VerificationEntity()
+        {
+            Id = Guid.CreateVersion7(),
+            UserId = user.Id,
+            Resource = resource,
+            Type = type,
+            ExpireDate = DateTimeOffset.UtcNow.AddMinutes(10),
+            CreateDate = DateTimeOffset.UtcNow
+        };
+        
+        await context.Verifications.AddAsync(entity, cancellationToken);
+        await context.SaveChangesAsync(cancellationToken);
+
+        return Result.Success();
+    }
+
+    public async ValueTask<Result> VerifyAsync(UserEntity user, 
+        CodeResource resource, CodeType type, CancellationToken cancellationToken)
+    {
+        var entity = await context.Verifications.SingleOrDefaultAsync(
+                x => x.UserId == user.Id 
+                     && x.Resource == resource 
+                     && x.Type == type, cancellationToken);
+
+        if (entity is null) return Results.NotFound("Verification not found");
+        if (entity.ExpireDate < DateTimeOffset.Now) return Results.BadRequest("Verification is expired");
+        
+        return Result.Success();
+    }
+}
