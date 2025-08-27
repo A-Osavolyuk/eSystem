0f38b0b3ce417ba12734a66e2f8b208cbdfdefde
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-08-27 11:56:24 +0300

    Fix issue with missed verification entity creation after code been verified

--- eShop.Auth.Api/Features/Security/Commands/VerifyCodeCommand.cs
+++ eShop.Auth.Api/Features/Security/Commands/VerifyCodeCommand.cs
@@ -6,22 +6,27 @@ public record VerifyCodeCommand(VerifyCodeRequest Request) : IRequest<Result>;
 
 public class VerifyCodeCommandHandler(
     IUserManager userManager,
-    ICodeManager codeManager) : IRequestHandler<VerifyCodeCommand, Result>
+    ICodeManager codeManager,
+    IVerificationManager verificationManager) : IRequestHandler<VerifyCodeCommand, Result>
 {
     private readonly IUserManager userManager = userManager;
     private readonly ICodeManager codeManager = codeManager;
+    private readonly IVerificationManager verificationManager = verificationManager;
 
     public async Task<Result> Handle(VerifyCodeCommand request, CancellationToken cancellationToken)
     {
         var user = await userManager.FindByIdAsync(request.Request.UserId, cancellationToken);
         if (user is null) return Results.NotFound($"Cannot find user with ID {request.Request.UserId}.");
-        
+
         var code = request.Request.Code;
         var sender = request.Request.Sender;
         var type = request.Request.Type;
         var resource = request.Request.Resource;
-        
-        var result = await codeManager.VerifyAsync(user, code, sender, type, resource, cancellationToken);
+
+        var codeResult = await codeManager.VerifyAsync(user, code, sender, type, resource, cancellationToken);
+        if (!codeResult.Succeeded) return codeResult;
+
+        var result = await verificationManager.CreateAsync(user, resource, type, cancellationToken);
         return result;
     }
 }
