0d2b345f73375a1dbc3f9b6308c4cadc8276b81c
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-08-28 10:53:53 +0300

    Implement recovery code usage instead of verification code

--- eShop.Auth.Api/Features/Security/Commands/VerifyCodeCommand.cs
+++ eShop.Auth.Api/Features/Security/Commands/VerifyCodeCommand.cs
@@ -7,10 +7,12 @@ public record VerifyCodeCommand(VerifyCodeRequest Request) : IRequest<Result>;
 public class VerifyCodeCommandHandler(
     IUserManager userManager,
     ICodeManager codeManager,
+    IRecoverManager recoverManager,
     IVerificationManager verificationManager) : IRequestHandler<VerifyCodeCommand, Result>
 {
     private readonly IUserManager userManager = userManager;
     private readonly ICodeManager codeManager = codeManager;
+    private readonly IRecoverManager recoverManager = recoverManager;
     private readonly IVerificationManager verificationManager = verificationManager;
 
     public async Task<Result> Handle(VerifyCodeCommand request, CancellationToken cancellationToken)
@@ -24,7 +26,11 @@ public class VerifyCodeCommandHandler(
         var resource = request.Request.Resource;
 
         var codeResult = await codeManager.VerifyAsync(user, code, sender, type, resource, cancellationToken);
-        if (!codeResult.Succeeded) return codeResult;
+        if (!codeResult.Succeeded)
+        {
+            var recoverCodeResult = await recoverManager.VerifyAsync(user, code, cancellationToken);
+            if (!recoverCodeResult.Succeeded) return codeResult;
+        }
 
         var result = await verificationManager.CreateAsync(user, resource, type, cancellationToken);
         return result;
