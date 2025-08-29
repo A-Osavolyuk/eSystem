8112f3b5be23bec52ea6b176fb2b5a651a26e01e
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-08-29 13:09:42 +0300

    Implement recovery email reset functionality

---
+++ eShop.Domain/Requests/API/Auth/ResetRecoveryEmailRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace eShop.Domain.Requests.API.Auth;
+
+public class ResetRecoveryEmailRequest
+{
+    public Guid UserId { get; set; }
+    public string NewRecoveryEmail { get; set; } = string.Empty;
+}
