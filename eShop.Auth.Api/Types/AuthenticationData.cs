e2c9f2fd17dde2618631ac4784957e3dae3f0d03
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-09-03 16:17:16 +0300

    Fix issue with invalid sign count parsing

--- eShop.Auth.Api/Types/AuthenticationData.cs
+++ eShop.Auth.Api/Types/AuthenticationData.cs
@@ -1,4 +1,7 @@
-﻿namespace eShop.Auth.Api.Types;
+﻿using System.Buffers.Binary;
+using PeterO.Cbor;
+
+namespace eShop.Auth.Api.Types;
 
 public class AuthenticationData
 {
@@ -9,21 +12,24 @@ public class AuthenticationData
     public required byte[] CredentialId { get; set; }
     public required byte[] CredentialPublicKey { get; set; }
 
-    public static AuthenticationData FromBytes(byte[] bytes)
+    public static AuthenticationData Parse(string attestationObject)
     {
+        var attestationBytes = CredentialUtils.Base64UrlDecode(attestationObject);
+        var attestationCbor = CBORObject.DecodeFromBytes(attestationBytes);
+        var authDataBytes = attestationCbor["authData"].GetByteString();
+        
         var offset = 0;
 
         // RP ID hash (32 bytes)
-        var rpIdHash = bytes.Skip(offset).Take(32).ToArray();
+        var rpIdHash = authDataBytes.Skip(offset).Take(32).ToArray();
         offset += 32;
 
         // Flags (1 byte)
-        var flags = bytes[offset];
+        var flags = authDataBytes[offset];
         offset += 1;
 
         // SignCount (4 bytes, big-endian)
-        var signCountBytes = bytes.Skip(offset).Take(4).Reverse().ToArray();
-        var signCount = BitConverter.ToUInt32(signCountBytes, 0);
+        var signCount = BinaryPrimitives.ReadUInt32BigEndian(authDataBytes.AsSpan(offset, 4));
         offset += 4;
 
         // Credential data exists flag
@@ -32,19 +38,19 @@ public class AuthenticationData
             throw new Exception("No credential data in authData");
 
         // AAGUID (16 bytes)
-        var aaGuid = bytes.Skip(offset).Take(16).ToArray();
+        var aaGuid = authDataBytes.Skip(offset).Take(16).ToArray();
         offset += 16;
 
         // Credential ID length (2 bytes, big-endian)
-        var credentialIdLength = (bytes[offset] << 8) | bytes[offset + 1];
+        var credentialIdLength = (authDataBytes[offset] << 8) | authDataBytes[offset + 1];
         offset += 2;
 
         // Credential ID
-        var credentialId = bytes.Skip(offset).Take(credentialIdLength).ToArray();
+        var credentialId = authDataBytes.Skip(offset).Take(credentialIdLength).ToArray();
         offset += credentialIdLength;
 
         // Credential public key (remaining bytes)
-        var credentialPublicKey = bytes.Skip(offset).ToArray();
+        var credentialPublicKey = authDataBytes.Skip(offset).ToArray();
 
         return new AuthenticationData
         {
