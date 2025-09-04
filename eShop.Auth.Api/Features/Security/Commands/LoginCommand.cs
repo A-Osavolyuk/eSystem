484ea96001f92803d110581d0c271baf1d923ca6
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-09-04 14:30:43 +0300

    Refactor login command

--- eShop.Auth.Api/Features/Security/Commands/LoginCommand.cs
+++ eShop.Auth.Api/Features/Security/Commands/LoginCommand.cs
@@ -26,6 +26,7 @@ public sealed class LoginCommandHandler(
     public async Task<Result> Handle(LoginCommand request, CancellationToken cancellationToken)
     {
         UserEntity? user = null;
+        LoginResponse? response;
 
         if (identityOptions.SignIn.AllowUserNameLogin)
         {
@@ -67,83 +68,9 @@ public sealed class LoginCommandHandler(
             if (!result.Succeeded) return result;
         }
 
-        var emailResult = CheckEmail(user);
-        if (!emailResult.Succeeded) return emailResult;
-
-        var lockoutState = await lockoutManager.FindAsync(user, cancellationToken);
-        if (lockoutState.Enabled) return LockedOut(user, lockoutState);
-
-        var passwordResult = await CheckPasswordAsync(user, request.Request.Password, cancellationToken);
-        if (!passwordResult.Succeeded) return passwordResult;
-
-        if (user.FailedLoginAttempts > 0)
-        {
-            var resetResult = await ResetFailedLoginAttemptsAsync(user, cancellationToken);
-            if (!resetResult.Succeeded) return resetResult;
-        }
-
-        if (identityOptions.SignIn.RequireTrustedDevice)
-        {
-            var deviceResult = CheckDevice(user, device);
-            if (!deviceResult.Succeeded) return deviceResult;
-        }
-
-        if (user.HasTwoFactor()) return CheckTwoFactorState(user, lockoutState);
-
-        var tokenResult = await GenerateTokensAsync(user, lockoutState, device, cancellationToken);
-        return tokenResult;
-    }
-
-    private Result CheckDevice(UserEntity user, UserDeviceEntity device)
-    {
-        if (device.IsBlocked)
-        {
-            var response = new LoginResponse()
-            {
-                UserId = user.Id,
-                Email = user.Email,
-                IsTrustedDevice = device.IsTrusted,
-                IsBlockedDevice = device.IsBlocked,
-                DeviceId = device.Id
-            };
-
-            return Results.BadRequest("Cannot sign in, device is blocked.", response);
-        }
-
-        if (!device.IsTrusted)
-        {
-            var response = new LoginResponse()
-            {
-                UserId = user.Id,
-                Email = user.Email,
-                IsTrustedDevice = device.IsTrusted,
-                IsBlockedDevice = device.IsBlocked,
-                DeviceId = device.Id
-            };
-
-            return Results.BadRequest("You need to trust this device before sign in.", response);
-        }
-
-        return Result.Success();
-    }
-
-    private Result LockedOut(UserEntity user, LockoutStateEntity lockoutState)
-    {
-        var response = new LoginResponse()
-        {
-            UserId = user.Id,
-            IsLockedOut = lockoutState.Enabled,
-            Reason = Mapper.Map(lockoutState.Reason),
-        };
-
-        return Results.BadRequest("Account is locked out", response);
-    }
-
-    private Result CheckEmail(UserEntity user)
-    {
         if (identityOptions.SignIn.RequireConfirmedEmail && !user.EmailConfirmed)
         {
-            var response = new LoginResponse()
+            response = new LoginResponse()
             {
                 UserId = user.Id,
                 Email = user.Email,
@@ -153,81 +80,118 @@ public sealed class LoginCommandHandler(
             return Results.BadRequest("The email address is not confirmed.", response);
         }
 
-        return Result.Success();
-    }
+        var lockoutState = await lockoutManager.FindAsync(user, cancellationToken);
+        if (lockoutState.Enabled)
+        {
+            response = new LoginResponse()
+            {
+                UserId = user.Id,
+                IsLockedOut = lockoutState.Enabled,
+                Reason = Mapper.Map(lockoutState.Reason),
+            };
 
-    private async Task<Result> CheckPasswordAsync(UserEntity user, string password, CancellationToken cancellationToken)
-    {
-        if (!user.HasPassword()) return Results.BadRequest("Password was not provided.");
+            return Results.BadRequest("Account is locked out", response);
+        }
 
-        var isValidPassword = await userManager.CheckPasswordAsync(user, password, cancellationToken);
-        if (isValidPassword) return Result.Success();
+        if (!user.HasPassword()) return Results.BadRequest("User doesn't have a password.");
 
-        user.FailedLoginAttempts += 1;
+        var isValidPassword = await userManager.CheckPasswordAsync(user, request.Request.Password, cancellationToken);
+        if (!isValidPassword)
+        {
+            user.FailedLoginAttempts += 1;
 
-        var updateResult = await userManager.UpdateAsync(user, cancellationToken);
-        if (!updateResult.Succeeded) return updateResult;
+            var updateResult = await userManager.UpdateAsync(user, cancellationToken);
+            if (!updateResult.Succeeded) return updateResult;
 
-        if (user.FailedLoginAttempts < identityOptions.SignIn.MaxFailedLoginAttempts)
-            return MaxFailedLoginAttempts(user);
+            if (user.FailedLoginAttempts < identityOptions.SignIn.MaxFailedLoginAttempts)
+            {
+                response = new LoginResponse()
+                {
+                    FailedLoginAttempts = user.FailedLoginAttempts,
+                    MaxFailedLoginAttempts = identityOptions.SignIn.MaxFailedLoginAttempts,
+                    IsLockedOut = false,
+                    UserId = user.Id,
+                };
 
-        var reason = await reasonManager.FindByTypeAsync(LockoutType.TooManyFailedLoginAttempts, cancellationToken);
-        if (reason is null)
-            return Results.NotFound($"Cannot find lockout type {LockoutType.TooManyFailedLoginAttempts}.");
+                return Results.BadRequest("The password is not valid.", response);
+            }
 
-        var lockoutResult = await lockoutManager.LockoutAsync(user, reason,
-            permanent: true, cancellationToken: cancellationToken);
+            var reason = await reasonManager.FindByTypeAsync(LockoutType.TooManyFailedLoginAttempts, cancellationToken);
+            if (reason is null)
+                return Results.NotFound($"Cannot find lockout type {LockoutType.TooManyFailedLoginAttempts}.");
 
-        if (!lockoutResult.Succeeded) return lockoutResult;
+            var lockoutResult = await lockoutManager.LockoutAsync(user, reason,
+                permanent: true, cancellationToken: cancellationToken);
 
-        return Results.BadRequest("Account is locked out due to too many failed login attempts",
-            new LoginResponse()
+            if (!lockoutResult.Succeeded) return lockoutResult;
+
+            response = new LoginResponse()
             {
                 UserId = user.Id,
                 IsLockedOut = true,
                 FailedLoginAttempts = user.FailedLoginAttempts,
                 MaxFailedLoginAttempts = identityOptions.SignIn.MaxFailedLoginAttempts,
                 Reason = Mapper.Map(reason)
-            });
-    }
+            };
+        
+            return Results.BadRequest("Account is locked out due to too many failed login attempts", response);
+        }
 
-    private Result MaxFailedLoginAttempts(UserEntity user)
-    {
-        var response = new LoginResponse()
+        if (user.FailedLoginAttempts > 0)
         {
-            FailedLoginAttempts = user.FailedLoginAttempts,
-            MaxFailedLoginAttempts = identityOptions.SignIn.MaxFailedLoginAttempts,
-            IsLockedOut = false,
-            UserId = user.Id,
-        };
+            user.FailedLoginAttempts = 0;
 
-        return Results.BadRequest("The password is not valid.", response);
-    }
+            var userUpdateResult = await userManager.UpdateAsync(user, cancellationToken);
+            if (!userUpdateResult.Succeeded) return userUpdateResult;
+        }
 
-    private async Task<Result> ResetFailedLoginAttemptsAsync(UserEntity user, CancellationToken cancellationToken)
-    {
-        user.FailedLoginAttempts = 0;
-        var result = await userManager.UpdateAsync(user, cancellationToken);
-        return result;
-    }
-
-    private Result CheckTwoFactorState(UserEntity user, LockoutStateEntity lockoutState)
-    {
-        return Result.Success(new LoginResponse()
+        if (identityOptions.SignIn.RequireTrustedDevice)
         {
-            IsLockedOut = lockoutState.Enabled,
-            IsTwoFactorEnabled = true,
-            UserId = user.Id
-        });
-    }
+            if (device.IsBlocked)
+            {
+                response = new LoginResponse()
+                {
+                    UserId = user.Id,
+                    Email = user.Email,
+                    IsTrustedDevice = device.IsTrusted,
+                    IsBlockedDevice = device.IsBlocked,
+                    DeviceId = device.Id
+                };
 
-    private async Task<Result> GenerateTokensAsync(UserEntity user, LockoutStateEntity lockoutState,
-        UserDeviceEntity device, CancellationToken cancellationToken)
-    {
+                return Results.BadRequest("Cannot sign in, device is blocked.", response);
+            }
+
+            if (!device.IsTrusted)
+            {
+                response = new LoginResponse()
+                {
+                    UserId = user.Id,
+                    Email = user.Email,
+                    IsTrustedDevice = device.IsTrusted,
+                    IsBlockedDevice = device.IsBlocked,
+                    DeviceId = device.Id
+                };
+
+                return Results.BadRequest("You need to trust this device before sign in.", response);
+            }
+        }
+
+        if (user.HasTwoFactor())
+        {
+            response = new LoginResponse()
+            {
+                IsLockedOut = lockoutState.Enabled,
+                IsTwoFactorEnabled = true,
+                UserId = user.Id
+            };
+
+            return Result.Success(response);
+        }
+        
         var accessToken = await tokenManager.GenerateAsync(user, TokenType.Access, cancellationToken);
         var refreshToken = await tokenManager.GenerateAsync(user, TokenType.Refresh, cancellationToken);
 
-        var response = new LoginResponse()
+        response = new LoginResponse()
         {
             AccessToken = accessToken,
             RefreshToken = refreshToken,
