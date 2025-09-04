53556bd56ed94b6424c54850423ffe8e938b01ac
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-09-04 10:39:43 +0300

    Refactor passkey management to PasskeyManager

--- eShop.Blazor.Infrastructure/Extensions.cs
+++ eShop.Blazor.Infrastructure/Extensions.cs
@@ -55,6 +55,7 @@ public static class Extensions
         builder.Services.AddCascadingAuthenticationState();
         builder.Services.AddScoped<TokenHandler>();
         builder.Services.AddScoped<AuthenticationManager>();
+        builder.Services.AddScoped<PasskeyManager>();
         builder.Services.AddScoped<JwtAuthenticationStateProvider>();
         builder.Services.AddScoped<AuthenticationStateProvider, JwtAuthenticationStateProvider>();
     }
