64a0aab142082978906f1bbff9224ec851300e6b
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-09-05 14:05:18 +0300

    Re-migrate DB

--- eShop.Auth.Api/Data/AuthDbContext.cs
+++ eShop.Auth.Api/Data/AuthDbContext.cs
@@ -314,5 +314,10 @@ public sealed class AuthDbContext(DbContextOptions<AuthDbContext> options) : DbC
                 .WithMany()
                 .HasForeignKey(x => x.UserId);
         });
+
+        builder.Entity<VerificationProviderEntity>(entity =>
+        {
+            entity.HasKey(x => x.Id);
+        });
     }
 }
---
+++ eShop.Auth.Api/Migrations/20250905110503_FixEntityIssue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eShop.Auth.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixEntityIssue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VerificationProviderEntity",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreateDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    UpdateDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VerificationProviderEntity", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VerificationProviderEntity");
+        }
+    }
+}
