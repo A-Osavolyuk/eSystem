619ca9bc864c3bfe6bca3c5d811d099d98527ac7
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-09-08 15:39:20 +0300

    Add seeding for user email entities

---
+++ eShop.Auth.Api/Data/Seeding/UserEmailSeed.cs
@@ -0,0 +1,25 @@
+﻿using eShop.Domain.Abstraction.Data.Seeding;
+
+namespace eShop.Auth.Api.Data.Seeding;
+
+public class UserEmailSeed : Seed<UserEmailEntity>
+{
+    public override List<UserEmailEntity> Get()
+    {
+        return
+        [
+            new UserEmailEntity()
+            {
+                Id = Guid.CreateVersion7(),
+                UserId = Guid.Parse("abb9d2ed-c3d2-4df9-ba88-eab018b95bc3"),
+                Email = "sasha.osavolll111@gmail.com",
+                NormalizedEmail = "sasha.osavolll111@gmail.com".ToUpperInvariant(),
+                IsPrimary = true,
+                IsVerified = true,
+                CreateDate = DateTimeOffset.UtcNow,
+                VerifiedDate = DateTimeOffset.UtcNow,
+                PrimaryDate = DateTimeOffset.UtcNow,
+            }
+        ];
+    }
+}
