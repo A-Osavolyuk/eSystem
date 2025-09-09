258ce9694e7aa646552e8b9a8cf5e6f1c110a4bc
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-09-09 10:53:21 +0300

    Refactor email add flow

--- eShop.Auth.Api/Features/Security/Commands/AddEmailCommand.cs
+++ eShop.Auth.Api/Features/Security/Commands/AddEmailCommand.cs
@@ -5,22 +5,31 @@ namespace eShop.Auth.Api.Features.Security.Commands;
 public record AddEmailCommand(AddEmailRequest Request) : IRequest<Result>;
 
 public class AddEmailCommandHandler(
-    IUserManager userManager) : IRequestHandler<AddEmailCommand, Result>
+    IUserManager userManager,
+    IdentityOptions identityOptions) : IRequestHandler<AddEmailCommand, Result>
 {
     private readonly IUserManager userManager = userManager;
+    private readonly IdentityOptions identityOptions = identityOptions;
 
     public async Task<Result> Handle(AddEmailCommand request, CancellationToken cancellationToken)
     {
         var user = await userManager.FindByIdAsync(request.Request.UserId, cancellationToken);
         if (user is null) return Results.NotFound($"Cannot find user with ID {request.Request.UserId}.");
 
-        if (user.HasEmail()) return Results.BadRequest("User already has an email address.");
-
-        var taken = await userManager.IsEmailTakenAsync(request.Request.Email, cancellationToken);
-        if (taken) return Results.BadRequest("Email already taken.");
-        
-        if(request.Request is { IsRecovery: true, IsPrimary: true }) 
+        if (request.Request is { IsRecovery: true, IsPrimary: true })
             return Results.BadRequest("Same email cannot be primary and recovery at the same time.");
+        
+        if (user.Emails.Count(x => x.IsPrimary) >= 1 && request.Request.IsPrimary)
+            return Results.BadRequest("User already has a primary email.");
+
+        if (user.Emails.Count(x => x.IsRecovery) >= 1 && request.Request.IsRecovery)
+            return Results.BadRequest("User already has a recovery email.");
+
+        if (identityOptions.Account.RequireUniqueEmail)
+        {
+            var taken = await userManager.IsEmailTakenAsync(request.Request.Email, cancellationToken);
+            if (taken) return Results.BadRequest("Email already taken.");
+        }
 
         var result = await userManager.AddEmailAsync(user, request.Request.Email,
             request.Request.IsPrimary, request.Request.IsRecovery, cancellationToken: cancellationToken);
