e180c1c4b4b9f1cc1a34e4a7507de061408d0446
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-09-09 11:46:54 +0300

    Re-migrate DB

---
+++ eShop.Auth.Api/Migrations/20250909084636_RemoveDates.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eShop.Auth.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveDates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PrimaryDate",
+                table: "UserPhoneNumbers");
+
+            migrationBuilder.DropColumn(
+                name: "PrimaryDate",
+                table: "UserEmails");
+
+            migrationBuilder.DropColumn(
+                name: "RecoveryDate",
+                table: "UserEmails");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "PrimaryDate",
+                table: "UserPhoneNumbers",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "PrimaryDate",
+                table: "UserEmails",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "RecoveryDate",
+                table: "UserEmails",
+                type: "datetimeoffset",
+                nullable: true);
+        }
+    }
+}
