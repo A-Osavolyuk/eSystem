b9895423f1613b9897794dbbe411f558222c73b6
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-09-11 16:02:55 +0300

    Add cookie accessor service

---
+++ eShop.Auth.Api/Interfaces/ICookieAccessor.cs
@@ -0,0 +1,9 @@
+﻿namespace eShop.Auth.Api.Interfaces;
+
+public interface ICookieAccessor
+{
+    public bool? Exists(string key);
+    public string? Get(string key);
+    public void Set(string key, string value, CookieOptions options);
+    public void Remove(string key, CookieOptions options);
+}
