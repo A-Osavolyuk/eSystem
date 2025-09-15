4085785c35a9d5a6af3ecbf6ad031dcff12422d7
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-09-15 15:23:33 +0300

    Implement API fetch script

---
+++ eShop.Blazor.UI/wwwroot/scripts/fetchClient.js
@@ -0,0 +1,13 @@
+﻿async function fetchApi(options){
+    const result = await fetch(options.url, {
+        method: options.method,
+        headers: options.headers,
+        body: options.body,
+        credentials: options.credentials
+    })
+    
+    const json = await result.json()
+    console.log(json)
+    
+    return json
+}
