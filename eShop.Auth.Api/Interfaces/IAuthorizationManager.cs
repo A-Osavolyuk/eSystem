3261c8eb071b69629904735789dd559241329843
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-09-16 13:32:03 +0300

    Implement authorization sessions management

---
+++ eShop.Auth.Api/Interfaces/IAuthorizationManager.cs
@@ -0,0 +1,12 @@
+﻿namespace eShop.Auth.Api.Interfaces;
+
+public interface IAuthorizationManager
+{
+    public ValueTask<AuthorizationSessionEntity?> FindAsync(UserEntity user, 
+        UserDeviceEntity device, CancellationToken cancellationToken);
+    
+    public ValueTask<Result> CreateAsync(UserEntity user, 
+        UserDeviceEntity device, CancellationToken cancellationToken);
+    
+    public ValueTask<Result> RemoveAsync(AuthorizationSessionEntity session, CancellationToken cancellationToken);
+}
---
+++ eShop.Auth.Api/Services/AuthorizationManager.cs
@@ -0,0 +1,40 @@
+﻿namespace eShop.Auth.Api.Services;
+
+[Injectable(typeof(IAuthorizationManager), ServiceLifetime.Scoped)]
+public class AuthorizationManager(AuthDbContext context) : IAuthorizationManager
+{
+    private readonly AuthDbContext context = context;
+
+    public async ValueTask<AuthorizationSessionEntity?> FindAsync(UserEntity user, 
+        UserDeviceEntity device, CancellationToken cancellationToken)
+    {
+        var entity = await context.AuthorizationSessions.FirstOrDefaultAsync(
+            x => x.UserId == user.Id && x.DeviceId == device.Id, cancellationToken);
+        return entity;
+    }
+
+    public async ValueTask<Result> CreateAsync(UserEntity user, 
+        UserDeviceEntity device, CancellationToken cancellationToken)
+    {
+        var entity = new AuthorizationSessionEntity()
+        {
+            Id = Guid.CreateVersion7(),
+            UserId = user.Id,
+            DeviceId = device.Id,
+            CreateDate = DateTimeOffset.UtcNow,
+        };
+        
+        await context.AuthorizationSessions.AddAsync(entity, cancellationToken);
+        await context.SaveChangesAsync(cancellationToken);
+
+        return Result.Success();
+    }
+
+    public async ValueTask<Result> RemoveAsync(AuthorizationSessionEntity session, CancellationToken cancellationToken)
+    {
+        context.AuthorizationSessions.Remove(session);
+        await context.SaveChangesAsync(cancellationToken);
+
+        return Result.Success();
+    }
+}
