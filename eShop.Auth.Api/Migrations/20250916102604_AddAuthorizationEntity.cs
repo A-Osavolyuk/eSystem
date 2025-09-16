518fac7eed23130a9f21e01a92e92a12134a9740
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-09-16 13:26:32 +0300

    Re-migrate DB

---
+++ eShop.Auth.Api/Migrations/20250916102604_AddAuthorizationEntity.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eShop.Auth.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAuthorizationEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AuthorizationSessions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DeviceId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreateDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    UpdateDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuthorizationSessions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AuthorizationSessions_UserDevices_DeviceId",
+                        column: x => x.DeviceId,
+                        principalTable: "UserDevices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AuthorizationSessions_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AuthorizationSessions_DeviceId",
+                table: "AuthorizationSessions",
+                column: "DeviceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AuthorizationSessions_UserId",
+                table: "AuthorizationSessions",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuthorizationSessions");
+        }
+    }
+}
