b26d386ceefa171fecc08bd188c14dc2c58d8933
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-09-22 15:59:43 +0300

    Refactor JWT authnetication handler

--- eShop.Blazor.Server.Infrastructure/Security/JwtAuthenticationHandler.cs
+++ eShop.Blazor.Server.Infrastructure/Security/JwtAuthenticationHandler.cs
@@ -3,6 +3,7 @@ using eShop.Blazor.Server.Domain.Interfaces;
 using eShop.Domain.Requests.API.Auth;
 using eShop.Domain.Responses.API.Auth;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
@@ -21,36 +22,24 @@ public class JwtAuthenticationHandler(
     private readonly TokenProvider tokenProvider = tokenProvider;
     private readonly TokenHandler tokenHandler = tokenHandler;
 
-    protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+    protected override Task<AuthenticateResult> HandleAuthenticateAsync()
     {
         try
         {
             var accessToken = tokenProvider.AccessToken;
-            if (string.IsNullOrEmpty(accessToken))
-            {
-                var refreshToken = tokenProvider.RefreshToken;
-                if (string.IsNullOrEmpty(refreshToken)) return AuthenticateResult.NoResult();
-            
-                var request = new AuthenticateRequest() { RefreshToken = refreshToken };
-                var result = await securityService.AuthenticateAsync(request);
-                if (!result.Success) return AuthenticateResult.NoResult();
-
-                var response = result.Get<AuthenticateResponse>()!;
-                accessToken = response.AccessToken;
-                tokenProvider.AccessToken = response.AccessToken;
-            }
+            if (string.IsNullOrEmpty(accessToken)) return Task.FromResult(AuthenticateResult.NoResult());
 
             var rawToken = tokenHandler.ReadToken(accessToken)!;
             var claims = rawToken.Claims.ToList();
-            var identity = new ClaimsIdentity(claims, JwtBearerDefaults.AuthenticationScheme);
+            var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
             var principal = new ClaimsPrincipal(identity);
-            var ticket = new AuthenticationTicket(principal, JwtBearerDefaults.AuthenticationScheme);
+            var ticket = new AuthenticationTicket(principal, CookieAuthenticationDefaults.AuthenticationScheme);
             
-            return AuthenticateResult.Success(ticket);
+            return Task.FromResult(AuthenticateResult.Success(ticket));
         }
         catch (Exception)
         {
-            return AuthenticateResult.NoResult();
+            return Task.FromResult(AuthenticateResult.NoResult());
         }
     }
 
