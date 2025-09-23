c18cd96ea710a7b58eb04554baf131e1fa4c44b0
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-09-23 15:58:27 +0300

    Remove cookie accessor as redundant

--- eShop.Auth.Api/Interfaces/ICookieAccessor.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace eShop.Auth.Api.Interfaces;
-
-public interface ICookieAccessor
-{
-    public bool? Exists(string key);
-    public string? Get(string key);
-    public void Set(string key, string value, CookieOptions options);
-    public void Remove(string key, CookieOptions options);
-}
