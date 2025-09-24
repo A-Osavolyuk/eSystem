471046e46c36510454c6f0cb66eeb4745bb6ef3d
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-09-24 14:18:30 +0300

    Implement key generator

---
+++ eShop.Auth.Api/Utilities/KeyGenerator.cs
@@ -0,0 +1,14 @@
+﻿using OtpNet;
+
+namespace eShop.Auth.Api.Utilities;
+
+public static class KeyGenerator
+{
+    public static string GenerateKey(uint length)
+    {
+        var keyBytes = KeyGeneration.GenerateRandomKey((int)length);
+        var keyString = Base32Encoding.ToString(keyBytes);
+        var key = keyString[0..(int)length]!;
+        return key;
+    }
+}
