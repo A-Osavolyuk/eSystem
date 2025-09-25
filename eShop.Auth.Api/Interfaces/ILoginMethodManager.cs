16de7828f8497d01db2be0e88c556c447a51431b
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-09-25 11:33:27 +0300

    Implement login method manager

---
+++ eShop.Auth.Api/Interfaces/ILoginMethodManager.cs
@@ -0,0 +1,7 @@
+﻿namespace eShop.Auth.Api.Interfaces;
+
+public interface ILoginMethodManager
+{
+    public ValueTask<List<LoginMethodEntity>> GetAllAsync(CancellationToken cancellationToken = default);
+    public ValueTask<LoginMethodEntity?> FindAsync(LoginType type, CancellationToken cancellationToken = default);
+}
---
+++ eShop.Auth.Api/Services/LoginMethodManager.cs
@@ -0,0 +1,21 @@
+﻿namespace eShop.Auth.Api.Services;
+
+[Injectable(typeof(ILoginMethodManager), ServiceLifetime.Scoped)]
+public class LoginMethodManager(AuthDbContext context) : ILoginMethodManager
+{
+    private readonly AuthDbContext context = context;
+
+    public async ValueTask<List<LoginMethodEntity>> GetAllAsync(CancellationToken cancellationToken = default)
+    {
+        var entities = await context.LoginMethods.ToListAsync(cancellationToken);
+        return entities;
+    }
+
+    public async ValueTask<LoginMethodEntity?> FindAsync(LoginType type, CancellationToken cancellationToken = default)
+    {
+        var entity = await context.LoginMethods.FirstOrDefaultAsync(
+            x => x.Type == type, cancellationToken);
+        
+        return entity;
+    }
+}
