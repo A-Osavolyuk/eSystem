686ea0905ee95f24cabc027ed077d4ff29e8206b
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-09-25 11:10:43 +0300

    Add seeding for login methods

---
+++ eShop.Auth.Api/Data/Seeding/LoginMethodSeed.cs
@@ -0,0 +1,37 @@
+﻿using eShop.Domain.Abstraction.Data.Seeding;
+
+namespace eShop.Auth.Api.Data.Seeding;
+
+public class LoginMethodSeed : Seed<LoginMethodEntity>
+{
+    public override List<LoginMethodEntity> Get()
+    {
+        return
+        [
+            new LoginMethodEntity()
+            {
+                Id = Guid.CreateVersion7(),
+                Type = LoginType.Password,
+                CreateDate = DateTimeOffset.UtcNow
+            },
+            new LoginMethodEntity()
+            {
+                Id = Guid.CreateVersion7(),
+                Type = LoginType.TwoFactor,
+                CreateDate = DateTimeOffset.UtcNow
+            },
+            new LoginMethodEntity()
+            {
+                Id = Guid.CreateVersion7(),
+                Type = LoginType.OAuth,
+                CreateDate = DateTimeOffset.UtcNow
+            },
+            new LoginMethodEntity()
+            {
+                Id = Guid.CreateVersion7(),
+                Type = LoginType.Passkey,
+                CreateDate = DateTimeOffset.UtcNow
+            }
+        ];
+    }
+}
