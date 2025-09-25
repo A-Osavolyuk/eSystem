4356c3d36470f6e003b0a639f16124e3da2d6cd6
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-09-25 16:32:13 +0300

    Refactor email adding flow on client-side

---
+++ eShop.Blazor.Server.Domain/Constants/EmailTypes.cs
@@ -0,0 +1,8 @@
+﻿namespace eShop.Blazor.Server.Domain.Constants;
+
+public static class EmailTypes
+{
+    public const string Primary = "primary";
+    public const string Recovery = "recovery";
+    public const string Secondary = "secondary";
+}
