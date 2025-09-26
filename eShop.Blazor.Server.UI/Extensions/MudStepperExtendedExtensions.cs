6541635971b937adfb487685bc2fb35bc8bbaaed
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-09-26 14:44:42 +0300

    Make StepNextAsync method as extension

---
+++ eShop.Blazor.Server.UI/Extensions/MudStepperExtendedExtensions.cs
@@ -0,0 +1,12 @@
+﻿using MudExtensions;
+
+namespace eShop.Blazor.Server.UI.Extensions;
+
+public static class MudStepperExtendedExtensions
+{
+    public static async Task StepNextAsync(this MudStepperExtended stepper)
+    {
+        var index = stepper.GetActiveIndex();
+        await stepper.CompleteStep(index);
+    }
+}
