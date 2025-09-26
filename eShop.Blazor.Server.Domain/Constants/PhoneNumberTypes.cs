7cf86a4f07827d2c88e826e5d212ad5410c3fe9b
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-09-26 12:02:32 +0300

    Refactor phone number adding flow on client-side

---
+++ eShop.Blazor.Server.Domain/Constants/PhoneNumberTypes.cs
@@ -0,0 +1,8 @@
+﻿namespace eShop.Blazor.Server.Domain.Constants;
+
+public static class PhoneNumberTypes
+{
+    public const string Primary = "primary";
+    public const string Recovery = "recovery";
+    public const string Secondary = "secondary";
+}
