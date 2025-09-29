2be6aaa62e0ee98caf8a876b9b0b34fb2a6fece8
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-09-29 15:19:12 +0300

    Prevent disable the only login method

--- eShop.Auth.Api/Features/Security/Commands/DisableMethodCommand.cs
+++ eShop.Auth.Api/Features/Security/Commands/DisableMethodCommand.cs
@@ -13,11 +13,28 @@ public class DisableMethodCommandHandler(
 
     public async Task<Result> Handle(DisableMethodCommand request, CancellationToken cancellationToken)
     {
+        var loginType = request.Request.Type;
+
         var user = await userManager.FindByIdAsync(request.Request.UserId, cancellationToken);
         if (user is null) return Results.NotFound($"Cannot find user with ID {request.Request.UserId}.");
-        if (!user.HasLoginMethod(request.Request.Type)) return Results.BadRequest("Method is already disabled.");
-        
-        var method = user.GetLoginMethod(request.Request.Type);
+        if (!user.HasLoginMethod(loginType)) return Results.BadRequest("Method is already disabled.");
+
+        if (loginType == LoginType.Passkey
+            && !user.HasLoginMethod(LoginType.Password)
+            || !user.HasLoginMethod(LoginType.OAuth))
+            return Results.BadRequest("You need to configure password sign-in or link external account");
+
+        if (loginType == LoginType.Password
+            && !user.HasLoginMethod(LoginType.Passkey)
+            || !user.HasLoginMethod(LoginType.OAuth))
+            return Results.BadRequest("You need to configure passkey sign-in or link external account");
+
+        if (loginType == LoginType.OAuth
+            && !user.HasLoginMethod(LoginType.Password)
+            || !user.HasLoginMethod(LoginType.Passkey))
+            return Results.BadRequest("You need to configure sign-in with password or passkey");
+
+        var method = user.GetLoginMethod(loginType);
         var result = await loginMethodManager.RemoveAsync(method, cancellationToken);
         return result;
     }
