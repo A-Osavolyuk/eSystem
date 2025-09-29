3a123a8cf941e2136603fbde05e7074fcc7ad919
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-09-29 14:29:36 +0300

    Refactor navigation on login management

---
+++ eShop.Blazor.Server.Domain/Constants/LoginTypes.cs
@@ -0,0 +1,9 @@
+﻿namespace eShop.Blazor.Server.Domain.Constants;
+
+public class LoginTypes
+{
+    public const string Password = "password";
+    public const string TwoFactor = "2fa";
+    public const string OAuth = "oauth";
+    public const string Passkey = "passkey";
+}
