7bb2306b18aea4c1f45c122f109eb6cda08815a2
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-09-30 14:29:29 +0300

    Re-migrate DB

---
+++ eShop.Auth.Api/Migrations/20250930112842_UpdateUserTwoFactorEntity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eShop.Auth.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserTwoFactorEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPrimary",
+                table: "UserTwoFactorProviders",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPrimary",
+                table: "UserTwoFactorProviders");
+        }
+    }
+}
