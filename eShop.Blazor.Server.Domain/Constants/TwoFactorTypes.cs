5b16707ddea6e2637d289c680361a0398155a0cd
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-10-02 15:41:11 +0300

    Enhance 2FA section UI

---
+++ eShop.Blazor.Server.Domain/Constants/TwoFactorTypes.cs
@@ -0,0 +1,8 @@
+﻿namespace eShop.Blazor.Server.Domain.Constants;
+
+public static class TwoFactorTypes
+{
+    public const string AuthenticatorApp = "authenticator";
+    public const string Passkeys = "passkeys";
+    public const string SMS = "sms";
+}
