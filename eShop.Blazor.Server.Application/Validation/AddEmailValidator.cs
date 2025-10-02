fc6755a74f7df8221ecbb935edad3abbb1d4e525
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-10-02 13:53:47 +0300

    Add validation on email add

---
+++ eShop.Blazor.Server.Application/Validation/AddEmailValidator.cs
@@ -0,0 +1,11 @@
+﻿using eShop.Blazor.Server.Domain.Models;
+
+namespace eShop.Blazor.Server.Application.Validation;
+
+public class AddEmailValidator : Validator<AddEmailModel>
+{
+    public AddEmailValidator()
+    {
+        RuleFor(x => x.Email).EmailAddress();
+    }
+}
