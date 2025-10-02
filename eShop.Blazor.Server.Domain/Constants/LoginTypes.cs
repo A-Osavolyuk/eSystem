d95b1bcb8e2cde2a6d990af1f08049347da22a1b
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-10-02 15:40:48 +0300

    Remove LoginTypes as redundant

--- eShop.Blazor.Server.Domain/Constants/LoginTypes.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace eShop.Blazor.Server.Domain.Constants;
-
-public class LoginTypes
-{
-    public const string Password = "password";
-    public const string OAuth = "oauth";
-    public const string Passkey = "passkey";
-}
