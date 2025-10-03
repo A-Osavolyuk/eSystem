15b07a3024e49d55bb15e86ea019ed9c9ea758dc
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-10-03 12:18:45 +0300

    Implement adding passkey page

---
+++ eShop.Blazor.Server.Application/Validation/CreatePasskeyValidator.cs
@@ -0,0 +1,13 @@
+﻿using eShop.Blazor.Server.Domain.Models;
+
+namespace eShop.Blazor.Server.Application.Validation;
+
+public class CreatePasskeyValidator : Validator<CreatePasskeyModel>
+{
+    public CreatePasskeyValidator()
+    {
+        RuleFor(x => x.DisplayName)
+            .NotEmpty().WithMessage("Field is required")
+            .MinimumLength(3).WithMessage("First name must be at least 3 characters long.");
+    }
+}
--- eShop.Blazor.Server.Domain/Models/CreatePasskeyModel.cs
+++ eShop.Blazor.Server.Domain/Models/CreatePasskeyModel.cs
@@ -2,6 +2,5 @@
 
 public class CreatePasskeyModel
 {
-    public Guid UserId { get; set; }
     public string DisplayName { get; set; } = string.Empty;
 }
