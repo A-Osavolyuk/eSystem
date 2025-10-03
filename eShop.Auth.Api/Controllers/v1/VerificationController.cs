5e633eda177a1feb1bffb6a98c7180f57ff3f3aa
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-10-03 14:23:05 +0300

    Add verification controller

---
+++ eShop.Auth.Api/Controllers/v1/VerificationController.cs
@@ -0,0 +1,53 @@
+﻿using eShop.Auth.Api.Features.Verification.Commands;
+using eShop.Domain.Common.Http;
+using eShop.Domain.Requests.API.Auth;
+
+namespace eShop.Auth.Api.Controllers.v1;
+
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiVersion("1.0")]
+[ApiController]
+[AllowAnonymous]
+public class VerificationController(ISender sender) : ControllerBase
+{
+    private readonly ISender sender = sender;
+    
+    [EndpointSummary("Send code")]
+    [EndpointDescription("Sends code")]
+    [ProducesResponseType(200)]
+    [HttpPost("code/send")]
+    public async ValueTask<IActionResult> SendCodeAsync([FromBody] SendCodeRequest request)
+    {
+        var result = await sender.Send(new SendCodeCommand(request));
+
+        return result.Match(
+            s => Ok(new ResponseBuilder().Succeeded().WithMessage(s.Message).WithResult(s.Value).Build()),
+            ErrorHandler.Handle);
+    }
+
+    [EndpointSummary("Resend code")]
+    [EndpointDescription("Resend code")]
+    [ProducesResponseType(200)]
+    [HttpPost("code/resend")]
+    public async ValueTask<IActionResult> ResendCodeAsync([FromBody] ResendCodeRequest request)
+    {
+        var result = await sender.Send(new ResendCodeCommand(request));
+
+        return result.Match(
+            s => Ok(new ResponseBuilder().Succeeded().WithMessage(s.Message).WithResult(s.Value).Build()),
+            ErrorHandler.Handle);
+    }
+
+    [EndpointSummary("Verify code")]
+    [EndpointDescription("Verify code")]
+    [ProducesResponseType(200)]
+    [HttpPost("code/verify")]
+    public async ValueTask<IActionResult> VerifyCodeAsync([FromBody] VerifyCodeRequest request)
+    {
+        var result = await sender.Send(new VerifyCodeCommand(request));
+
+        return result.Match(
+            s => Ok(new ResponseBuilder().Succeeded().WithMessage(s.Message).WithResult(s.Value).Build()),
+            ErrorHandler.Handle);
+    }
+}
