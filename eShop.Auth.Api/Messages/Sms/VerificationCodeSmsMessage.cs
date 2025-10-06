fbfd4903c9f5f6f5972a365c543bd1e86c904f91
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-10-06 12:09:41 +0300

    Add unified verification messages

---
+++ eShop.Auth.Api/Messages/Sms/VerificationCodeSmsMessage.cs
@@ -0,0 +1,15 @@
+﻿namespace eShop.Auth.Api.Messages.Sms;
+
+public class VerificationCodeSmsMessage : Message
+{
+    public override string Build()
+    {
+        return $"Your verification code {Payload["Code"]}";
+    }
+
+    public override void Initialize(Dictionary<string, string> payload)
+    {
+        Credentials["To"] = payload["To"];
+        Payload = payload;
+    }
+}
