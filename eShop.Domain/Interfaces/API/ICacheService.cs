20c3342c52dfee384b05fab1b3bf3fd6e4bb50a2
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-10-06 11:25:47 +0300

    Remove redundant interfaces on Domain layer

--- eShop.Domain/Interfaces/API/ICacheService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace eShop.Domain.Interfaces.API;
-
-public interface ICacheService
-{
-    public ValueTask<bool> IsKeyExistsAsync(string key);
-    public ValueTask<T?> GetAsync<T>(string key);
-    public ValueTask SetAsync<T>(string key, T value, TimeSpan expirationTime);
-    public ValueTask RemoveAsync(string key);
-}
