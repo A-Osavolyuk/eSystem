b0a5d8348d9ec02f1b8435716bb32bfb80bcdfa9
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-10-13 10:27:56 +0300

    Refactor secret protection

--- eShop.Auth.Api/Security/Protection/SecretProtector.cs
+++ eShop.Auth.Api/Security/Protection/SecretProtector.cs
@@ -2,14 +2,9 @@
 
 namespace eShop.Auth.Api.Security.Protection;
 
-public class SecretProtector
+public class SecretProtector(IDataProtectionProvider protectionProvider)
 {
-    private readonly IDataProtector protector;
-    
-    public SecretProtector(IDataProtectionProvider protectionProvider)
-    {
-        protector = protectionProvider.CreateProtector("TOTP.Secret");
-    }
+    private readonly IDataProtector protector = protectionProvider.CreateProtector("TOTP.Secret");
     
     public string Unprotect(string protectedText) => protector.Unprotect(protectedText);
     public string Protect(string unprotectedText) => protector.Protect(unprotectedText);
