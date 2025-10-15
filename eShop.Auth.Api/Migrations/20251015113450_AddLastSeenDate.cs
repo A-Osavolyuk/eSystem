b167519bd37ffc61a1ccde4ccc08679d2fd8bcd7
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-10-15 14:36:25 +0300

    Re-migrate DB

---
+++ eShop.Auth.Api/Migrations/20251015113450_AddLastSeenDate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eShop.Auth.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLastSeenDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "LastSeenDate",
+                table: "UserPasskeys",
+                type: "datetimeoffset",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastSeenDate",
+                table: "UserPasskeys");
+        }
+    }
+}
