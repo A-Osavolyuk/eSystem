0f73d09eef9ef68300af27910747dc154052f915
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-10-16 13:47:32 +0300

    Add support for 'Enterprise' attestation type

--- eShop.Domain/Common/Security/Constants/Attestations.cs
+++ eShop.Domain/Common/Security/Constants/Attestations.cs
@@ -5,4 +5,5 @@ public static class Attestations
     public const string None = "none";
     public const string Direct = "direct";
     public const string Indirect = "indirect";
+    public const string Enterprise = "enterprise";
 }
