30d84e96061305a99d4fdf11c9bd9fd29452206f
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-10-16 13:31:44 +0300

    Implement error handling on passkey flow

--- eShop.Domain/Common/Security/Credentials/PublicKeyCredentialCreationResponse.cs
+++ eShop.Domain/Common/Security/Credentials/PublicKeyCredentialCreationResponse.cs
@@ -5,35 +5,35 @@ namespace eShop.Domain.Common.Security.Credentials;
 public class PublicKeyCredentialCreationResponse
 {
     [JsonPropertyName("id")]
-    public string Id { get; set; } = null!;
+    public required string Id { get; set; } = null!;
     
     [JsonPropertyName("rawId")]
-    public string RawId { get; set; } = null!;
+    public required string RawId { get; set; } = null!;
     
     [JsonPropertyName("type")]
-    public string Type { get; set; } = null!;
+    public required string Type { get; set; } = null!;
     
     [JsonPropertyName("response")]
-    public CredentialResponse Response { get; set; } = null!;
+    public required CredentialResponse Response { get; set; } = null!;
 }
 
 public class CredentialResponse
 {
     [JsonPropertyName("attestationObject")]
-    public string AttestationObject { get; set; } = null!;
+    public required string AttestationObject { get; set; } = null!;
     
     [JsonPropertyName("clientDataJSON")]
-    public string ClientDataJson { get; set; } = null!;
+    public required string ClientDataJson { get; set; } = null!;
     
     [JsonPropertyName("authenticatorData")]
-    public string AuthenticatorData { get; set; } = null!;
+    public required string AuthenticatorData { get; set; } = null!;
     
     [JsonPropertyName("publicKey")]
-    public string PublicKey { get; set; } = null!;
+    public required string PublicKey { get; set; } = null!;
     
     [JsonPropertyName("publicKeyAlgorithm")]
-    public int PublicKeyAlgorithm { get; set; }
+    public required int PublicKeyAlgorithm { get; set; }
 
     [JsonPropertyName("transports")] 
-    public string[] Transports { get; set; } = null!;
+    public required string[] Transports { get; set; } = null!;
 }
