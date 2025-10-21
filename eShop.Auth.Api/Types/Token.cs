99d0e37972d285fda4fd551fbe7b0a4b1f124b3a
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-10-21 14:20:12 +0300

    Remove redundant type

--- eShop.Auth.Api/Types/Token.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace eShop.Auth.Api.Types;
-
-public class Token
-{
-    public required Guid Id { get; set; }
-    public required string Value { get; set; }
-    public required DateTime ExpireDate { get; set; }
-}
