9b4f57b830d60dd6c9416a85ef163a04375df43b
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-10-24 15:57:17 +0300

    Refister SSO controller in proxy

--- eSystem.Proxy/Extensions/HostApplicationBuilderExtensions.cs
+++ eSystem.Proxy/Extensions/HostApplicationBuilderExtensions.cs
@@ -91,6 +91,11 @@ public static class HostApplicationBuilderExtensions
             },
             new RouteConfig
             {
+                RouteId = "sso-route", ClusterId = "security-cluster",
+                Match = new RouteMatch { Path = "/api/v1/Sso/{**catch-all}" }
+            },
+            new RouteConfig
+            {
                 RouteId = "files-route", ClusterId = "files-cluster",
                 Match = new RouteMatch { Path = "/api/v1/Files/{**catch-all}" }
             },
