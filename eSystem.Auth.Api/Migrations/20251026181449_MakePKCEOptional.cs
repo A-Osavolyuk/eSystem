b97a0a89080dcdd1810895011865ed24bd834d6e
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2025-10-26 20:15:34 +0200

    Re-migrate DB

---
+++ eSystem.Auth.Api/Migrations/20251026181449_MakePKCEOptional.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eSystem.Auth.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakePKCEOptional : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CodeChallengeMethod",
+                table: "AuthorizationCodes",
+                type: "nvarchar(16)",
+                maxLength: 16,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(16)",
+                oldMaxLength: 16);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CodeChallenge",
+                table: "AuthorizationCodes",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CodeChallengeMethod",
+                table: "AuthorizationCodes",
+                type: "nvarchar(16)",
+                maxLength: 16,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(16)",
+                oldMaxLength: 16,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CodeChallenge",
+                table: "AuthorizationCodes",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200,
+                oldNullable: true);
+        }
+    }
+}
