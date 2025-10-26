5b8d88a4b3957dba9a2a994536c60e8f96c3b665
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2025-10-26 21:37:19 +0200

    Re-migrate DB

---
+++ eSystem.Auth.Api/Migrations/20251026193658_ReduceRefreshTokenLength.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eSystem.Auth.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReduceRefreshTokenLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Token",
+                table: "RefreshTokens",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Nonce",
+                table: "AuthorizationCodes",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Nonce",
+                table: "AuthorizationCodes");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Token",
+                table: "RefreshTokens",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+        }
+    }
+}
