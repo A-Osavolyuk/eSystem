f506a5089437f174e3b300692c71e57182986288
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-10-27 10:52:45 +0200

    Implement PKCE handling

---
+++ eSystem.Auth.Api/Security/Authentication/SSO/PKCE/ChallengeMethods.cs
@@ -0,0 +1,7 @@
+namespace eSystem.Auth.Api.Security.Authentication.SSO.PKCE;
+
+public static class ChallengeMethods
+{
+    public const string Plain = "plain";
+    public const string S256 = "S256";
+}
--- eSystem.Auth.Api/Security/Authentication/SSO/SsoExtensions.cs
+++ eSystem.Auth.Api/Security/Authentication/SSO/SsoExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using eSystem.Auth.Api.Security.Authentication.SSO.Client;
 using eSystem.Auth.Api.Security.Authentication.SSO.Code;
+using eSystem.Auth.Api.Security.Authentication.SSO.PKCE;
 using eSystem.Auth.Api.Security.Authentication.SSO.Session;
 
 namespace eSystem.Auth.Api.Security.Authentication.SSO;
@@ -15,5 +16,6 @@ public static class SsoExtensions
         
         builder.Services.AddScoped<IClientManager, ClientManager>();
         builder.Services.AddScoped<IAuthorizationCodeManager, AuthorizationCodeManager>();
+        builder.Services.AddScoped<IPkceHandler, PkceHandler>();
     }
 }
