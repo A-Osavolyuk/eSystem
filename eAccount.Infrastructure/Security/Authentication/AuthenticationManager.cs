b187ba54b3a0426e1249a559369f8922a7fe52b5
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-10-27 15:00:58 +0200

    Implement SSO client configuration

--- eAccount.Infrastructure/Security/Authentication/AuthenticationManager.cs
+++ eAccount.Infrastructure/Security/Authentication/AuthenticationManager.cs
@@ -2,11 +2,13 @@
 using eAccount.Domain.Options;
 using eAccount.Domain.Requests;
 using eAccount.Infrastructure.Security.Authentication.JWT;
+using eAccount.Infrastructure.Security.Authentication.SSO.Clients;
 using eSystem.Core.Requests.Auth;
 using eSystem.Core.Responses.Auth;
 using eSystem.Core.Security.Authentication.SSO.Constants;
 using eSystem.Core.Security.Cryptography.Keys;
 using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Options;
 using SignInRequest = eAccount.Domain.Requests.SignInRequest;
 using SignInResponse = eAccount.Domain.Responses.SignInResponse;
 
@@ -19,7 +21,8 @@ public class AuthenticationManager(
     NavigationManager navigationManager,
     IKeyFactory keyFactory,
     IFetchClient fetchClient,
-    IStorage storage)
+    IStorage storage,
+    IOptions<ClientOptions> options)
 {
     private readonly AuthenticationStateProvider authenticationStateProvider = authenticationStateProvider;
     private readonly UserState userState = userState;
@@ -28,88 +31,81 @@ public class AuthenticationManager(
     private readonly IKeyFactory keyFactory = keyFactory;
     private readonly IFetchClient fetchClient = fetchClient;
     private readonly IStorage storage = storage;
+    private readonly ClientOptions options = options.Value;
 
     public async Task SignInAsync()
     {
-        var clientId = "eAccount";
-        var clientSecret = "2f213a036e325a55dc19320f03c2fad7c13f0169788b5968686cb4931341c393a651d7e6";
-        var redirectUri = "none";
         var nonce = keyFactory.Create(16);
         var state = keyFactory.Create(16);
-        var scopes = new List<string>()
-        {
-            Scopes.OpenId,
-            Scopes.Profile,
-            Scopes.Email,
-            Scopes.PhoneNumber,
-            Scopes.Address
-        };
-        
+
         var authorizeRequest = new AuthorizeRequest()
         {
             UserId = userState.UserId,
-            ClientId = clientId,
-            RedirectUri = redirectUri,
-            Scopes = scopes,
+            ClientId = options.ClientId,
+            RedirectUri = options.RedirectUri,
+            Scopes = options.Scopes,
             State = state,
             Nonce = nonce,
             ResponseType = ResponseTypes.Code
         };
-        
+
         var authorizeResult = await AuthorizeAsync(authorizeRequest);
-        if (authorizeResult.Success)
+        if (!authorizeResult.Success)
         {
-            var response = authorizeResult.Get<AuthorizeResponse>()!;
-
-            if (!state.Equals(response.State))
-            {
-                //TODO: Handle invalid state
-            }
-
-            var tokenRequest = new TokenRequest()
-            {
-                GrantType = GrantTypes.AuthorizationCode,
-                Code = response.Code,
-                ClientId = clientId,
-                ClientSecret = clientSecret,
-                RedirectUri = redirectUri
-            };
             
-            var tokenResult = await TokenAsync(tokenRequest);
-
-            if (tokenResult.Success)
-            {
-                var tokenResponse = tokenResult.Get<TokenResponse>()!;
-                
-                tokenProvider.AccessToken = tokenResponse.AccessToken;
-
-                var signInRequest = new SignInRequest()
-                {
-                    AccessToken = tokenResponse.AccessToken,
-                    RefreshToken = tokenResponse.RefreshToken
-                };
-                
-                var signInResult = await SignInAsync(signInRequest);
-                
-                if (signInResult.Success)
-                {
-                    var signInResponse = signInResult.Get<SignInResponse>()!;
-
-                    var identity = signInResponse.Identity;
-                    var claims = identity.Claims
-                        .Select(x => new Claim(x.Key, x.Value))
-                        .ToList();
-                    
-                    var claimsIdentity = new ClaimsIdentity(claims, identity.Scheme);
-                    var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
-
-                    (authenticationStateProvider as JwtAuthenticationStateProvider)!.SignIn(claimsPrincipal);
-                }
-                //TODO: Handle sign-in failure
-            }
-            //TODO: Handle token failure
         }
-        //TODO: Handle authorize failure
+
+        var authorizeResponse = authorizeResult.Get<AuthorizeResponse>()!;
+
+        if (!state.Equals(authorizeResponse.State))
+        {
+            //TODO: Handle invalid state
+        }
+
+        var tokenRequest = new TokenRequest()
+        {
+            GrantType = GrantTypes.AuthorizationCode,
+            Code = authorizeResponse.Code,
+            ClientId = options.ClientId,
+            ClientSecret = options.ClientSecret,
+            RedirectUri = options.RedirectUri
+        };
+
+        var tokenResult = await TokenAsync(tokenRequest);
+
+        if (!tokenResult.Success)
+        {
+            
+        }
+        
+        var tokenResponse = tokenResult.Get<TokenResponse>()!;
+
+        tokenProvider.AccessToken = tokenResponse.AccessToken;
+
+        var signInRequest = new SignInRequest()
+        {
+            AccessToken = tokenResponse.AccessToken,
+            RefreshToken = tokenResponse.RefreshToken
+        };
+
+        var signInResult = await SignInAsync(signInRequest);
+
+        if (!signInResult.Success)
+        {
+
+        }
+        
+        var signInResponse = signInResult.Get<SignInResponse>()!;
+
+        var identity = signInResponse.Identity;
+        var claims = identity.Claims
+            .Select(x => new Claim(x.Key, x.Value))
+            .ToList();
+
+        var claimsIdentity = new ClaimsIdentity(claims, identity.Scheme);
+        var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+        (authenticationStateProvider as JwtAuthenticationStateProvider)!.SignIn(claimsPrincipal);
     }
 
     private async Task<HttpResponse> SignInAsync(SignInRequest request)
@@ -135,7 +131,7 @@ public class AuthenticationManager(
 
         return await fetchClient.FetchAsync(fetchOptions);
     }
-    
+
     private async Task<HttpResponse> TokenAsync(TokenRequest request)
     {
         var fetchOptions = new FetchOptions()
@@ -155,7 +151,7 @@ public class AuthenticationManager(
             UserId = userState.UserId,
             AccessToken = tokenProvider.AccessToken!
         };
-        
+
         var fetchOptions = new FetchOptions()
         {
             Method = HttpMethod.Post,
--- eAccount.Infrastructure/Security/SecurityExtensions.cs
+++ eAccount.Infrastructure/Security/SecurityExtensions.cs
@@ -1,8 +1,10 @@
 ﻿using eAccount.Infrastructure.Security.Authentication.JWT;
+using eAccount.Infrastructure.Security.Authentication.SSO.Clients;
 using eAccount.Infrastructure.Security.Credentials.PublicKey;
 using eAccount.Infrastructure.Security.Cryptography.Keys;
 using eAccount.Infrastructure.Security.Cryptography.Protection;
 using eSystem.Core.Security.Authentication.Cookies;
+using eSystem.Core.Security.Authentication.SSO.Constants;
 using eSystem.Core.Security.Cryptography.Keys;
 using eSystem.Core.Security.Cryptography.Protection;
 using Microsoft.AspNetCore.Authentication.Cookies;
@@ -14,6 +16,20 @@ public static class SecurityExtensions
 {
     public static void AddAuthentication(this IHostApplicationBuilder builder)
     {
+        builder.Services.ConfigureClient(cfg =>
+        {
+            cfg.ClientId = "eAccount";
+            cfg.ClientSecret = "2f213a036e325a55dc19320f03c2fad7c13f0169788b5968686cb4931341c393a651d7e6";
+            cfg.RedirectUri = "none";
+            cfg.Scopes =
+            [
+                Scopes.Address,
+                Scopes.Email,
+                Scopes.OpenId,
+                Scopes.PhoneNumber,
+                Scopes.Profile,
+            ];
+        });
         builder.Services.AddAuthorization();
         builder.Services.AddProtection();
         builder.Services.AddScoped<IKeyFactory, RandomKeyFactory>();
