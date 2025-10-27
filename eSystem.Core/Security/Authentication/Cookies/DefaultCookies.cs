46bf38d8a5af2c898c4400b651be081f8bec95a3
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-10-27 14:35:30 +0200

    Refactor default cookies

--- eSystem.Core/Security/Authentication/Cookies/DefaultCookies.cs
+++ eSystem.Core/Security/Authentication/Cookies/DefaultCookies.cs
@@ -1,9 +1,9 @@
 ﻿namespace eSystem.Core.Security.Authentication.Cookies;
 
-public class DefaultCookies
+public static class DefaultCookies
 {
-    public const string RefreshToken = "eAccount.Authentication.RefreshToken";
     public const string External = "eAccount.Authentication.External";
     public const string State = "eAccount.Authentication.State";
     public const string Session = "eAccount.Authentication.SSO.Session";
+    public const string RefreshToken = "eAccount.Authentication.SSO.RefreshToken";
 }
