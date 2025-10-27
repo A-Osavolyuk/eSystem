646576d2869dd7822ac27c39d76ab4b4afa3c275
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-10-27 14:36:32 +0200

    Add protection for refresh token

--- eSystem.Core/Security/Cryptography/Protection/ProtectionPurposes.cs
+++ eSystem.Core/Security/Cryptography/Protection/ProtectionPurposes.cs
@@ -5,4 +5,5 @@ public static class ProtectionPurposes
     public const string Secret = "TOTP.Secret";
     public const string RecoveryCode = "2FA.RecoveryCode";
     public const string Session = "SSO.Session";
+    public const string RefreshToken = "SSO.RefreshToken";
 }
