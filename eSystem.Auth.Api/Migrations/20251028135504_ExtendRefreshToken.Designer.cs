8b578bc0b054b40e058238c074e1c85b084ce23a
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-10-28 15:57:28 +0200

    Re-migrate DB

--- eSystem.Auth.Api/Migrations/20251028135504_ExtendRefreshToken.Designer.cs
+++ eSystem.Auth.Api/Migrations/20251028135504_ExtendRefreshToken.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eSystem.Auth.Api.Data;
 
@@ -11,9 +12,11 @@ using eSystem.Auth.Api.Data;
 namespace eSystem.Auth.Api.Migrations
 {
     [DbContext(typeof(AuthDbContext))]
-    partial class AuthDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251028135504_ExtendRefreshToken")]
+    partial class ExtendRefreshToken
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
