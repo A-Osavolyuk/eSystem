8293aff280d79e9f9645a8822b8567193db24613
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-10-30 15:29:54 +0200

    Implement ID token claim types

--- eSystem.Core/Security/Identity/Claims/AppClaimTypes.cs
+++ eSystem.Core/Security/Identity/Claims/AppClaimTypes.cs
@@ -3,11 +3,17 @@
 public static class AppClaimTypes
 {
     public const string Jti = "jti";
-    public const string Exp = "exp";
+    public const string Iat = "iat";
     public const string Sub = "sub";
+    public const string Aud = "aud";
+    public const string Iss = "iss";
+    public const string Exp = "exp";
     public const string Permission = "permission";
     public const string Role = "role";
-    public const string Type = "type";
     public const string Nonce = "nonce";
     public const string Scope = "scope";
+    public const string Name = "name";
+    public const string PhoneNumber = "phone_number";
+    public const string PreferredUsername = "preferred_username";
+    public const string AuthenticationTime = "auth_time";
 }
