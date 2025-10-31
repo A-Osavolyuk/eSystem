307d5cc96e08806038dbcaf942c2f96a83b5cf76
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-10-31 11:11:14 +0200

    Extend claims

--- eSystem.Core/Security/Identity/Claims/AppClaimTypes.cs
+++ eSystem.Core/Security/Identity/Claims/AppClaimTypes.cs
@@ -3,19 +3,41 @@
 public static class AppClaimTypes
 {
     public const string Jti = "jti";
-    public const string Iat = "iat";
-    public const string Sub = "sub";
     public const string Aud = "aud";
     public const string Iss = "iss";
+    public const string Sub = "sub";
     public const string Exp = "exp";
+    public const string Iat = "iat";
+    public const string AuthenticationTime = "auth_time";
+    
     public const string Nbf = "nbf";
+    public const string Sid = "sid";
+    public const string Azp = "azp";
+    public const string Amr = "amr";
+    public const string Acr = "acr";
+    
     public const string Permission = "permission";
     public const string Role = "role";
     public const string Nonce = "nonce";
     public const string Scope = "scope";
+    public const string AccessTokenHash = "at_hash";
+    public const string AuthorizationCodeHash = "c_hash";
+    
     public const string Name = "name";
-    public const string Email = "email";
-    public const string PhoneNumber = "phone_number";
+    public const string GivenName = "given_name";
+    public const string FamilyName = "family_name";
     public const string PreferredUsername = "preferred_username";
-    public const string AuthenticationTime = "auth_time";
+    public const string Picture = "picture";
+    public const string UpdatedAt = "updated_at";
+    public const string Locale = "locale";
+    
+    public const string Address = "address";
+    
+    public const string Email = "email";
+    public const string EmailVerified = "email_verified";
+    
+    public const string PhoneNumber = "phone_number";
+    public const string PhoneNumberVerified = "phone_number_verified";
+    
+
 }
