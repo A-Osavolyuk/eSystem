f8037be44b0e1d97709347f4a7308eb91f2a1b78
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-10-31 11:11:40 +0200

    Rename scope 'phone-number' to 'phone'

--- eSystem.Core/Security/Authentication/SSO/Constants/Scopes.cs
+++ eSystem.Core/Security/Authentication/SSO/Constants/Scopes.cs
@@ -5,6 +5,6 @@ public static class Scopes
     public const string OpenId = "openid";
     public const string Profile = "profile";
     public const string Email = "email";
-    public const string PhoneNumber = "phone-number";
+    public const string PhoneNumber = "phone";
     public const string Address = "address";
 }
