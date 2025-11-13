8a621fb172f638da745d679733e2c0be53c8fff2
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-11-13 12:28:55 +0200

    Implement JWT authentiocation scheme

--- eSecurity.Server/Security/Authentication/Handlers/JwtAuthenticationHandler.cs
+++ eSecurity.Server/Security/Authentication/Handlers/JwtAuthenticationHandler.cs
@@ -1,4 +1,9 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Cryptography.X509Certificates;
 using System.Text.Encodings.Web;
+using eSecurity.Server.Security.Authentication.Oidc.Client;
+using eSecurity.Server.Security.Cryptography.Signing.Certificates;
+using eSecurity.Server.Security.Cryptography.Tokens.Jwt;
 using Microsoft.AspNetCore.Authentication;
 
 namespace eSecurity.Server.Security.Authentication.Handlers;
@@ -8,12 +13,47 @@ public class JwtAuthenticationSchemeOptions : AuthenticationSchemeOptions {}
 public class JwtAuthenticationHandler(
     IOptionsMonitor<JwtAuthenticationSchemeOptions> options, 
     ILoggerFactory logger, 
-    UrlEncoder encoder) : AuthenticationHandler<JwtAuthenticationSchemeOptions>(options, logger, encoder)
+    UrlEncoder encoder,
+    ICertificateProvider certificateProvider,
+    IClientManager clientManager,
+    IOptions<TokenOptions> tokenOptions) : AuthenticationHandler<JwtAuthenticationSchemeOptions>(options, logger, encoder)
 {
-    protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+    private readonly ICertificateProvider _certificateProvider = certificateProvider;
+    private readonly IClientManager _clientManager = clientManager;
+    private readonly TokenOptions _tokenOptions = tokenOptions.Value;
+
+    protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
     {
-        //TODO: Implement JWT authentication handler
+        var authorizationHeader = Request.Headers.Authorization.ToString();
+        if (string.IsNullOrEmpty(authorizationHeader) || !authorizationHeader.StartsWith("Bearer ")) 
+            return AuthenticateResult.NoResult();
         
-        throw new NotImplementedException();
+        var token = authorizationHeader["Bearer ".Length..];
+        var handler = new JwtSecurityTokenHandler();
+        var securityToken = handler.ReadJwtToken(token);
+        if (securityToken is null) return AuthenticateResult.Fail("Invalid token.");
+
+        var kid = Guid.Parse(securityToken.Header.Kid);
+        var certificate = await _certificateProvider.FindByIdAsync(kid);
+        if (certificate is null) return AuthenticateResult.Fail("Certificate not found.");
+        
+        var publicKey = certificate.Certificate.GetRSAPublicKey()!;
+        var audiences = await _clientManager.GetAudiencesAsync();
+        
+        var validationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = true,
+            ValidIssuer = _tokenOptions.Issuer,
+            ValidateAudience = true,
+            ValidAudiences = audiences,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new RsaSecurityKey(publicKey),
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.FromMinutes(5)
+        };
+        
+        var principal = handler.ValidateToken(token, validationParameters, out var validatedToken);
+        var ticket = new AuthenticationTicket(principal, Scheme.Name);
+        return AuthenticateResult.Success(ticket);
     }
 }
