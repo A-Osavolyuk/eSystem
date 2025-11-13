e3de53ded3b21cc98e571f1227a61e55a86feb5e
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-11-13 16:15:37 +0200

    Remove redundant results

--- eSystem.Core/Common/Results/Results.cs
+++ eSystem.Core/Common/Results/Results.cs
@@ -2,23 +2,14 @@
 
 public static class Results
 {
-    public static Result NotFound(string details, object? value = null)
+    public static Result NotFound(string details)
     {
         return Result.Failure(new Error()
         {
             Code = ErrorCode.NotFound,
             Message = "Not found",
             Details = details
-        }, value);
-    }
-    
-    public static Result NotFound(object? value = null)
-    {
-        return Result.Failure(new Error()
-        {
-            Code = ErrorCode.NotFound,
-            Message = "Not found",
-        }, value);
+        });
     }
     
     public static Result BadRequest(string details, object? value = null)
@@ -31,15 +22,6 @@ public static class Results
         }, value);
     }
     
-    public static Result BadRequest(object? value = null)
-    {
-        return Result.Failure(new Error()
-        {
-            Code = ErrorCode.BadRequest,
-            Message = "Bad request",
-        }, value);
-    }
-    
     public static Result InternalServerError(string details, object? value = null)
     {
         return Result.Failure(new Error()
@@ -47,34 +29,6 @@ public static class Results
             Code = ErrorCode.InternalServerError,
             Message = "Internal server error",
             Details = details
-        }, value);
-    }
-    
-    public static Result InternalServerError(object? value = null)
-    {
-        return Result.Failure(new Error()
-        {
-            Code = ErrorCode.InternalServerError,
-            Message = "Internal server error",
-        }, value);
-    }
-
-    public static Result Unauthorized(string details, object? value = null)
-    {
-        return Result.Failure(new Error()
-        {
-            Code = ErrorCode.Unauthorized,
-            Message = "Unauthorized",
-            Details = details
-        }, value);
-    }
-    
-    public static Result Unauthorized(object? value = null)
-    {
-        return Result.Failure(new Error()
-        {
-            Code = ErrorCode.Unauthorized,
-            Message = "Unauthorized",
         }, value);
     }
 }
