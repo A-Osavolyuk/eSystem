ccf38b4e45d5dd69b5327d6e8b269ead946bc6ac
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-11-14 13:15:05 +0200

    Fix issue with deserializing SignInPayload derived types

--- eSecurity.Core/Security/Authentication/SignIn/SignInPayload.cs
+++ eSecurity.Core/Security/Authentication/SignIn/SignInPayload.cs
@@ -1,3 +1,11 @@
+using System.Text.Json.Serialization;
+
 namespace eSecurity.Core.Security.Authentication.SignIn;
 
+[JsonPolymorphic(TypeDiscriminatorPropertyName = "type")]
+[JsonDerivedType(typeof(PasswordSignInPayload), typeDiscriminator: "password")]
+[JsonDerivedType(typeof(PasskeySignInPayload), typeDiscriminator: "passkey")]
+[JsonDerivedType(typeof(OAuthSignInPayload), typeDiscriminator: "oauth")]
+[JsonDerivedType(typeof(AuthenticatorSignInPayload), typeDiscriminator: "authenticator")]
+[JsonDerivedType(typeof(RecoveryCodeSignInPayload), typeDiscriminator: "recoveryCode")]
 public abstract class SignInPayload { }
