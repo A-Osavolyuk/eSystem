28cfd506a43f130b2fe644add31d22701f1f50c6
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-11-17 16:29:52 +0200

    Configure error JSON serialization by RFC 6749 standart

--- eSystem.Core/Common/Results/Error.cs
+++ eSystem.Core/Common/Results/Error.cs
@@ -1,9 +1,12 @@
+using System.Text.Json.Serialization;
+
 namespace eSystem.Core.Common.Results;
 
 public class Error
 {
-    public required string ErrorCode { get; init; }
-    public required string ErrorDescription { get; init; }
+    [JsonPropertyName("error")]
     public required string Code { get; init; }
+    
+    [JsonPropertyName("error_description")]
     public required string Description { get; init; }
 }
