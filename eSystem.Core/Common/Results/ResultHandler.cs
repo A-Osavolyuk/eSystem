d9b6bd36d5aa6b205dd9bc847f3a46a8b20dff6c
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-11-17 16:34:29 +0200

    Fix issue with error serialization

--- eSystem.Core/Common/Results/ResultHandler.cs
+++ eSystem.Core/Common/Results/ResultHandler.cs
@@ -13,12 +13,12 @@ public static class ResultHandler
             HttpStatusCode.Created => new ObjectResult(result.Value) { StatusCode = (int)result.StatusCode },
             HttpStatusCode.Found => new ObjectResult(result.Value) { StatusCode = (int)result.StatusCode },
             HttpStatusCode.SeeOther => new ObjectResult(result.Value) { StatusCode = (int)result.StatusCode },
-            HttpStatusCode.BadRequest => new BadRequestObjectResult(result.Value),
-            HttpStatusCode.Unauthorized => new UnauthorizedObjectResult(result.Value),
-            HttpStatusCode.Forbidden => new ObjectResult(result.Value) { StatusCode = (int)result.StatusCode },
-            HttpStatusCode.NotFound => new NotFoundObjectResult(result.Value),
-            HttpStatusCode.TooManyRequests => new ObjectResult(result.Value) { StatusCode = (int)result.StatusCode },
-            HttpStatusCode.InternalServerError => new ObjectResult(result.Value) { StatusCode = (int)result.StatusCode },
+            HttpStatusCode.BadRequest => new BadRequestObjectResult(result.GetError()),
+            HttpStatusCode.Unauthorized => new UnauthorizedObjectResult(result.GetError()),
+            HttpStatusCode.Forbidden => new ObjectResult(result.GetError()) { StatusCode = (int)result.StatusCode },
+            HttpStatusCode.NotFound => new NotFoundObjectResult(result.GetError()),
+            HttpStatusCode.TooManyRequests => new ObjectResult(result.GetError()) { StatusCode = (int)result.StatusCode },
+            HttpStatusCode.InternalServerError => new ObjectResult(result.GetError()) { StatusCode = (int)result.StatusCode },
             _ => throw new NotSupportedException("Not supported status code.")
         };
     }
