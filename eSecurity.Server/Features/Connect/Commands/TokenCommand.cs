c543343b1b6407987e43e5ccfb1e4e4213b1e4e8
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-11-18 16:31:47 +0200

    Implement token request validation

--- eSecurity.Server/Features/Connect/Commands/TokenCommand.cs
+++ eSecurity.Server/Features/Connect/Commands/TokenCommand.cs
@@ -1,4 +1,5 @@
 ﻿using eSecurity.Core.Common.Requests;
+using eSecurity.Core.Security.Authentication.Oidc;
 using eSecurity.Server.Security.Authentication.Oidc.Token;
 using eSecurity.Server.Security.Authentication.Oidc.Token.Strategies;
 using eSystem.Core.Security.Authentication.Oidc.Constants;
@@ -7,12 +8,43 @@ namespace eSecurity.Server.Features.Connect.Commands;
 
 public record TokenCommand(TokenRequest Request) : IRequest<Result>;
 
-public class TokenCommandHandler(ITokenStrategyResolver tokenStrategyResolver) : IRequestHandler<TokenCommand, Result>
+public class TokenCommandHandler(
+    ITokenStrategyResolver tokenStrategyResolver,
+    IOptions<OpenIdOptions> options) : IRequestHandler<TokenCommand, Result>
 {
     private readonly ITokenStrategyResolver _tokenStrategyResolver = tokenStrategyResolver;
+    private readonly OpenIdOptions _options = options.Value;
 
     public async Task<Result> Handle(TokenCommand request, CancellationToken cancellationToken)
     {
+        if (string.IsNullOrEmpty(request.Request.GrantType))
+            return Results.BadRequest(new Error()
+            {
+                Code = Errors.OAuth.InvalidRequest,
+                Description = $"'grant_type' param is required"
+            });
+        
+        if (string.IsNullOrEmpty(request.Request.ClientId))
+            return Results.BadRequest(new Error()
+            {
+                Code = Errors.OAuth.InvalidRequest,
+                Description = $"'client_id' param is required"
+            });
+        
+        if (string.IsNullOrEmpty(request.Request.RedirectUri))
+            return Results.BadRequest(new Error()
+            {
+                Code = Errors.OAuth.InvalidRequest,
+                Description = $"'redirect_uri' param is required"
+            });
+        
+        if (!_options.GrantTypesSupported.Contains(request.Request.GrantType))
+            return Results.BadRequest(new Error()
+            {
+                Code = Errors.OAuth.InvalidGrant,
+                Description = $"'{request.Request.GrantType}' grant type is not supported"
+            });
+
         TokenPayload payload = request.Request.GrantType switch
         {
             GrantTypes.AuthorizationCode => new AuthorizationCodeTokenPayload()
@@ -20,9 +52,9 @@ public class TokenCommandHandler(ITokenStrategyResolver tokenStrategyResolver) :
                 ClientId = request.Request.ClientId,
                 GrantType = request.Request.GrantType,
                 ClientSecret = request.Request.ClientSecret,
-                Code = request.Request.Code,
                 CodeVerifier = request.Request.CodeVerifier,
-                RedirectUri = request.Request.RedirectUri
+                RedirectUri = request.Request.RedirectUri,
+                Code = request.Request.Code
             },
             GrantTypes.RefreshToken => new RefreshTokenPayload()
             {
@@ -34,7 +66,7 @@ public class TokenCommandHandler(ITokenStrategyResolver tokenStrategyResolver) :
             },
             _ => throw new NotSupportedException("Unsupported grant type")
         };
-        
+
         var strategy = _tokenStrategyResolver.Resolve(request.Request.GrantType);
         return await strategy.ExecuteAsync(payload, cancellationToken);
     }
