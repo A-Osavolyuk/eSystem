5461d53d70bc1fa3a67287d9f7b84da62a7e669a
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-11-19 11:56:48 +0200

    Configure JSON serialization for OIDC token flow request and response

--- eSecurity.Core/Common/Requests/TokenRequest.cs
+++ eSecurity.Core/Common/Requests/TokenRequest.cs
@@ -1,13 +1,30 @@
+using System.Text.Json.Serialization;
+
 namespace eSecurity.Core.Common.Requests;
 
 public class TokenRequest
 {
+    [JsonPropertyName("grant_type")]
     public required string GrantType { get; set; }
+    
+    [JsonPropertyName("client_id")]
     public required string ClientId { get; set; }
+    
+    [JsonPropertyName("redirect_uri")]
     public string? RedirectUri { get; set; }
+    
+    [JsonPropertyName("refresh_token")]
     public string? RefreshToken { get; set; }
+    
+    [JsonPropertyName("code")]
     public string? Code { get; set; }
+    
+    [JsonPropertyName("client_secret")]
     public string? ClientSecret { get; set; }
+    
+    [JsonPropertyName("code_verifier")]
     public string? CodeVerifier { get; set; }
+    
+    [JsonPropertyName("scope")]
     public string? Scope { get; set; }
 }
