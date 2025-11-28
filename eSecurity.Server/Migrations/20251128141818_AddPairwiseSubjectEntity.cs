e0157939a0020d0a7cbfebd6b8e3697c78645246
AleksanDOOR <sasha.osavoll11@gmail.com>
2025-11-28 16:18:31 +0200

    Re-migrate DB

---
+++ eSecurity.Server/Migrations/20251128141818_AddPairwiseSubjectEntity.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eSecurity.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPairwiseSubjectEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PairwiseSubjects",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ClientId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SectorIdentifier = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    SubjectIdentifier = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    CreateDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    UpdateDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PairwiseSubjects", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PairwiseSubjects_Clients_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Clients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PairwiseSubjects_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PairwiseSubjects_ClientId",
+                table: "PairwiseSubjects",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PairwiseSubjects_UserId",
+                table: "PairwiseSubjects",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PairwiseSubjects");
+        }
+    }
+}
