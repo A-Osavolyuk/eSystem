948bf49e3c25f905bd1d99c3738f8aa5ee2debca
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2025-11-30 09:56:36 +0200

    Refactor OIDC configuration

--- eSecurity.Server/Security/Authentication/AuthenticationExtensions.cs
+++ eSecurity.Server/Security/Authentication/AuthenticationExtensions.cs
@@ -2,12 +2,16 @@
 using eSecurity.Server.Security.Authentication.Handlers;
 using eSecurity.Server.Security.Authentication.Lockout;
 using eSecurity.Server.Security.Authentication.Oidc;
+using eSecurity.Server.Security.Authentication.Oidc.Constants;
 using eSecurity.Server.Security.Authentication.Password;
 using eSecurity.Server.Security.Authentication.SignIn;
 using eSecurity.Server.Security.Authentication.TwoFactor;
 using eSecurity.Server.Security.Authorization.OAuth;
 using eSecurity.Server.Security.Authorization.OAuth.Schemes;
 using eSystem.Core.Common.Configuration;
+using eSystem.Core.Security.Authentication.Oidc;
+using eSystem.Core.Security.Authentication.Oidc.Authorization;
+using eSystem.Core.Security.Authentication.Oidc.Token;
 using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace eSecurity.Server.Security.Authentication;
@@ -22,7 +26,60 @@ public static class AuthenticationExtensions
         builder.Services.AddSignInStrategies();
         builder.Services.Add2Fa();
         builder.Services.AddLockout();
-        builder.Services.AddOidc();
+        builder.Services.AddOidc(cfg =>
+            {
+                cfg.Issuer = "https://localhost:6201";
+                cfg.AuthorizationEndpoint = "https://localhost:6501/connect/authorize";
+                cfg.EndSessionEndpoint = "https://localhost:6501/connect/logout";
+                cfg.TokenEndpoint = "https://localhost:6201/api/v1/connect/token";
+                cfg.UserinfoEndpoint = "https://localhost:6201/api/v1/connect/userinfo";
+                cfg.IntrospectionEndpoint = "https://localhost:6201/api/v1/connect/introspection";
+                cfg.RevocationEndpoint = "https://localhost:6201/api/v1/connect/revocation";
+                cfg.JwksUri = "https://localhost:6201/api/v1/connect/jwks.json";
+
+                cfg.ResponseTypesSupported = [ResponseTypes.Code];
+                cfg.GrantTypesSupported =
+                [
+                    GrantTypes.AuthorizationCode,
+                    GrantTypes.RefreshToken
+                ];
+                
+                cfg.PromptValuesSupported =
+                [
+                    Prompts.None,
+                    Prompts.Login,
+                    Prompts.Consent,
+                    Prompts.SelectAccount
+                ];
+
+                cfg.SubjectTypesSupported = [SubjectTypes.Public, SubjectTypes.Pairwise];
+                cfg.IdTokenSigningAlgValuesSupported = [SecurityAlgorithms.RsaSha256];
+                cfg.TokenEndpointAuthMethodsSupported =
+                [
+                    TokenAuthMethods.ClientSecretBasic,
+                    TokenAuthMethods.ClientSecretJwt,
+                    TokenAuthMethods.ClientSecretPost,
+                    TokenAuthMethods.PrivateKeyJwt,
+                    TokenAuthMethods.None
+                ];
+
+                cfg.CodeChallengeMethodsSupported = [ChallengeMethods.S256];
+                cfg.ScopesSupported =
+                [
+                    Scopes.OfflineAccess,
+                    Scopes.OpenId,
+                    Scopes.Email,
+                    Scopes.Profile,
+                    Scopes.Phone,
+                    Scopes.Address
+                ];
+
+                cfg.BackchannelLogoutSupported = false;
+                cfg.BackchannelLogoutSessionSupported = false;
+
+                cfg.FrontchannelLogoutSupported = true;
+                cfg.FrontchannelLogoutSessionSupported = true;
+            });
         
         builder.Services.AddAuthentication(options =>
             {
@@ -73,6 +130,5 @@ public static class AuthenticationExtensions
                 JwtBearerDefaults.AuthenticationScheme, _ => {})
             .AddScheme<BasicAuthenticationSchemeOptions, BasicAuthenticationHandler>(
                 BasicAuthenticationDefaults.AuthenticationScheme, _ => {});
-        ;
     }
 }
--- eSecurity.Server/Security/Authentication/Oidc/OdicExtensions.cs
+++ eSecurity.Server/Security/Authentication/Oidc/OdicExtensions.cs
@@ -20,112 +20,30 @@ public static class OdicExtensions
 {
     extension(IServiceCollection services)
     {
-        public void AddOidc()
-        {
-            services.AddPkceHandler();
-            services.AddTokenFlow();
-            services.AddIntrospectionFlow();
-            services.AddClientManagement();
-            services.AddAuthorizationCodeManagement();
-            services.AddSession(cfg => { cfg.Timestamp = TimeSpan.FromDays(30); });
-
-            services.AddOpenidConfiguration(cfg =>
-            {
-                cfg.Issuer = "https://localhost:6201";
-                cfg.AuthorizationEndpoint = "https://localhost:6501/connect/authorize";
-                cfg.EndSessionEndpoint = "https://localhost:6501/connect/logout";
-                cfg.TokenEndpoint = "https://localhost:6201/api/v1/connect/token";
-                cfg.UserinfoEndpoint = "https://localhost:6201/api/v1/connect/userinfo";
-                cfg.IntrospectionEndpoint = "https://localhost:6201/api/v1/connect/introspection";
-                cfg.RevocationEndpoint = "https://localhost:6201/api/v1/connect/revocation";
-                cfg.JwksUri = "https://localhost:6201/api/v1/connect/jwks.json";
-
-                cfg.ResponseTypesSupported = [ResponseTypes.Code];
-                cfg.GrantTypesSupported =
-                [
-                    GrantTypes.AuthorizationCode,
-                    GrantTypes.RefreshToken
-                ];
-                
-                cfg.PromptValuesSupported =
-                [
-                    Prompts.None,
-                    Prompts.Login,
-                    Prompts.Consent,
-                    Prompts.SelectAccount
-                ];
-
-                cfg.SubjectTypesSupported = [SubjectTypes.Public, SubjectTypes.Pairwise];
-                cfg.IdTokenSigningAlgValuesSupported = [SecurityAlgorithms.RsaSha256];
-                cfg.TokenEndpointAuthMethodsSupported =
-                [
-                    TokenAuthMethods.ClientSecretBasic,
-                    TokenAuthMethods.ClientSecretJwt,
-                    TokenAuthMethods.ClientSecretPost,
-                    TokenAuthMethods.PrivateKeyJwt,
-                    TokenAuthMethods.None
-                ];
-
-                cfg.CodeChallengeMethodsSupported = [ChallengeMethods.S256];
-                cfg.ScopesSupported =
-                [
-                    Scopes.OfflineAccess,
-                    Scopes.OpenId,
-                    Scopes.Email,
-                    Scopes.Profile,
-                    Scopes.Phone,
-                    Scopes.Address
-                ];
-
-                cfg.BackchannelLogoutSupported = false;
-                cfg.BackchannelLogoutSessionSupported = false;
-
-                cfg.FrontchannelLogoutSupported = true;
-                cfg.FrontchannelLogoutSessionSupported = true;
-            });
-        }
-
-        private void AddClientManagement()
+        public void AddOidc(Action<OpenIdOptions> configure)
         {
             services.AddScoped<IClientManager, ClientManager>();
-        }
-
-        private void AddAuthorizationCodeManagement()
-        {
             services.AddScoped<IAuthorizationCodeManager, AuthorizationCodeManager>();
-        }
-
-        private void AddOpenidConfiguration(Action<OpenIdOptions> configure)
-        {
-            services.Configure(configure);
-        }
-
-        private void AddPkceHandler()
-        {
             services.AddScoped<IPkceHandler, PkceHandler>();
+            
+            services.AddScoped<ITokenManager, TokenManager>();
+            services.AddScoped<ITokenValidator, TokenValidator>();
+            services.AddScoped<ITokenStrategyResolver, TokenStrategyResolver>();
+            services.AddKeyedScoped<ITokenStrategy, AuthorizationCodeStrategy>(GrantTypes.AuthorizationCode);
+            services.AddKeyedScoped<ITokenStrategy, RefreshTokenStrategy>(GrantTypes.RefreshToken);
+            services.AddSingleton<IClaimBuilderFactory, ClaimBuilderFactory>();
+            
+            services.AddScoped<IIntrospectionResolver, IntrospectionResolver>();
+            services.AddKeyedScoped<IIntrospectionStrategy, JwtTokenIntrospectionStrategy>(IntrospectionType.Jwt);
+            services.AddKeyedScoped<IIntrospectionStrategy, ReferenceTokenIntrospectionStrategy>(IntrospectionType.Reference);
+            
+            services.AddSession(cfg => { cfg.Timestamp = TimeSpan.FromDays(30); });
         }
 
         private void AddSession(Action<SessionOptions> configure)
         {
             services.AddScoped<ISessionManager, SessionManager>();
             services.Configure(configure);
-        }
-
-        private void AddTokenFlow()
-        {
-            services.AddScoped<ITokenManager, TokenManager>();
-            services.AddScoped<ITokenValidator, TokenValidator>();
-            services.AddSingleton<IClaimBuilderFactory, ClaimBuilderFactory>();
-            services.AddScoped<ITokenStrategyResolver, TokenStrategyResolver>();
-            services.AddKeyedScoped<ITokenStrategy, AuthorizationCodeStrategy>(GrantTypes.AuthorizationCode);
-            services.AddKeyedScoped<ITokenStrategy, RefreshTokenStrategy>(GrantTypes.RefreshToken);
-        }
-
-        private void AddIntrospectionFlow()
-        {
-            services.AddScoped<IIntrospectionResolver, IntrospectionResolver>();
-            services.AddKeyedScoped<IIntrospectionStrategy, JwtTokenIntrospectionStrategy>(IntrospectionType.Jwt);
-            services.AddKeyedScoped<IIntrospectionStrategy, ReferenceTokenIntrospectionStrategy>(IntrospectionType.Reference);
         }
     }
 }
