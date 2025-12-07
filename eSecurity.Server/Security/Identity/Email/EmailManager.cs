c4745cf63a9476ed1ea12d4170f6c3f93a64a417
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2025-12-07 23:19:46 +0200

    Add EmailManager

---
+++ eSecurity.Server/Security/Identity/Email/EmailManager.cs
@@ -0,0 +1,39 @@
+﻿using eSecurity.Core.Security.Identity;
+using eSecurity.Server.Data;
+using eSecurity.Server.Data.Entities;
+
+namespace eSecurity.Server.Security.Identity.Email;
+
+public class EmailManager(AuthDbContext context) : IEmailManager
+{
+    private readonly AuthDbContext _context = context;
+
+    public async ValueTask<List<UserEmailEntity>> GetAllAsync(UserEntity user, CancellationToken cancellationToken)
+    {
+        return await _context.UserEmails
+            .Where(x => x.UserId == user.Id)
+            .ToListAsync(cancellationToken);
+    }
+
+    public async ValueTask<List<UserEmailEntity>> GetAllAsync(UserEntity user, EmailType type,
+        CancellationToken cancellationToken)
+    {
+        return await _context.UserEmails
+            .Where(x => x.UserId == user.Id && x.Type == type)
+            .ToListAsync(cancellationToken);
+    }
+
+    public async ValueTask<UserEmailEntity?> FindByTypeAsync(UserEntity user, EmailType type,
+        CancellationToken cancellationToken)
+    {
+        return await _context.UserEmails.FirstOrDefaultAsync(
+            x => x.UserId == user.Id && x.Type == type, cancellationToken);
+    }
+
+    public async ValueTask<UserEmailEntity?> FindByEmailAsync(UserEntity user, string email,
+        CancellationToken cancellationToken)
+    {
+        return await _context.UserEmails.FirstOrDefaultAsync(
+            x => x.UserId == user.Id && x.Email == email, cancellationToken);
+    }
+}
---
+++ eSecurity.Server/Security/Identity/Email/IEmailManager.cs
@@ -0,0 +1,18 @@
+﻿using eSecurity.Core.Security.Identity;
+using eSecurity.Server.Data.Entities;
+
+namespace eSecurity.Server.Security.Identity.Email;
+
+public interface IEmailManager
+{
+    public ValueTask<List<UserEmailEntity>> GetAllAsync(UserEntity user, CancellationToken cancellationToken);
+    
+    public ValueTask<List<UserEmailEntity>> GetAllAsync(UserEntity user, EmailType type, 
+        CancellationToken cancellationToken);
+    
+    public ValueTask<UserEmailEntity?> FindByTypeAsync(UserEntity user, EmailType type, 
+        CancellationToken cancellationToken);
+    
+    public ValueTask<UserEmailEntity?> FindByEmailAsync(UserEntity user, string email, 
+        CancellationToken cancellationToken);
+}
