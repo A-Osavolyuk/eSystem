2b0972e98a05f9036f80812dcea1c37ce92c8fd8
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2025-12-07 23:19:56 +0200

    Add PhoneManager

---
+++ eSecurity.Server/Security/Identity/Phone/IPhoneManager.cs
@@ -0,0 +1,18 @@
+﻿using eSecurity.Core.Security.Identity;
+using eSecurity.Server.Data.Entities;
+
+namespace eSecurity.Server.Security.Identity.Phone;
+
+public interface IPhoneManager
+{
+    public ValueTask<List<UserPhoneNumberEntity>> GetAllAsync(UserEntity user, CancellationToken cancellationToken);
+    
+    public ValueTask<List<UserPhoneNumberEntity>> GetAllAsync(UserEntity user, PhoneNumberType type, 
+        CancellationToken cancellationToken);
+    
+    public ValueTask<UserPhoneNumberEntity?> FindByTypeAsync(UserEntity user, PhoneNumberType type, 
+        CancellationToken cancellationToken);
+    
+    public ValueTask<UserPhoneNumberEntity?> FindByPhoneAsync(UserEntity user, string phone, 
+        CancellationToken cancellationToken);
+}
---
+++ eSecurity.Server/Security/Identity/Phone/PhoneManager.cs
@@ -0,0 +1,40 @@
+﻿using eSecurity.Core.Security.Identity;
+using eSecurity.Server.Data;
+using eSecurity.Server.Data.Entities;
+
+namespace eSecurity.Server.Security.Identity.Phone;
+
+public class PhoneManager(AuthDbContext context) : IPhoneManager
+{
+    private readonly AuthDbContext _context = context;
+
+    public async ValueTask<List<UserPhoneNumberEntity>> GetAllAsync(UserEntity user,
+        CancellationToken cancellationToken)
+    {
+        return await _context.UserPhoneNumbers
+            .Where(x => x.UserId == user.Id)
+            .ToListAsync(cancellationToken);
+    }
+
+    public async ValueTask<List<UserPhoneNumberEntity>> GetAllAsync(UserEntity user, PhoneNumberType type,
+        CancellationToken cancellationToken)
+    {
+        return await _context.UserPhoneNumbers
+            .Where(x => x.UserId == user.Id && x.Type == type)
+            .ToListAsync(cancellationToken);
+    }
+
+    public async ValueTask<UserPhoneNumberEntity?> FindByTypeAsync(UserEntity user, PhoneNumberType type,
+        CancellationToken cancellationToken)
+    {
+        return await _context.UserPhoneNumbers.FirstOrDefaultAsync(
+            x => x.UserId == user.Id && x.Type == type, cancellationToken);
+    }
+
+    public async ValueTask<UserPhoneNumberEntity?> FindByPhoneAsync(UserEntity user, string phone,
+        CancellationToken cancellationToken)
+    {
+        return await _context.UserPhoneNumbers.FirstOrDefaultAsync(
+            x => x.UserId == user.Id && x.PhoneNumber == phone, cancellationToken);
+    }
+}
