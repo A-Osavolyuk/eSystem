bedbdc428645002cb943bdfbacb31bf28beaf950
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-01-04 21:01:31 +0100

    Implement username set functionality

---
+++ eSecurity.Core/Common/Requests/SetUsernameRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace eSecurity.Core.Common.Requests;
+
+public class SetUsernameRequest
+{
+    public required Guid UserId { get; set; }
+    public required string Username { get; set; }
+}
---
+++ eSecurity.Server/Features/Username/Commands/SetUsernameCommand.cs
@@ -0,0 +1,28 @@
+﻿using eSecurity.Core.Common.Requests;
+using eSecurity.Server.Security.Identity.User;
+
+namespace eSecurity.Server.Features.Username.Commands;
+
+public sealed record SetUsernameCommand(SetUsernameRequest Request) : IRequest<Result>;
+
+public sealed class SetUsernameCommandHandler(IUserManager userManager) : IRequestHandler<SetUsernameCommand, Result>
+{
+    private readonly IUserManager _userManager = userManager;
+
+    public async Task<Result> Handle(SetUsernameCommand request, CancellationToken cancellationToken)
+    {
+        var user = await _userManager.FindByIdAsync(request.Request.UserId, cancellationToken);
+        if (user is null) return Results.NotFound("User not found");
+
+        if (await _userManager.IsUsernameTakenAsync(request.Request.Username, cancellationToken))
+        {
+            return Results.BadRequest(new Error()
+            {
+                Code = Errors.Common.UsernameTaken,
+                Description = "The username is already taken."
+            });
+        }
+
+        return await _userManager.SetUsernameAsync(user, request.Request.Username, cancellationToken);
+    }
+}
