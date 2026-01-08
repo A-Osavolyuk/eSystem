9e2388012e04e66b812d74ccc2d06ed56cd15d74
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-01-08 17:19:56 +0100

    Refactor SignInSessionDto

--- eSecurity.Core/Common/DTOs/SignInSessionDto.cs
+++ eSecurity.Core/Common/DTOs/SignInSessionDto.cs
@@ -1,7 +1,17 @@
-﻿namespace eSecurity.Core.Common.DTOs;
+﻿using System.Text.Json.Serialization;
+using eSecurity.Core.Security.Authentication.SignIn.Session;
+
+namespace eSecurity.Core.Common.DTOs;
 
 public class SignInSessionDto
 {
+    [JsonPropertyName("sid")]
     public Guid Id { get; set; }
+    
+    [JsonPropertyName("user_id")]
     public Guid UserId { get; set; }
+    
+    [JsonPropertyName("next_step")]
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public SignInStep NextStep { get; set; }
 }
