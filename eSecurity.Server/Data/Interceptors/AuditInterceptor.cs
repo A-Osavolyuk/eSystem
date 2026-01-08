6685a5d86eaf579fe382bf23ce83e94291dee848
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-01-09 00:09:50 +0100

    Implement audit interceptor

---
+++ eSecurity.Server/Data/Interceptors/AuditInterceptor.cs
@@ -0,0 +1,30 @@
+﻿using eSystem.Core.Data.Entities;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace eSecurity.Server.Data.Interceptors;
+
+public class AuditInterceptor : SaveChangesInterceptor
+{
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(
+        DbContextEventData eventData,
+        InterceptionResult<int> result,
+        CancellationToken cancellationToken = new CancellationToken())
+    {
+        var context = eventData.Context!;
+        
+        foreach (var entry in context.ChangeTracker.Entries<Entity>())
+        {
+            if (entry.State == EntityState.Added)
+            {
+                entry.Entity.CreateDate = DateTime.Now;
+            }
+            else if (entry.State == EntityState.Modified)
+            {
+                entry.Property(e => e.CreateDate).IsModified = false;
+                entry.Entity.UpdateDate = DateTime.Now;
+            }
+        }
+
+        return base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+}
