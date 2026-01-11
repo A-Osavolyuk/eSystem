e96616e9c23fadd77e293ac2341faa57464922c2
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-01-11 16:28:20 +0100

    Add eCinema.Server projest with basic configuration

---
+++ src/eCinema.Server/Extensions/HostApplicationBuilderExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json.Serialization;
+using eSystem.Core.Common.Documentation;
+using eSystem.Core.Common.Errors;
+using eSystem.Core.Common.Network.Gateway;
+using eSystem.Core.Common.Versioning;
+using eSystem.ServiceDefaults;
+
+namespace eCinema.Server.Extensions;
+
+public static class HostApplicationBuilderExtensions
+{
+    extension(IHostApplicationBuilder builder)
+    {
+        public void AddServices()
+        {
+            builder.AddServiceDefaults();
+            builder.AddExceptionHandler();
+            builder.AddDocumentation();
+            builder.AddVersioning();
+
+            builder.Services.AddOpenApi();
+            builder.Services.AddGateway();
+            builder.Services.AddHttpContextAccessor();
+            builder.Services.AddDistributedMemoryCache();
+            builder.Services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(5);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+
+            builder.Services.AddControllers()
+                .AddJsonOptions(cfg =>
+                {
+                    cfg.JsonSerializerOptions.WriteIndented = true;
+                    cfg.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+                });;
+        }
+    }
+}
---
+++ src/eCinema.Server/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,23 @@
+﻿using eSystem.ServiceDefaults;
+using Scalar.AspNetCore;
+
+namespace eCinema.Server.Extensions;
+
+public static class WebApplicationExtensions
+{
+    extension(WebApplication app)
+    {
+        public void MapServices()
+        {
+            app.UseExceptionHandler();
+            app.UseRouting();
+            app.UseSession();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.MapOpenApi();
+            app.MapScalarApiReference();
+            app.MapDefaultEndpoints();
+        }
+    }
+}
