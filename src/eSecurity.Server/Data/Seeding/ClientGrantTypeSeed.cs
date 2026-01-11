d8bd9f263ead36580614a0280a32e6e54a8819b6
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-01-11 16:55:53 +0100

    Add default seed for eCinema.Client client

--- src/eSecurity.Server/Data/Seeding/ClientGrantTypeSeed.cs
+++ src/eSecurity.Server/Data/Seeding/ClientGrantTypeSeed.cs
@@ -10,6 +10,7 @@ public class ClientGrantTypeSeed : Seed<ClientGrantTypeEntity>
     {
         return
         [
+            // eSecurity.Client
             new ClientGrantTypeEntity()
             {
                 Id = Guid.CreateVersion7(),
@@ -22,17 +23,19 @@ public class ClientGrantTypeSeed : Seed<ClientGrantTypeEntity>
                 ClientId = Guid.Parse("392e390f-33bd-4f30-af70-ccbe04bbb2c4"),
                 Type = GrantTypes.AuthorizationCode,
             },
+            
+            // eCinema.Client
             new ClientGrantTypeEntity()
             {
                 Id = Guid.CreateVersion7(),
-                ClientId = Guid.Parse("392e390f-33bd-4f30-af70-ccbe04bbb2c4"),
-                Type = GrantTypes.ClientCredentials,
+                ClientId = Guid.Parse("307268b0-005c-4ee4-a0e8-a93bd0010382"),
+                Type = GrantTypes.RefreshToken,
             },
             new ClientGrantTypeEntity()
             {
                 Id = Guid.CreateVersion7(),
-                ClientId = Guid.Parse("392e390f-33bd-4f30-af70-ccbe04bbb2c4"),
-                Type = GrantTypes.DeviceCode,
+                ClientId = Guid.Parse("307268b0-005c-4ee4-a0e8-a93bd0010382"),
+                Type = GrantTypes.AuthorizationCode,
             },
         ];
     }
--- src/eSecurity.Server/Data/Seeding/ClientUriSeed.cs
+++ src/eSecurity.Server/Data/Seeding/ClientUriSeed.cs
@@ -10,6 +10,7 @@ public class ClientUriSeed : Seed<ClientUriEntity>
     {
         return
         [
+            // eSecurity.Client
             new ClientUriEntity()
             {
                 Id = Guid.CreateVersion7(),
@@ -30,6 +31,29 @@ public class ClientUriSeed : Seed<ClientUriEntity>
                 ClientId = Guid.Parse("392e390f-33bd-4f30-af70-ccbe04bbb2c4"),
                 Type = UriType.FrontChannelLogout,
                 Uri = "https://localhost:6501/connect/logout/callback",
+            },
+
+            // eCinema.Client
+            new ClientUriEntity()
+            {
+                Id = Guid.CreateVersion7(),
+                ClientId = Guid.Parse("307268b0-005c-4ee4-a0e8-a93bd0010382"),
+                Type = UriType.Redirect,
+                Uri = "https://localhost:6202/connect/callback",
+            },
+            new ClientUriEntity()
+            {
+                Id = Guid.CreateVersion7(),
+                ClientId = Guid.Parse("307268b0-005c-4ee4-a0e8-a93bd0010382"),
+                Type = UriType.PostLogoutRedirect,
+                Uri = "https://localhost:6502/connect/logged-out",
+            },
+            new()
+            {
+                Id = Guid.CreateVersion7(),
+                ClientId = Guid.Parse("307268b0-005c-4ee4-a0e8-a93bd0010382"),
+                Type = UriType.FrontChannelLogout,
+                Uri = "https://localhost:6502/connect/logout/callback",
             }
         ];
     }
