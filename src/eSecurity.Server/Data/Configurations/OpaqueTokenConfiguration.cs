c309472641a5efaaa98413c40a850ed9eaa97016
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-01-12 21:20:12 +0100

    Increase OpaqueTokenEntity TokenHash property from 100 to 500

--- src/eSecurity.Server/Data/Configurations/OpaqueTokenConfiguration.cs
+++ src/eSecurity.Server/Data/Configurations/OpaqueTokenConfiguration.cs
@@ -9,7 +9,7 @@ public sealed class OpaqueTokenConfiguration : IEntityTypeConfiguration<OpaqueTo
     public void Configure(EntityTypeBuilder<OpaqueTokenEntity> builder)
     {
         builder.HasKey(x => x.Id);
-        builder.Property(x => x.TokenHash).HasMaxLength(100);
+        builder.Property(x => x.TokenHash).HasMaxLength(500);
         builder.Property(x => x.TokenType).HasConversion<string>();
             
         builder.HasOne(x => x.Client)
