d1659e4a6ae29a43d7bb545e7662df507bee0994
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-01-12 13:15:16 +0100

    Add support for Google Material Icons/Symbols

---
+++ src/eSecurity.Client/Common/Assets/GoogleIcons.MaterialIcons.cs
@@ -0,0 +1,35 @@
+﻿namespace eSecurity.Client.Common.Assets;
+
+public static partial class GoogleIcons
+{
+    /// <summary>
+    /// Google Material Icons SVG icons.
+    /// </summary>
+    public static class MaterialIcons
+    {
+        public static class Outlined
+        {
+            
+        }
+        
+        public static class Filled
+        {
+            
+        }
+        
+        public static class Rounded
+        {
+            
+        }
+
+        public static class Sharp
+        {
+            
+        }
+        
+        public static class TwoTone
+        {
+            
+        }
+    }
+}
