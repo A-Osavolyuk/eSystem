3d784513bdf58b5cf2f55e79639cb782c0c675df
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-01-12 21:20:21 +0100

    Re-migrate DB

---
+++ src/eSecurity.Server/Migrations/20260112201515_IncreaseTokenHashLength.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eSecurity.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncreaseTokenHashLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TokenHash",
+                table: "OpaqueTokens",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TokenHash",
+                table: "OpaqueTokens",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+        }
+    }
+}
