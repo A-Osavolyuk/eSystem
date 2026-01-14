fc1d6a13f902e52e16fc01d09216f81cd77769b7
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-01-14 13:34:05 +0100

    Fix issue with incorrect query parameters formation on OIDC authorization flow

--- src/eSecurity.Client/Security/Authentication/AuthenticationManager.cs
+++ src/eSecurity.Client/Security/Authentication/AuthenticationManager.cs
@@ -1,3 +1,4 @@
+using System.Web;
 using eSecurity.Client.Common.JS.Fetch;
 using eSecurity.Client.Common.Storage;
 using eSecurity.Client.Security.Authentication.Oidc.Clients;
@@ -25,18 +26,31 @@ public sealed class AuthenticationManager(
 
     public void Authorize()
     {
-        var redirectUri = QueryBuilder.Create()
-            .WithUri(Links.Connect.Authorize)
-            .WithQueryParam("response_type", ResponseTypes.Code)
-            .WithQueryParam("client_id", _clientOptions.ClientId)
-            .WithQueryParam("redirect_uri", _clientOptions.CallbackUri)
-            .WithQueryParam("scope", string.Join(" ", _clientOptions.Scopes))
-            .WithQueryParam("nonce", Guid.NewGuid().ToString())
-            .WithQueryParam("state", Guid.NewGuid().ToString())
-            .WithQueryParam("prompt", Prompts.Consent)
-            .Build();
-        
-        _navigationManager.NavigateTo(redirectUri);
+        var decodedUri = HttpUtility.UrlDecode(_navigationManager.Uri);
+        var queryParams = QueryParser.GetQueryParameters(decodedUri);
+
+        var builder = QueryBuilder.Create().WithUri(Links.Connect.Authorize)
+            .WithQueryParam("prompt", Prompts.Consent);
+
+        builder.WithQueryParam("response_type", queryParams.GetValueOrDefault("response_type", ResponseTypes.Code))
+            .WithQueryParam("client_id", queryParams.GetValueOrDefault("client_id", _clientOptions.ClientId))
+            .WithQueryParam("redirect_uri", queryParams.GetValueOrDefault("redirect_uri", _clientOptions.CallbackUri))
+            .WithQueryParam("scope", queryParams.GetValueOrDefault("scope", string.Join(" ", _clientOptions.Scopes)))
+            .WithQueryParam("state", queryParams.GetValueOrDefault("state", Guid.NewGuid().ToString()))
+            .WithQueryParam("nonce", queryParams.GetValueOrDefault("nonce", Guid.NewGuid().ToString()));
+
+        if (queryParams.TryGetValue("prompt", out var prompt))
+        {
+            var prompts = prompt.Split(' ');
+            if (prompts.Length > 1)
+            {
+                builder.WithQueryParam("prompt", string.Join(" ", prompts.Skip(1)));
+            }
+        }
+        else 
+            builder.WithQueryParam("prompt", Prompts.Consent);
+
+        _navigationManager.NavigateTo(builder.Build());
     }
 
     public async Task SignOutAsync()
@@ -54,7 +68,7 @@ public sealed class AuthenticationManager(
             await _storage.ClearAsync();
         }
     }
-    
+
     public async Task LogoutAsync()
     {
         var fetchOptions = new FetchOptions()
