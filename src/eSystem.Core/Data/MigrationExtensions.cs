960b4ee3509930babbb3cee4e1d603b29511e321
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-01-14 12:04:47 +0100

    Fix issue with DB migration

--- src/eSystem.Core/Data/MigrationExtensions.cs
+++ src/eSystem.Core/Data/MigrationExtensions.cs
@@ -1,44 +1,18 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace eSystem.Core.Data;
 
 public static class MigrationExtensions
 {
-    extension(WebApplication app)
+    public static async Task ConfigureDatabaseAsync<TDbContext>(
+        this WebApplication app)
+        where TDbContext : DbContext
     {
-        public async Task ConfigureDatabaseAsync<TDbContext>() where TDbContext : DbContext
-        {
-            using var score = app.Services.CreateScope();
-            var context = score.ServiceProvider.GetRequiredService<TDbContext>();
-            await EnsureDatabaseAsync(context);
-            await RunMigrationsAsync(context);
-        }
-    }
+        using var scope = app.Services.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<TDbContext>();
 
-    private static async Task EnsureDatabaseAsync(DbContext context)
-    {
-        var dbCreator = context.GetService<IRelationalDatabaseCreator>();
-        if (!await context.Database.CanConnectAsync())
-        {
-            await dbCreator.CreateAsync();
-        }
-    }
-
-    private static async Task RunMigrationsAsync(DbContext context)
-    {
-        if (!await IsDatabaseMigratedAsync(context))
-        {
-            await context.Database.MigrateAsync();
-        }
-    }
-    
-    private static async Task<bool> IsDatabaseMigratedAsync(DbContext context)
-    {
-        var pendingMigrations = await context.Database.GetPendingMigrationsAsync();
-        return !pendingMigrations.Any();
+        await context.Database.MigrateAsync();
     }
 }
