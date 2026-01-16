7c2f19fe043d891c58c2b1cf030a0b37c7bd312a
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-01-16 21:29:44 +0100

    Remove AuthorizationState as redundant

--- src/eCinema.Server/Security/Authentication/Oidc/AuthorizationState.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text.Json.Serialization;
-
-namespace eCinema.Server.Security.Authentication.Oidc;
-
-public class AuthorizationState
-{
-    [JsonPropertyName("redirect_uri")]
-    public required string RedirectUri { get; set; }
-}
