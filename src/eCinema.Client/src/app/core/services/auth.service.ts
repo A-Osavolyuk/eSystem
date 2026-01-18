49e5edc21eb83ada4efb933d8692ffac1f842ba5
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-01-18 16:48:22 +0100

    Implement authentication service

---
+++ src/eCinema.Client/src/app/core/services/auth.service.ts
@@ -0,0 +1,29 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../../environments/environment';
+import {User} from '../interfaces/user.interface';
+import {catchError, Observable, of} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private httpClient = inject(HttpClient)
+
+  public getMe = () : Observable<User | null> => {
+    return this.httpClient.get<User | null>(`${environment.backendUri}/api/v1/authentication/me`)
+      .pipe(
+        catchError((error) => {
+          console.error('[AuthService] getMe error', error);
+          return of(null);
+      }));
+  }
+
+  public authorize = () : void => {
+    document.location.href = `${environment.backendUri}/api/v1/connect/authorize`;
+  }
+
+  public logout = () : void => {
+    document.location.href = `${environment.backendUri}/api/v1/connect/logout`;
+  }
+}
