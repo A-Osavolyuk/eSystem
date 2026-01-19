6d6e83f38ff65321efba72a1a114a5eec673c3e8
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-01-19 16:26:31 +0100

    Cover eSystem.Core.Http project with unit tests

---
+++ tests/System.Core.Http.Tests/ApiResponseTests.cs
@@ -0,0 +1,78 @@
+﻿using eSystem.Core.Http;
+using eSystem.Core.Http.Results;
+
+namespace System.Core.Http.Tests;
+
+public class ApiResponseTests
+{
+    [Fact]
+    public void TryGetValue_WhenResultIsNull_ShouldReturnFalseAndDefaultValue()
+    {
+        //Arrange
+        var response = ApiResponse.Success();
+
+        //Act
+        var result = response.TryGetValue<bool>(out var value);
+
+        //Assert
+        Assert.False(result);
+        Assert.False(value);
+    }
+    
+    [Fact]
+    public void TryGetValue_WhenResultIsInvalidJson_ShouldReturnFalseAndDefaultValue()
+    {
+        //Arrange
+        var response = ApiResponse.Success("{");
+
+        //Act
+        var result = response.TryGetValue<string>(out var value);
+
+        //Assert
+        Assert.False(result);
+        Assert.Null(value);
+    }
+    
+    [Fact]
+    public void TryGetValue_WhenResultIsValidJsonButTypeIsIncompatible_ShouldReturnFalseAndDefaultValue()
+    {
+        //Arrange
+        var response = ApiResponse.Success("\"string\"");
+
+        //Act
+        var result = response.TryGetValue<int>(out var value);
+
+        //Assert
+        Assert.False(result);
+        Assert.Equal(0, value);
+    }
+    
+    [Fact]
+    public void TryGetValue_WhenResultIsValidAndTypeIsCompatible_ShouldReturnTrueAndString()
+    {
+        //Arrange
+        var response = ApiResponse.Success("\"string\"");
+
+        //Act
+        var result = response.TryGetValue<string>(out var value);
+
+        //Assert
+        Assert.True(result);
+        Assert.Equal("string", value);
+    }
+    
+    [Fact]
+    public void GetError_ShouldReturnError()
+    {
+        //Arrange
+        var error = new Error { Code = "error", Description = "description" };
+        var response = ApiResponse.Fail(error);
+        
+        //Act
+        var resultError = response.GetError();
+        
+        //Assert
+        Assert.False(response.Succeeded);
+        Assert.Same(resultError, error);
+    }
+}
