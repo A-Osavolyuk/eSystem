9c7317e41d692543d0eb30dbe94c4825f69ce85d
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-01-25 20:53:22 +0100

    Mark plain challenge method as obsolete

--- src/eSystem.Core/Security/Authentication/OpenIdConnect/Constants/ChallengeMethods.cs
+++ src/eSystem.Core/Security/Authentication/OpenIdConnect/Constants/ChallengeMethods.cs
@@ -2,6 +2,8 @@ namespace eSystem.Core.Security.Authentication.OpenIdConnect.Constants;
 
 public static class ChallengeMethods
 {
-    public const string Plain = "plain";
     public const string S256 = "S256";
+    
+    [Obsolete("Use 'S256' instead. 'plain' is not recommended due to security reasons.")]
+    public const string Plain = "plain";
 }
