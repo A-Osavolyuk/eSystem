90fcac9c23e68335420f0dfe57fef8ec775dc27a
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-01-25 20:53:05 +0100

    Mark redundant by OAuth 2.1 spec. response types as obsolete

--- src/eSystem.Core/Security/Authentication/OpenIdConnect/Constants/ResponseTypes.cs
+++ src/eSystem.Core/Security/Authentication/OpenIdConnect/Constants/ResponseTypes.cs
@@ -2,9 +2,15 @@
 
 public static class ResponseTypes
 {
-    public const string Token = "token";
-    public const string IdToken = "id_token";
     public const string Code = "code";
+    
+    [Obsolete("Use 'Code' instead. Removed in OAuth 2.1")]
+    public const string Token = "token";
+    
+    [Obsolete("Use 'Code' instead. Removed in OAuth 2.1")]
+    public const string IdToken = "id_token";
+    
+    [Obsolete("Use 'Code' instead. Removed in OAuth 2.1")]
     public const string Hybrid = "code id_token";
     
 }
