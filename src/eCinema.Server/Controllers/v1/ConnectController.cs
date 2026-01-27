d34d5a1b8861b4fd8cc6114291a986e225eba6a0
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-01-27 21:33:39 +0100

    Add support of frontchannel logout flow for eCinema

---
+++ src/eCinema.Server/Controllers/v1/ConnectController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCinema.Server.Controllers.v1;
+
+[ApiController]
+[AllowAnonymous]
+[Route("[controller]")]
+public class ConnectController : ControllerBase
+{
+    [HttpGet("frontchannel-logout")]
+    [EndpointSummary("Front-Channel Logout Endpoint")]
+    public IActionResult FrontChannelLogout()
+    {
+        return SignOut(
+            new AuthenticationProperties(),
+            CookieAuthenticationDefaults.AuthenticationScheme
+        );
+    }
+}
