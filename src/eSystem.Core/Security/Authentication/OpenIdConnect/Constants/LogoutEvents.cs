30e14f3e58d01449a2bc42cecb5228990f1accfb
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-01-27 21:41:12 +0100

    Add support for backchannel-logout event

---
+++ src/eSystem.Core/Security/Authentication/OpenIdConnect/Constants/LogoutEvents.cs
@@ -0,0 +1,6 @@
+﻿namespace eSystem.Core.Security.Authentication.OpenIdConnect.Constants;
+
+public static class LogoutEvents
+{
+    public const string BackChannelLogout = "http://schemas.openid.net/event/backchannel-logout";
+}
