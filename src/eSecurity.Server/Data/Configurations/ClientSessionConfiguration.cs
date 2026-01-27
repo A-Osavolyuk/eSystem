75007c7e8e1df17c8705e4eca27e01d6d0d5cce0
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-01-27 22:26:29 +0100

    Add ClientSessionEntity

---
+++ src/eSecurity.Server/Data/Configurations/ClientSessionConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using eSecurity.Server.Data.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace eSecurity.Server.Data.Configurations;
+
+public class ClientSessionConfiguration : IEntityTypeConfiguration<ClientSessionEntity>
+{
+    public void Configure(EntityTypeBuilder<ClientSessionEntity> builder)
+    {
+        builder.HasKey(x => new { x.ClientId, x.SessionId });
+        
+        builder.HasOne(x => x.Client)
+            .WithMany()
+            .HasForeignKey(f => f.ClientId)
+            .OnDelete(DeleteBehavior.Cascade);
+        
+        builder.HasOne(x => x.Session)
+            .WithMany()
+            .HasForeignKey(f => f.SessionId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
