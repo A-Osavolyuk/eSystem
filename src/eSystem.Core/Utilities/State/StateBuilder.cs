bad108d8187ef8970685590810d52b21e936e1c6
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-01-29 16:04:56 +0100

    Implement query param state management

---
+++ src/eSystem.Core/Utilities/State/StateBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace eSystem.Core.Utilities.State;
+
+public class StateBuilder
+{
+    private StateBuilder()
+    {
+    }
+    
+    private Dictionary<string, string> _data = [];
+    
+    public static StateBuilder Create() => new();
+    
+    public StateBuilder WithData(string key, string value)
+    {
+        _data[key] = value;
+        return this;
+    }
+
+    public string Build()
+    {
+        var dataJson = JsonSerializer.Serialize(_data);
+        var bytes = Encoding.UTF8.GetBytes(dataJson);
+        return Convert.ToBase64String(bytes);
+    }
+}
---
+++ src/eSystem.Core/Utilities/State/StateParser.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace eSystem.Core.Utilities.State;
+
+public static class StateParser
+{
+    public static Dictionary<string, string> Parse(string state)
+    {
+        try
+        {
+            var bytes = Convert.FromBase64String(state);
+            var dataJson = Encoding.UTF8.GetString(bytes);
+            return JsonSerializer.Deserialize<Dictionary<string, string>>(dataJson) ?? [];
+        }
+        catch (Exception)
+        {
+            return [];
+        }
+    }
+}
