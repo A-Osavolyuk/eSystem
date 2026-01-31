6bb441a73f6d6658989c40ffdcc9f5de4abaaedf
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-01-31 16:44:01 +0100

    Initial first DB migration

--- src/eCinema.Server/Controllers/v1/ConnectController.cs
+++ src/eCinema.Server/Controllers/v1/ConnectController.cs
@@ -1,15 +1,21 @@
-﻿using Microsoft.AspNetCore.Authentication;
+﻿using eCinema.Server.Hubs;
+using eSystem.Core.Http.Constants;
+using eSystem.Core.Security.Authentication.OpenIdConnect.Logout;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 
 namespace eCinema.Server.Controllers.v1;
 
 [ApiController]
 [AllowAnonymous]
 [Route("[controller]")]
-public class ConnectController : ControllerBase
+public class ConnectController(IHubContext<AuthenticationHub> hub) : ControllerBase
 {
+    private readonly IHubContext<AuthenticationHub> _hub = hub;
+
     [HttpGet("frontchannel-logout")]
     [EndpointSummary("Front-Channel Logout Endpoint")]
     public IActionResult FrontChannelLogout()
@@ -18,5 +24,13 @@ public class ConnectController : ControllerBase
             new AuthenticationProperties(),
             CookieAuthenticationDefaults.AuthenticationScheme
         );
+    }
+
+    [HttpPost("backchannel-logout")]
+    [EndpointSummary("Back-Channel Logout Endpoint")]
+    [Consumes(ContentTypes.Application.XwwwFormUrlEncoded)]
+    public async Task<IActionResult> BackChannelLogoutAsync([FromForm] BackChannelLogoutRequest request)
+    {
+        return Ok();
     }
 }
---
+++ src/eCinema.Server/Migrations/20260131154243_Initial.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eCinema.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "public");
+
+            migrationBuilder.CreateTable(
+                name: "Sessions",
+                schema: "public",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    SessionKey = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Sid = table.Column<string>(type: "character varying(36)", maxLength: 36, nullable: false),
+                    UserId = table.Column<string>(type: "character varying(36)", maxLength: 36, nullable: false),
+                    ClientId = table.Column<string>(type: "character varying(36)", maxLength: 36, nullable: false),
+                    ExpiresUtc = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    LastAccessUtc = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    IsRevoked = table.Column<bool>(type: "boolean", nullable: false),
+                    CreateDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    UpdateDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sessions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Claims",
+                schema: "public",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    SessionId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Type = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Value = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: false),
+                    CreateDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    UpdateDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Claims", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Claims_Sessions_SessionId",
+                        column: x => x.SessionId,
+                        principalSchema: "public",
+                        principalTable: "Sessions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Properties",
+                schema: "public",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    SessionId = table.Column<Guid>(type: "uuid", nullable: false),
+                    IssuedUtc = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    ExpiresUtc = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    IsPersistent = table.Column<bool>(type: "boolean", nullable: false),
+                    AllowRefresh = table.Column<bool>(type: "boolean", nullable: false),
+                    AuthTime = table.Column<long>(type: "bigint", nullable: true),
+                    Acr = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: true),
+                    Amr = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: true),
+                    RedirectUri = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: true),
+                    CreateDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    UpdateDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Properties", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Properties_Sessions_SessionId",
+                        column: x => x.SessionId,
+                        principalSchema: "public",
+                        principalTable: "Sessions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tokens",
+                schema: "public",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    SessionId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TokenType = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    EncryptedValue = table.Column<string>(type: "character varying(3000)", maxLength: 3000, nullable: false),
+                    ExpiresUtc = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    CreateDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    UpdateDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tokens", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tokens_Sessions_SessionId",
+                        column: x => x.SessionId,
+                        principalSchema: "public",
+                        principalTable: "Sessions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Claims_SessionId",
+                schema: "public",
+                table: "Claims",
+                column: "SessionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Properties_SessionId",
+                schema: "public",
+                table: "Properties",
+                column: "SessionId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sessions_SessionKey",
+                schema: "public",
+                table: "Sessions",
+                column: "SessionKey",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sessions_Sid",
+                schema: "public",
+                table: "Sessions",
+                column: "Sid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sessions_UserId",
+                schema: "public",
+                table: "Sessions",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tokens_SessionId",
+                schema: "public",
+                table: "Tokens",
+                column: "SessionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Claims",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "Properties",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "Tokens",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "Sessions",
+                schema: "public");
+        }
+    }
+}
