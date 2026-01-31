f938670692e77134e76f47d43d64ff3323344324
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-01-31 23:49:43 +0100

    Fix issue with user state initialization

--- src/eSecurity.Client/Security/Authentication/OpenIdConnect/Session/SessionAccessor.cs
+++ src/eSecurity.Client/Security/Authentication/OpenIdConnect/Session/SessionAccessor.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.Json;
+using eSecurity.Client.Common.State.States;
 using eSecurity.Client.Security.Cookies;
 using eSecurity.Client.Security.Cookies.Constants;
 using eSecurity.Client.Security.Cryptography.Protection.Constants;
@@ -7,9 +8,11 @@ using Microsoft.AspNetCore.DataProtection;
 namespace eSecurity.Client.Security.Authentication.OpenIdConnect.Session;
 
 public class SessionAccessor(
+    UserState userState,
     IHttpContextAccessor httpContextAccessor,
     IDataProtectionProvider protectionProvider) : ISessionAccessor
 {
+    private readonly UserState _userState = userState;
     private readonly HttpContext _httpContext = httpContextAccessor.HttpContext!;
     private readonly IDataProtector _protector = protectionProvider.CreateProtector(ProtectionPurposes.Session);
 
@@ -22,6 +25,11 @@ public class SessionAccessor(
             return null;
         
         var json = _protector.Unprotect(value);
-        return JsonSerializer.Deserialize<SessionCookie>(json);
+        var session = JsonSerializer.Deserialize<SessionCookie>(json);
+        if (session is null) 
+            return null;
+
+        _userState.UserId = session.UserId;
+        return session;
     }
 }
