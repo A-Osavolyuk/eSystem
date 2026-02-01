6ae5437cf4157a74e1b276b8b629a21872c9ac24
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-02-01 15:04:27 +0100

    Add logout-related properties to ClientEntity

--- src/eSecurity.Server/Data/Entities/ClientEntity.cs
+++ src/eSecurity.Server/Data/Entities/ClientEntity.cs
@@ -11,18 +11,21 @@ public class ClientEntity : Entity
     public required string Audience { get; set; }
     public required ClientType ClientType { get; set; }
     public required AccessTokenType AccessTokenType { get; set; }
-    
+
     public bool RequireClientSecret { get; set; }
     public bool RequirePkce { get; set; }
     public string? Secret { get; set; }
-    
+
     public bool AllowOfflineAccess { get; set; }
     public bool RefreshTokenRotationEnabled { get; set; } = true;
     public TimeSpan RefreshTokenLifetime { get; set; }
-    
+
+    public bool AllowFrontChannelLogout { get; set; }
+    public bool AllowBackChannelLogout { get; set; }
+
     public required SubjectType SubjectType { get; set; }
     public string? SectorIdentifierUri { get; set; }
-    
+
     public string? LogoUri { get; set; }
     public string? ClientUri { get; set; }
 
@@ -30,14 +33,16 @@ public class ClientEntity : Entity
     public ICollection<ClientAllowedScopeEntity> AllowedScopes { get; set; } = null!;
     public ICollection<ClientGrantTypeEntity> GrantTypes { get; set; } = null!;
     public ICollection<ClientUriEntity> Uris { get; set; } = null!;
-    
+
     public bool HasScopes(List<string> scopes)
         => scopes.All(scope => AllowedScopes.Any(x => x.Scope.Name == scope));
-    public bool HasScope(string scope) 
+
+    public bool HasScope(string scope)
         => AllowedScopes.Any(x => x.Scope.Name == scope);
+
     public bool HasGrantType(string grantType)
         => GrantTypes.Any(x => x.Type == grantType);
-    
-    public bool HasUri(string uri, UriType type) 
+
+    public bool HasUri(string uri, UriType type)
         => Uris.Any(x => x.Uri == uri && x.Type == type);
 }
