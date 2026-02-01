2cba7f6eb2b8f83de73fae1357384d7e0651a343
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-02-01 12:50:42 +0100

    Add RsaConverter

---
+++ src/eSystem.Core/Security/Cryptography/Encryption/RsaConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography;
+using Microsoft.IdentityModel.Tokens;
+
+namespace eSystem.Core.Security.Cryptography.Encryption;
+
+public static class RsaConverter
+{
+    public static RSA FromJsonWebkey(JsonWebKey jwk)
+    {
+        var rsa = RSA.Create();
+
+        var parameters = new RSAParameters
+        {
+            Modulus = Base64UrlEncoder.DecodeBytes(jwk.N),
+            Exponent = Base64UrlEncoder.DecodeBytes(jwk.E)
+        };
+
+        rsa.ImportParameters(parameters);
+        return rsa;
+    }
+}
