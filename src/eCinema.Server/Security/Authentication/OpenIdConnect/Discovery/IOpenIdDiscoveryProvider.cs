fad20e68cee93c03cc7cdfa9edbbb8b3eff0e3b1
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-02-01 13:06:31 +0100

    Implement OIDC discovery retrieve

---
+++ src/eCinema.Server/Security/Authentication/OpenIdConnect/Discovery/IOpenIdDiscoveryProvider.cs
@@ -0,0 +1,9 @@
+﻿using eSystem.Core.Security.Authentication.OpenIdConnect.Discovery;
+
+namespace eCinema.Server.Security.Authentication.OpenIdConnect.Discovery;
+
+public interface IOpenIdDiscoveryProvider
+{
+    public ValueTask<OpenIdConfiguration?> GetOpenIdDiscoveryAsync(CancellationToken cancellationToken = default);
+    public ValueTask<JsonWebKeySet?> GetJsonWebKeySetAsync(CancellationToken cancellationToken = default);
+}
---
+++ src/eCinema.Server/Security/Authentication/OpenIdConnect/Discovery/OpenIdDiscoveryProvider.cs
@@ -0,0 +1,29 @@
+﻿using eSystem.Core.Common.Gateway;
+using eSystem.Core.Security.Authentication.OpenIdConnect.Discovery;
+using Microsoft.Extensions.Options;
+
+namespace eCinema.Server.Security.Authentication.OpenIdConnect.Discovery;
+
+public class OpenIdDiscoveryProvider(
+    IHttpClientFactory httpClientFactory,
+    IOptions<OAuthOptions> oauthOptions,
+    GatewayOptions gatewayOptions) : IOpenIdDiscoveryProvider
+{
+    private readonly HttpClient _client = httpClientFactory.CreateClient("oidc");
+    private readonly OAuthOptions _oauthOptions = oauthOptions.Value;
+    private readonly GatewayOptions _gatewayOptions = gatewayOptions;
+    
+    public async ValueTask<OpenIdConfiguration?> GetOpenIdDiscoveryAsync(CancellationToken cancellationToken = default)
+    {
+        var discoveryUri = $"{_gatewayOptions.Url}{_oauthOptions.Authority}/.well-known/openid-configuration";
+        return await _client.GetFromJsonAsync<OpenIdConfiguration>(discoveryUri, cancellationToken);
+    }
+
+    public async ValueTask<JsonWebKeySet?> GetJsonWebKeySetAsync(CancellationToken cancellationToken = default)
+    {
+        var discovery = await GetOpenIdDiscoveryAsync(cancellationToken);
+        if (discovery is null) return null;
+
+        return await _client.GetFromJsonAsync<JsonWebKeySet>(discovery.JwksUri, cancellationToken);
+    }
+}
