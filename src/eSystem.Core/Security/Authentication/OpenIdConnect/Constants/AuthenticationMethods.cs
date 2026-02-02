7451dd04c017faee1e6186269ffe51e172c20443
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-02-02 19:35:32 +0100

    Add AuthenticationMethods constant class

---
+++ src/eSystem.Core/Security/Authentication/OpenIdConnect/Constants/AuthenticationMethods.cs
@@ -0,0 +1,19 @@
+﻿namespace eSystem.Core.Security.Authentication.OpenIdConnect.Constants;
+
+public static class AuthenticationMethods
+{
+    public const string Password = "pwd";
+    public const string Mfa = "mfa";
+    public const string Otp = "otp";
+    public const string SoftwareKey = "swk";
+    public const string HardwareKey = "hwk";
+    public const string Saml = "saml";
+
+    public static class OAuth
+    {
+        public const string Google = "oauth:google";
+        public const string Microsoft = "oauth:mircosoft";
+        public const string X = "oauth:x";
+        public const string Facebook = "oauth:facebook";
+    }
+}
