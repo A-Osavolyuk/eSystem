ca293d2beb860009d0b244580b9ac846d5c8ce70
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-02-03 21:44:11 +0100

    Include addition grant types

--- src/eSystem.Core/Security/Authorization/OAuth/Constants/GrantTypes.cs
+++ src/eSystem.Core/Security/Authorization/OAuth/Constants/GrantTypes.cs
@@ -2,8 +2,14 @@
 
 public static class GrantTypes
 {
+    public const string Implicit = "implicit";
     public const string AuthorizationCode = "authorization_code";
     public const string RefreshToken = "refresh_token";
     public const string ClientCredentials = "client_credentials";
-    public const string DeviceCode = "device_code";
+    public const string Password = "password";
+    public const string DeviceCode = "urn:ietf:params:oauth:grant-type:device_code";
+    public const string JwtBearer = "urn:ietf:params:oauth:grant-type:jwt-bearer";
+    public const string Saml2Bearer = "urn:ietf:params:oauth:grant-type:saml2-bearer";
+    public const string TokenExchange = "urn:ietf:params:oauth:grant-type:token-exchange";
+    public const string Ciba = "urn:openid:params:grant-type:ciba";
 }
