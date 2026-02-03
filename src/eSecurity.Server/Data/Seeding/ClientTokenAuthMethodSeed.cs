9c888eeb23a3806844d3e94f0b24471e95b03d56
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-02-03 14:57:29 +0100

    Add default token endpoint auth methods for pre-registered clients

---
+++ src/eSecurity.Server/Data/Seeding/ClientTokenAuthMethodSeed.cs
@@ -0,0 +1,56 @@
+﻿using eSecurity.Server.Data.Entities;
+using eSecurity.Server.Security.Authorization.Constants;
+using eSystem.Core.Data.Seeding;
+
+namespace eSecurity.Server.Data.Seeding;
+
+public class ClientTokenAuthMethodSeed : Seed<ClientTokenAuthMethodEntity>
+{
+    public override List<ClientTokenAuthMethodEntity> Get()
+    {
+        return
+        [
+            // eSecurity
+            new ClientTokenAuthMethodEntity()
+            {
+                Id = Guid.CreateVersion7(),
+                ClientId = Guid.Parse("392e390f-33bd-4f30-af70-ccbe04bbb2c4"),
+                Method = TokenAuthMethods.ClientSecretBasic
+            },
+            new ClientTokenAuthMethodEntity()
+            {
+                Id = Guid.CreateVersion7(),
+                ClientId = Guid.Parse("392e390f-33bd-4f30-af70-ccbe04bbb2c4"),
+                Method = TokenAuthMethods.ClientSecretPost
+            },
+            
+            // eCinema
+            new ClientTokenAuthMethodEntity()
+            {
+                Id = Guid.CreateVersion7(),
+                ClientId = Guid.Parse("307268b0-005c-4ee4-a0e8-a93bd0010382"),
+                Method = TokenAuthMethods.ClientSecretBasic
+            },
+            new ClientTokenAuthMethodEntity()
+            {
+                Id = Guid.CreateVersion7(),
+                ClientId = Guid.Parse("307268b0-005c-4ee4-a0e8-a93bd0010382"),
+                Method = TokenAuthMethods.ClientSecretPost
+            },
+            
+            // eMessage
+            new ClientTokenAuthMethodEntity()
+            {
+                Id = Guid.CreateVersion7(),
+                ClientId = Guid.Parse("fc1c1662-cd80-4fab-b924-a39168765558"),
+                Method = TokenAuthMethods.ClientSecretBasic
+            },
+            new ClientTokenAuthMethodEntity()
+            {
+                Id = Guid.CreateVersion7(),
+                ClientId = Guid.Parse("fc1c1662-cd80-4fab-b924-a39168765558"),
+                Method = TokenAuthMethods.ClientSecretPost
+            },
+        ];
+    }
+}
