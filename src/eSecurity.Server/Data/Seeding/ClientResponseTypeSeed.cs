51833ab7510c0800f89d3c76eb87108dfe7f76ca
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-02-03 18:04:52 +0100

    Add default response type for pre-registered clients

---
+++ src/eSecurity.Server/Data/Seeding/ClientResponseTypeSeed.cs
@@ -0,0 +1,38 @@
+﻿using eSecurity.Server.Data.Entities;
+using eSystem.Core.Data.Seeding;
+using eSystem.Core.Security.Authentication.OpenIdConnect.Constants;
+
+namespace eSecurity.Server.Data.Seeding;
+
+public class ClientResponseTypeSeed : Seed<ClientResponseTypeEntity>
+{
+    public override List<ClientResponseTypeEntity> Get()
+    {
+        return
+        [
+            // eSecurity
+            new ClientResponseTypeEntity()
+            {
+                Id = Guid.CreateVersion7(),
+                ClientId = Guid.Parse("392e390f-33bd-4f30-af70-ccbe04bbb2c4"),
+                ResponseType = ResponseTypes.Code
+            },
+
+            // eCinema
+            new ClientResponseTypeEntity()
+            {
+                Id = Guid.CreateVersion7(),
+                ClientId = Guid.Parse("307268b0-005c-4ee4-a0e8-a93bd0010382"),
+                ResponseType = ResponseTypes.Code
+            },
+
+            // eSecurity
+            new ClientResponseTypeEntity()
+            {
+                Id = Guid.CreateVersion7(),
+                ClientId = Guid.Parse("fc1c1662-cd80-4fab-b924-a39168765558"),
+                ResponseType = ResponseTypes.Code
+            }
+        ];
+    }
+}
