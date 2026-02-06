2df7f69aa638d1908041a54d8618229b11d6bb0f
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-02-06 20:48:51 +0100

    Add delegated claim type

--- src/eSystem.Core/Security/Identity/Claims/AppClaimTypes.cs
+++ src/eSystem.Core/Security/Identity/Claims/AppClaimTypes.cs
@@ -46,6 +46,7 @@ public static class AppClaimTypes
     public const string PhoneNumberVerified = "phone_number_verified";
     
     public const string Act = "act";
+    public const string Delegated = "delegated";
     public const string Events = "events";
     public const string ClientId = "client_id";
     public const string ClientSecret = "client_secret";
