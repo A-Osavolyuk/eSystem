42405e77808262ee23285fe8e6e35d20c609f75c
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-02-07 21:04:55 +0100

    Add token extraction feature

---
+++ src/eSecurity.Server/Security/Authorization/OAuth/Token/TokenExchange/Extraction/ExtractionResult.cs
@@ -0,0 +1,28 @@
+﻿namespace eSecurity.Server.Security.Authorization.OAuth.Token.TokenExchange.Extraction;
+
+public abstract class ExtractionResult
+{
+    public bool IsSucceeded { get; protected init; }
+}
+
+public abstract class ExtractionResult<TValue> : ExtractionResult
+{
+    public TValue? Value { get; private init; }
+
+    protected static TDerived Success<TDerived>(TValue value) where TDerived : ExtractionResult<TValue>, new()
+    {
+        return new TDerived()
+        {
+            IsSucceeded =  true,
+            Value = value
+        };
+    }
+
+    protected static TDerived Fail<TDerived>() where TDerived : ExtractionResult<TValue>, new()
+    {
+        return new TDerived()
+        {
+            IsSucceeded =  false,
+        };
+    }
+}
---
+++ src/eSecurity.Server/Security/Authorization/OAuth/Token/TokenExchange/Extraction/ITokenExtractor.cs
@@ -0,0 +1,6 @@
+﻿namespace eSecurity.Server.Security.Authorization.OAuth.Token.TokenExchange.Extraction;
+
+public interface ITokenExtractor<TResult> where TResult : ExtractionResult
+{
+    public ValueTask<TResult> ExtractAsync(string source, CancellationToken cancellationToken);
+}
