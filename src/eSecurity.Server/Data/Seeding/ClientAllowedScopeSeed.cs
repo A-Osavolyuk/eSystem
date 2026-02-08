7280caa6a884b83e377541939a3bc818a54a52a5
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-02-08 20:40:55 +0100

    Refactor seed of pre-registered clients allowed scopes

--- src/eSecurity.Server/Data/Seeding/ClientAllowedScopeSeed.cs
+++ src/eSecurity.Server/Data/Seeding/ClientAllowedScopeSeed.cs
@@ -46,6 +46,18 @@ public class ClientAllowedScopeSeed : Seed<ClientAllowedScopeEntity>
                 ClientId = Guid.Parse("392e390f-33bd-4f30-af70-ccbe04bbb2c4"),
                 ScopeId = Guid.Parse("33721238-6e31-4cd3-9457-11316dbf5eeb")
             },
+            new ClientAllowedScopeEntity
+            {
+                Id = Guid.CreateVersion7(),
+                ClientId = Guid.Parse("392e390f-33bd-4f30-af70-ccbe04bbb2c4"),
+                ScopeId = Guid.Parse("ca265929-4c5c-41fd-b509-dacaa5c33586")
+            },
+            new ClientAllowedScopeEntity
+            {
+                Id = Guid.CreateVersion7(),
+                ClientId = Guid.Parse("392e390f-33bd-4f30-af70-ccbe04bbb2c4"),
+                ScopeId = Guid.Parse("74be25d7-c054-4d40-a9b9-a3913c4e1b5e")
+            },
 
             // eCinema
             new ClientAllowedScopeEntity
@@ -83,6 +95,18 @@ public class ClientAllowedScopeSeed : Seed<ClientAllowedScopeEntity>
                 Id = Guid.CreateVersion7(),
                 ClientId = Guid.Parse("307268b0-005c-4ee4-a0e8-a93bd0010382"),
                 ScopeId = Guid.Parse("f067c039-ee63-44bb-b224-00a5872dfe28")
+            },
+            new ClientAllowedScopeEntity
+            {
+                Id = Guid.CreateVersion7(),
+                ClientId = Guid.Parse("307268b0-005c-4ee4-a0e8-a93bd0010382"),
+                ScopeId = Guid.Parse("ca265929-4c5c-41fd-b509-dacaa5c33586")
+            },
+            new ClientAllowedScopeEntity
+            {
+                Id = Guid.CreateVersion7(),
+                ClientId = Guid.Parse("307268b0-005c-4ee4-a0e8-a93bd0010382"),
+                ScopeId = Guid.Parse("74be25d7-c054-4d40-a9b9-a3913c4e1b5e")
             },
             
             // eCinema TV App
@@ -129,7 +153,19 @@ public class ClientAllowedScopeSeed : Seed<ClientAllowedScopeEntity>
                 Id = Guid.CreateVersion7(),
                 ClientId = Guid.Parse("fc1c1662-cd80-4fab-b924-a39168765558"),
                 ScopeId = Guid.Parse("e36782cf-c927-40c7-97d5-3fcc75fd211f")
-            }
+            },
+            new ClientAllowedScopeEntity
+            {
+                Id = Guid.CreateVersion7(),
+                ClientId = Guid.Parse("fc1c1662-cd80-4fab-b924-a39168765558"),
+                ScopeId = Guid.Parse("ca265929-4c5c-41fd-b509-dacaa5c33586")
+            },
+            new ClientAllowedScopeEntity
+            {
+                Id = Guid.CreateVersion7(),
+                ClientId = Guid.Parse("fc1c1662-cd80-4fab-b924-a39168765558"),
+                ScopeId = Guid.Parse("74be25d7-c054-4d40-a9b9-a3913c4e1b5e")
+            },
         ];
     }
 }
