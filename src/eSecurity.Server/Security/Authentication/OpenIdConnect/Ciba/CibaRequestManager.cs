71952c08e23314cd1c3753f2d98dbd5a0a6bd464
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-02-10 19:12:14 +0100

    Implement CIBA request management

---
+++ src/eSecurity.Server/Security/Authentication/OpenIdConnect/Ciba/CibaRequestManager.cs
@@ -0,0 +1,33 @@
+﻿using eSecurity.Server.Data;
+using eSecurity.Server.Data.Entities;
+using eSystem.Core.Http.Results;
+
+namespace eSecurity.Server.Security.Authentication.OpenIdConnect.Ciba;
+
+public sealed class CibaRequestManager(AuthDbContext context) : ICibaRequestManager
+{
+    private readonly AuthDbContext _context = context;
+
+    public async ValueTask<CibaRequestEntity?> FindByAuthReqIdAsync(string authReqId, 
+        CancellationToken cancellationToken = default)
+    {
+        return await _context.CibaRequests.FirstOrDefaultAsync(
+            x => x.AuthReqId == authReqId, cancellationToken);
+    }
+
+    public async ValueTask<Result> CreateAsync(CibaRequestEntity entity, CancellationToken cancellationToken = default)
+    {
+        await _context.CibaRequests.AddAsync(entity, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return Results.Ok();
+    }
+
+    public async ValueTask<Result> UpdateAsync(CibaRequestEntity entity, CancellationToken cancellationToken = default)
+    {
+        _context.CibaRequests.Update(entity);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return Results.Ok();
+    }
+}
---
+++ src/eSecurity.Server/Security/Authentication/OpenIdConnect/Ciba/ICibaRequestManager.cs
@@ -0,0 +1,16 @@
+﻿using eSecurity.Server.Data.Entities;
+using eSystem.Core.Http.Results;
+
+namespace eSecurity.Server.Security.Authentication.OpenIdConnect.Ciba;
+
+public interface ICibaRequestManager
+{
+    public ValueTask<CibaRequestEntity?> FindByAuthReqIdAsync(string authReqId,
+        CancellationToken cancellationToken = default);
+
+    public ValueTask<Result> CreateAsync(CibaRequestEntity entity, 
+        CancellationToken cancellationToken = default);
+    
+    public ValueTask<Result> UpdateAsync(CibaRequestEntity entity, 
+        CancellationToken cancellationToken = default);
+}
