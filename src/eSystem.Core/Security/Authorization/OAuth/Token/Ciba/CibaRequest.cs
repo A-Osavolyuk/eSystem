474ff0898cf7307f0114328ea13639a56a6ef3c7
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-02-10 12:38:31 +0100

    Add support for standard CIBA requests/responses

---
+++ src/eSystem.Core/Security/Authorization/OAuth/Token/Ciba/CibaRequest.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eSystem.Core.Security.Authorization.OAuth.Token.Ciba;
+
+public sealed class CibaRequest : TokenRequest
+{
+    [FromForm(Name = "auth_req_id")]
+    public required string AuthReqId { get; set; }
+}
---
+++ src/eSystem.Core/Security/Authorization/OAuth/Token/Ciba/CibaResponse.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace eSystem.Core.Security.Authorization.OAuth.Token.Ciba;
+
+public sealed class CibaResponse : TokenResponse
+{
+    [JsonPropertyName("id_token")]
+    public string? IdToken { get; set; }
+    
+    [JsonPropertyName("refresh_token")]
+    public string? RefreshToken { get; set; }
+    
+    [JsonPropertyName("scope")]
+    public string? Scope { get; set; }
+}
