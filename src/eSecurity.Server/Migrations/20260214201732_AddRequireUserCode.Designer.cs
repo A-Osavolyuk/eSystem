0f2e68a6510617250d27e6863107d920310066d7
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-02-14 21:17:45 +0100

    Re-migrate DB

--- src/eSecurity.Server/Migrations/20260214201732_AddRequireUserCode.Designer.cs
+++ src/eSecurity.Server/Migrations/20260214201732_AddRequireUserCode.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using eSecurity.Server.Data;
@@ -11,9 +12,11 @@ using eSecurity.Server.Data;
 namespace eSecurity.Server.Migrations
 {
     [DbContext(typeof(AuthDbContext))]
-    partial class AuthDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260214201732_AddRequireUserCode")]
+    partial class AddRequireUserCode
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/eSecurity.Server/Migrations/20260214201732_AddRequireUserCode.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eSecurity.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRequireUserCode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "RequireUserCode",
+                schema: "public",
+                table: "Clients",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RequireUserCode",
+                schema: "public",
+                table: "Clients");
+        }
+    }
+}
