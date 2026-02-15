8ae57687ceff963ae56a9a4dd03f134a2ab98f31
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-02-15 14:32:51 +0100

    Enhance authentication session entity

--- src/eSecurity.Server/Data/Configurations/AuthenticationSessionConfiguration.cs
+++ src/eSecurity.Server/Data/Configurations/AuthenticationSessionConfiguration.cs
@@ -9,10 +9,17 @@ public sealed class AuthenticationSessionConfiguration : IEntityTypeConfiguratio
     {
         builder.HasKey(x => x.Id);
         builder.Property(x => x.IdentityProvider).HasMaxLength(32);
+        builder.Property(x => x.OAuthFlow).HasConversion<string>();
         
         builder.HasOne(x => x.User)
             .WithMany()
             .HasForeignKey(x => x.UserId)
+            .IsRequired(false)
+            .OnDelete(DeleteBehavior.Cascade);
+        
+        builder.HasOne(x => x.Session)
+            .WithMany()
+            .HasForeignKey(x => x.SessionId)
             .IsRequired(false)
             .OnDelete(DeleteBehavior.Cascade);
     }
--- src/eSecurity.Server/Data/Entities/AuthenticationSessionEntity.cs
+++ src/eSecurity.Server/Data/Entities/AuthenticationSessionEntity.cs
@@ -7,17 +7,22 @@ public sealed class AuthenticationSessionEntity : Entity
     public Guid Id { get; set; }
 
     public string? IdentityProvider { get; set; }
-    public List<string> PassedAuthenticationMethods { get; set; } = [];
-    public List<string> RequiredAuthenticationMethods { get; set; } = [];
+    public OAuthFlow? OAuthFlow { get; set; }
+    
+    public string[] PassedAuthenticationMethods { get; set; } = [];
+    public string[] RequiredAuthenticationMethods { get; set; } = [];
     
     public bool IsRevoked { get; set; }
     public DateTimeOffset? RevokedAt { get; set; }
     
-    public DateTimeOffset ExpiresAt { get; set; }
+    public DateTimeOffset ExpiredAt { get; set; }
     public DateTimeOffset CreatedAt { get; set; }
     
-    public bool IsActive => !IsRevoked && ExpiresAt > DateTimeOffset.UtcNow;
+    public bool IsActive => !IsRevoked && ExpiredAt > DateTimeOffset.UtcNow;
 
     public Guid? UserId { get; set; }
     public UserEntity? User { get; set; }
+    
+    public Guid? SessionId { get; set; }
+    public SessionEntity? Session { get; set; }
 }
