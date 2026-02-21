4ef5cdfe2ed86d074bc65a6d6a96682e94e3b90b
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-02-21 01:26:10 +0100

    Add EnumHelper

---
+++ src/eSystem.Core/Enums/EnumHelper.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.Serialization;
+
+namespace eSystem.Core.Enums;
+
+public static class EnumHelper
+{
+    public static string? GetString<TEnum>(TEnum value) where TEnum : struct, Enum
+    {
+        var field = typeof(TEnum).GetField(value.ToString());
+        if (field is null)
+            return null;
+
+        var attribute = field.GetCustomAttribute<EnumMemberAttribute>();
+        return string.IsNullOrEmpty(attribute?.Value) ? null : attribute.Value;
+    }
+
+    public static TEnum? FromString<TEnum>(string value) where TEnum : struct, Enum
+    {
+        var fields = typeof(TEnum).GetFields(BindingFlags.Public | BindingFlags.Static);
+        foreach (var field in fields)
+        {
+            var attribute = field.GetCustomAttribute<EnumMemberAttribute>();
+            if (string.IsNullOrEmpty(attribute?.Value))
+                continue;
+            
+            if (!string.Equals(attribute.Value, value, StringComparison.Ordinal))
+                continue;
+            
+            var enumValue = field.GetValue(null);
+            if (enumValue is null)
+                return null;
+                
+            return (TEnum)enumValue;
+        }
+        
+        return null;
+    }
+}
