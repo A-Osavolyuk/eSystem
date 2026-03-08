39bea8004827bb1b15f857625843f689abd53d05
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-03-08 14:36:34 +0100

    Refactor CodeEntity

--- src/eSecurity.Server/Data/Entities/CodeEntity.cs
+++ src/eSecurity.Server/Data/Entities/CodeEntity.cs
@@ -8,12 +8,10 @@ public class CodeEntity : Entity, IExpirable
 {
     public Guid Id { get; init; } = Guid.CreateVersion7();
     
-    public Guid UserId { get; init; }
-    public string CodeHash { get; init; } = string.Empty;
-    public ActionType Action { get; init; }
     public SenderType Sender { get; init; }
-    public PurposeType Purpose { get; init; }
+    public required string CodeHash { get; init; }
     public DateTimeOffset ExpireDate { get; set; } = DateTime.UtcNow.AddMinutes(10);
-
+    
+    public Guid UserId { get; init; }
     public UserEntity? User { get; init; }
 }
