0a5d8ef9586f200bde74fcaf30cda14977019740
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-03-08 12:05:15 +0100

    Remove redundant DTOs

--- src/eSecurity.Core/Common/DTOs/ScopeDto.cs
+++
@@ -1,7 +0,0 @@
-namespace eSecurity.Core.Common.DTOs;
-
-public class ScopeDto
-{
-    public required string Name { get; set; }
-    public required string Description { get; set; }
-}
--- src/eSecurity.Core/Common/DTOs/UserDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace eSecurity.Core.Common.DTOs;
-
-public class UserDto
-{
-    public Guid Id { get; set; }
-    public string Username { get; set; } = string.Empty;
-    public string? Email { get; set; }
-}
