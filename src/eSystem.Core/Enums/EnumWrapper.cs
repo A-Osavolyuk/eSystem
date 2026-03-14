97bc2e2e22cd794a617eaa35428d84a40035926a
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-03-14 21:52:58 +0100

    Add enum wrapper feature

---
+++ src/eSystem.Core/Enums/EnumWrapper.cs
@@ -0,0 +1,13 @@
+﻿namespace eSystem.Core.Enums;
+
+public readonly struct EnumWrapper<TEnum>(TEnum value, string alias)
+    where TEnum : struct, Enum
+{
+    public string Alias { get; } = alias;
+    public TEnum Value { get; } = value;
+    
+    public static implicit operator EnumWrapper<TEnum>(TEnum value)
+    {
+        return new EnumWrapper<TEnum>(value, EnumHelper.GetString(value));
+    }
+}
