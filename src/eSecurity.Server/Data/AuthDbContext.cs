60770ff2318465026ae164a71e8f3c833c2b2aa5
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-03-14 20:11:29 +0100

    Remove VerificationEntity as redundant

--- src/eSecurity.Server/Data/AuthDbContext.cs
+++ src/eSecurity.Server/Data/AuthDbContext.cs
@@ -19,7 +19,6 @@ public sealed class AuthDbContext(DbContextOptions<AuthDbContext> options) : DbC
     public DbSet<CodeEntity> Codes { get; set; }
     public DbSet<UserLockoutStateEntity> LockoutStates { get; set; }
     public DbSet<UserRecoveryCodeEntity> UserRecoveryCodes { get; set; }
-    public DbSet<VerificationEntity> Verifications { get; set; }
     public DbSet<ClientEntity> Clients { get; set; }
     public DbSet<UserClientEntity> UserClients { get; set; }
     public DbSet<ClientAllowedScopeEntity> ClientAllowedScopes { get; set; }
