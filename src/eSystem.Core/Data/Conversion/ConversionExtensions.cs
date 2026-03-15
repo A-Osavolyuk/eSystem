1457e4efacb8573dc8dbb99ee1e8004edab5532c
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-03-15 17:24:23 +0100

    Move enum value conversion to extension method

---
+++ src/eSystem.Core/Data/Conversion/ConversionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace eSystem.Core.Data.Conversion;
+
+public static class ConversionExtensions
+{
+    public static PropertyBuilder<TEnum?> HasEnumConversion<TEnum>(
+        this PropertyBuilder<TEnum?> propertyBuilder)
+        where TEnum : struct, Enum
+    {
+        return propertyBuilder.HasConversion(new NullableEnumValueConverter<TEnum>());
+    }
+
+    public static PropertyBuilder<TEnum> HasEnumConversion<TEnum>(
+        this PropertyBuilder<TEnum> propertyBuilder)
+        where TEnum : struct, Enum
+    {
+        return propertyBuilder.HasConversion(new EnumValueConverter<TEnum>());
+    }
+}
