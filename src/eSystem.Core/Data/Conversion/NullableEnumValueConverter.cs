6d7fc3e3e61feb0de930c508de4d9438151e25c1
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-03-15 17:24:05 +0100

    Add NullableEnumValueConverter

---
+++ src/eSystem.Core/Data/Conversion/NullableEnumValueConverter.cs
@@ -0,0 +1,13 @@
+﻿using eSystem.Core.Enums;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace eSystem.Core.Data.Conversion;
+
+public sealed class NullableEnumValueConverter<TEnum> : ValueConverter<TEnum?, string?> 
+    where TEnum : struct, Enum
+{
+    public NullableEnumValueConverter() 
+        : base(
+            v => v == null ? null : EnumHelper.GetString(v.Value), 
+            v => v == null ? null : EnumHelper.FromStringOrThrow<TEnum>(v)) { }
+}
