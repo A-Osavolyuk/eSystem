b28bcaf634c971b37096a14c9409c71b9d25a99e
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-04-12 21:22:42 +0200

    feat: add ACR definion AuthenticationContextClassReference

---
+++ src/eSystem.Core/Security/Authentication/OpenIdConnect/AuthenticationContextClassReference.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+using eSystem.Core.Enums;
+using eSystem.Core.Enums.Serialization;
+
+namespace eSystem.Core.Security.Authentication.OpenIdConnect;
+
+[JsonConverter(typeof(JsonEnumValueConverter<AuthenticationContextClassReference>))]
+public enum AuthenticationContextClassReference
+{
+    [EnumValue("loa1")]
+    LoA1,
+    
+    [EnumValue("loa2")]
+    LoA2,
+    
+    [EnumValue("loa3")]
+    LoA3
+}
