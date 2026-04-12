4da76113cb351155b9de5ee8218a9442a59f4fe3
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-04-12 13:48:30 +0200

    refactor(configs): enhance configurations extensions

--- src/eSystem.Core/Common/Configuration/ConfigurationExtensions.cs
+++ src/eSystem.Core/Common/Configuration/ConfigurationExtensions.cs
@@ -9,18 +9,12 @@ public static class ConfigurationExtensions
         public TValue Get<TValue>(string key)
         {
             var section = configuration.GetSection(key);
-        
             if (!section.Exists())
-            {
-                throw new KeyNotFoundException($"Configuration section '{key}' was not found.");
-            }
+                throw new KeyNotFoundException($"Section '{key}' was not found.");
 
             var value = section.Get<TValue>();
-
             if (value == null)
-            {
-                throw new InvalidOperationException($"Configuration value for key '{key}' could not be found or is null.");
-            }
+                throw new InvalidOperationException($"Value of key '{key}' could not be found or is null.");
 
             return value;
         }
