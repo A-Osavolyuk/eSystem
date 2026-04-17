8e3073a9879a18339bff6ea74e8cb497854c46ac
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-04-17 13:03:08 +0200

    feat: add RequestHandlerBase

---
+++ src/eSystem.Core/Mediator/RequestHandlerBase.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace eSystem.Core.Mediator;
+
+public abstract class RequestHandlerBase<TRequest, TResponse>(IHttpContextAccessor httpContextAccessor) 
+    : RequestHandlerBase(httpContextAccessor), IRequestHandler<TRequest, TResponse> 
+    where TRequest : IRequest<TResponse>
+{
+    public abstract Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken);
+}
+
+public abstract class RequestHandlerBase<TRequest>(IHttpContextAccessor httpContextAccessor) 
+    : RequestHandlerBase(httpContextAccessor), IRequestHandler<TRequest> 
+    where TRequest : IRequest
+{
+    public abstract Task Handle(TRequest request, CancellationToken cancellationToken);
+}
+
+public abstract class RequestHandlerBase(IHttpContextAccessor httpContextAccessor)
+{
+    protected IHttpContextAccessor HttpContextAccessor { get; } = httpContextAccessor;
+    protected HttpContext HttpContext => 
+        HttpContextAccessor.HttpContext ?? throw new InvalidOperationException("HttpContext is not available");
+}
