e17a95aee427887f835480030256d2668b92dccf
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-05-16 13:46:28 +0200

    feat: add BFF features

---
+++ src/eSystem.Core.Server/Bff/BffExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace eSystem.Core.Server.Bff;
+
+public static class BffExtensions
+{
+    public static void AddBff(this IHostApplicationBuilder builder)
+    {
+        builder.Services.Configure<BffOptions>(builder.Configuration.GetSection("OAuth"));
+    }
+}
---
+++ src/eSystem.Core.Server/Bff/BffOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace eSystem.Core.Server.Bff;
+
+public sealed class BffOptions
+{
+    public required string FrontendUri { get; set; }
+}
+
