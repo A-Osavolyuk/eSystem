38b906c49df27ec5f35492d01f530bc7d1375b50
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-06-19 14:06:57 +0200

    feat: add Normalizer feature

---
+++ src/eSecurity.Idp/Common/Validation/Normalizer.cs
@@ -0,0 +1,12 @@
+﻿namespace eSecurity.Idp.Common.Validation;
+
+public static class Normalizer
+{
+    public static string Normalize(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new ArgumentNullException(nameof(value));
+
+        return value.ToUpperInvariant();
+    }
+}
