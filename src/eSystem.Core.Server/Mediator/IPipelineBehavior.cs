71b25488202b3451c8562393a9cd99013c9e3c52
AleksanDOOR <79473627+A-Osavolyuk@users.noreply.github.com>
2026-06-22 16:43:43 +0200

    refactor: enhance IPipelineBehavior

--- src/eSystem.Core.Server/Mediator/IPipelineBehavior.cs
+++ src/eSystem.Core.Server/Mediator/IPipelineBehavior.cs
@@ -3,7 +3,10 @@
 public interface IPipelineBehavior<in TRequest, TResponse> 
     where TRequest : IRequest<TResponse>
 {
-    public Task Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken);
+    public Task<TResponse> Handle(
+        TRequest request, 
+        RequestHandlerDelegate<TResponse> next, 
+        CancellationToken cancellationToken);
 }
 
 public delegate Task<TResponse> RequestHandlerDelegate<TResponse>();
